3eedccd2e872bc160397b26276fb76ebc7836026
Pestrikov Alexander <alexander.pestrikov@cbca.ru>
2013-05-20 19:06:57 +0400

    Настроена страница блога, созданы контроллеры для удаления записей. Допилен файл _blogRecordTpl

--- app/controllers/Blog.scala
+++ app/controllers/Blog.scala
@@ -17,11 +17,44 @@ import views.html.blog._
 
 
 
+
 object Blog extends Controller with ContextT with AclT {
+
+  /**
+   * Функция запроса всех записей блога
+   * @return
+   */
 
   def recordList = maybeAuthenticated { implicit pw_opt => implicit request =>
     val blogs = MBlog.getAll
     Ok(listTpl(blogs))
+  }
+
+  /**
+   *Функция запроса запис в блоге по id.
+   * @param blog_id
+   * @return
+   */
+
+  def readOne(blog_id: String) = maybeAuthenticated { implicit pw_opt => implicit request =>
+    val recordOpt = MBlog.getById(blog_id)
+    recordOpt match {
+    case Some(record) =>
+    Ok(_blogRecordTpl(record))
+    case None =>
+    NotFound
+    }
+  }
+
+  /**
+   * Функция уудаление записи в блоге.
+   * @param rec_id
+   * @return
+   */
+
+  def deleteRecAdmin (rec_id: String) =  isAdminAction { implicit pw_opt => implicit request =>
+    val deleteRec = MBlog.delete(rec_id)
+    Ok(deleteRec.toString)
   }
 
 }
