43eff14b9c38f6f9a62218fa63e93e06af930b37
Pestrikov Alexander <alexander.pestrikov@cbca.ru>
2013-05-21 19:49:42 +0400

    Добавлена форма и функции для записей блога.

--- app/controllers/Blog.scala
+++ app/controllers/Blog.scala
@@ -11,9 +11,12 @@ package controllers
 
 import models.MBlog
 import play.api._
+import play.api.data._
+import play.api.data.Forms._
 import play.api.mvc._
 import util.{AclT, ContextT}
 import views.html.blog._
+import play.api.data.validation.Constraints._
 
 
 
@@ -52,9 +55,77 @@ object Blog extends Controller with ContextT with AclT {
    * @return
    */
 
-  def deleteRecAdmin (rec_id: String) =  isAdminAction { implicit pw_opt => implicit request =>
-    val deleteRec = MBlog.delete(rec_id)
-    Ok(deleteRec.toString)
+  // def deleteRecAdmin (rec_id: String) =  isAdminAction { implicit pw_opt => implicit request =>
+  //  val deleteRec = MBlog.delete(rec_id)
+  //  Ok(deleteRec.toString)
+  //}
+
+
+   def deleteRecAdmin (rec_id: String) =  maybeAuthenticated { implicit pw_opt => implicit request =>
+   val deleteRec = MBlog.delete(rec_id)
+   Ok(deleteRec.toString)
   }
+
+
+  /**
+   * Определение параметров формы для создания записи в блоге.
+   */
+
+  val postFormM = Form(
+    tuple(
+    "id" ->   nonEmptyText,
+    "title" ->  nonEmptyText,
+    "description" ->   nonEmptyText,
+    "bg_image" -> text,
+    "bg_color" -> text,
+    "text"  -> text
+   )
+)
+
+
+ // val dataId = Map {
+ //   case (title,description) =>
+  // }
+
+  /**
+   * Функция получения параметров из вышеопределенной формы
+   * @return
+   */
+
+ def newRecS =  maybeAuthenticated { implicit pw_opt => implicit request =>
+    postFormM.bindFromRequest.fold(
+      formWithErrors => NotAcceptable,
+       {case (id, title, description, bg_image, bg_color, text1) =>
+     val new_blog_record = MBlog(id = id, title = title, description = description, bg_image = bg_image, bg_color = bg_color, text = text1).save
+      Redirect(routes.Blog.readOne(new_blog_record.id))
+    }
+  )
+}
+
+
+//  val saveForm = Form(
+//    mapping (
+//      "id"  ->  number,
+//      "title" ->   text,
+//      "description" -> text,
+//      "bg_image" -> text,
+//      "bg_color" -> text,
+//      "text" -> text
+//    )
+//  )
+
+//  val dataForm = Map ( id -> 18, title ->
+ // )
+
+  /**
+   * Функция получения чистой формы для создания новой записи в блоге.
+   * @param
+   * @return
+   */
+  def newRec  = maybeAuthenticated { implicit pw_opt => implicit request =>
+     Ok(postFormTpl(postFormM, isEdit = false))
+ }
+
+
 
 }
