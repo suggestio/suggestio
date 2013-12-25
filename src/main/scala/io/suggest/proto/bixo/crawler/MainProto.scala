5ab9cb26b5edadd0fbccbd360c55a4f7bff436f3
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2013-12-25 19:26:14 +0400

    bixo.MainProto: ReferrersBulk для пересылки награбленных реферреров из web-морды (#103).

--- src/main/scala/io/suggest/proto/bixo/crawler/MainProto.scala
+++ src/main/scala/io/suggest/proto/bixo/crawler/MainProto.scala
@@ -14,3 +14,10 @@ object MainProto {
 }
 
 case object MajorRebuildMsg extends Serializable
+
+/**
+ * Веб-морда перекидывает пачку награбленных referrer'ов в main-кравлер.
+ * Кравлер должен проверять ссылки на принадлежность к dkeys самостоятельно.
+ * @param urls Ссылки на страницы.
+ */
+case class ReferrersBulk(urls: List[String]) extends Serializable
