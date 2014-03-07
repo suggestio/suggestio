c82c64a6c271004d6e8f928c30ea0edaa30a4017
Maksim Sharipov <maksim.sharipov@cbca.ru>
2014-03-07 13:47:54 +0400

    cbca-select ver 0.2

--- app/assets/javascripts/market/lk/mx_dev.js
+++ app/assets/javascripts/market/lk/mx_dev.js
@@ -1,6 +1,9 @@
+var cbca = {};
+
 ;$(function() {
 
-  CbcaSelect();
+  cbca.select = new CbcaSelect();
+  ChooseCategory();
 
 });
 
@@ -15,14 +18,19 @@ function CbcaSelect() {
       var $this = $(this),
       $dropDown = $this.find('.dropdown');
 
-      $this.data({
-        'open': false
-      });
-
-      $dropDown.css('height', 0);
+      if(!$this.data('init')) {
+        $this.data({
+          'open': false,
+          'init': true
+        });
+        $dropDown.css('height', 0);
+      }
     });
 
+   }//self.init end
 
+
+  self.bindEvents = function() {
     $(document).on('click', '.cbca-select .selectbox',
     function(e) {
       e.stopPropagation();
@@ -55,7 +63,7 @@ function CbcaSelect() {
       self.closeAll();
     });
 
-  }//self.init end
+  }//self.bindEvents end
 
 
   self.close = function($select) {
@@ -72,7 +80,7 @@ function CbcaSelect() {
 
   self.setValue = function($select, title, value) {
     $select.find('.selected').html(title);
-    $select.find('.result').val(value);
+    $select.find('.result').val(value).trigger('change');
   }//self.setValue end
 
 
@@ -87,6 +95,77 @@ function CbcaSelect() {
     });
   }//self.closeAll end
 
-  self.init();
+  self.generateCbcaSelect = function(options) {
 
+    var defaults = {
+      data: '',
+      id: '',
+      preText: '',
+      style: ''
+    },
+    options = $.extend(defaults, options),
+    html = '<span class="cbca-select" id="'+options.id+'" style="'+options.style+'">'+options.preText+'<input type="hidden" value="" class="result"  />'+
+    '<div class="selectbox"><div class="value"><div class="selected">--------</div><div class="dropdown">';
+
+    for(key in options.data) {
+      var obj = options.data[key],
+      optionClass;
+
+      if(key%2 == 0) {
+        optionClass = 'odd';
+      }
+      else {
+        optionClass = 'even';
+      }
+      html += '<div class="option '+optionClass+'" data-value="'+obj.id+'">'+obj.name+'</div>';
+    }
+
+    html += '</div></div><div class="triger"></div></div></span>';
+
+    return html;
+  }
+
+  self.init();
+  self.bindEvents();
+
+}
+
+function ChooseCategory() {
+    var self = this;
+
+    self.init = function() {
+
+      $(document).on('change', '#first-cat',
+      function(e) {
+        var $this = $(this),
+        catId = $this.val();
+
+
+        jsRoutes.controllers.MarketCategory.directSubcatsOf(catId).ajax({
+          success: function(data) {
+            if(data.length) {
+              var $wrap = $this.closest('.cbca-select'),
+              left = $wrap.width();
+
+              var html = cbca.select.generateCbcaSelect({
+                'data': data,
+                'id': 'second-cat',
+                'preText': '<span class="pre-span">&nbsp;/&nbsp;</span>',
+                'style':  'top: 0; left: '+left+'px;'
+              });
+              $('.categories').append(html);
+              cbca.select.init();
+            }
+          },
+          error: function(error) {
+            console.log(error);
+          }
+        });
+
+      });
+    }//self.init end
+
+
+
+    self.init();
 }
