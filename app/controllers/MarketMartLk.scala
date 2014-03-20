5fc45ed5420338e0db9ae48a3eaaba3c8774ac57
Maksim Sharipov <maksim.sharipov@cbca.ru>
2014-03-20 16:15:51 +0400

    galochki

--- app/controllers/MarketMartLk.scala
+++ app/controllers/MarketMartLk.scala
@@ -126,6 +126,19 @@ object MarketMartLk extends SioController with PlayMacroLogsImpl {
   }
 
   /**
+   * Рендер страницы со списком арендаторов.
+   * @param martId id ТЦ
+   * @param sortByRaw Сортировка магазинов по указанному полю. Если не задано, то порядок не определён.
+   * @param isReversed Если true, то будет сортировка в обратном порядке. Иначе в прямом.
+   */
+  def shopsShow(martId: MartId_t, sortByRaw: Option[String], isReversed: Boolean) = IsMartAdmin(martId).async { implicit request =>
+    val sortBy = sortByRaw flatMap handleShopsSortBy
+    MShop.findByMartId(martId, sortBy, isReversed) map { shops =>
+      Ok(shopsShowTpl(request.mmart, shops))
+    }
+  }
+
+  /**
    * Рендер страницы с формой редактирования ТЦ в личном кабинете.
    * @param martId id ТЦ.
    */
