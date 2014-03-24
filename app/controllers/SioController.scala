255b31dfa2f8099a992028f079ef08a6b058cedb
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2014-03-24 18:17:13 +0400

    ctl.MarketAd: preview submit для shop-формы.

--- app/controllers/SioController.scala
+++ app/controllers/SioController.scala
@@ -12,24 +12,26 @@ import play.api.libs.json.{JsString, JsValue}
  * Suggest.io
  * User: Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
  * Created: 11.10.13 11:43
- * Description:
+ * Description: Базис для контроллеров s.io.
  */
 
 trait SioController extends Controller with ContextT {
+
   implicit protected def simpleResult2async(sr: SimpleResult): Future[SimpleResult] = {
     Future.successful(sr)
   }
 
   implicit def sn = SiowebNotifier
 
-  implicit def html2string(html: HtmlFormat.Appendable): String = {
-    HtmlCompressUtil.compressor.compress(html.body)
-  }
+  implicit def html4email(html: HtmlFormat.Appendable) = HtmlCompressUtil.compressForEmail(html)
 
-  implicit def html2jsStr(html: HtmlFormat.Appendable) = JsString(html)
+  implicit def html2jsStr(html: HtmlFormat.Appendable) = JsString(
+    HtmlCompressUtil.compressForJson(html)
+  )
 
   implicit def txt2str(txt: TxtFormat.Appendable): String = txt.body.trim
 
   implicit def txt2jsStr(txt: TxtFormat.Appendable) = JsString(txt)
+
 }
 
