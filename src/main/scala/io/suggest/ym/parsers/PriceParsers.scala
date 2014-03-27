93ef152358695fb7e16e73bcbec7f5c0d9131803
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2014-03-27 21:13:19 +0400

    PriceParsers: тесты, фиксы. Теперь работает.

--- src/main/scala/io/suggest/ym/parsers/PriceParsers.scala
+++ src/main/scala/io/suggest/ym/parsers/PriceParsers.scala
@@ -13,44 +13,53 @@ import java.util.Currency
 object PriceParsers extends JavaTokenParsers {
 
   /** Парсер float-чисел, вводимых юзерами. Не исключаем, что юзеры могут вводить float-числа, начиная их с запятой. */
-  val floatP: Parser[Float] = """-?(\d+([.,]+\d*)?|\d*[.,]+\d+)""".r ^^ { _.toFloat }
+  val floatP: Parser[Float] = """-?(\d+([.,]+\d*)?|\d*[.,]+\d+)""".r ^^ {
+    _.replace(',', '.').toFloat
+  }
 
   type Price_t = (Float, Currency)
   type PriceP_t = Parser[Price_t]
 
+  // TODO Надо бы по-сильнее отделить валюты от парсеров цен.
   val currRUBc = Currency.getInstance("RUB")
 
-  // Российские рубли. FIXME Белорусские рубли тоже могут сюда попасть, если обозначены через "руб." Латинская p тоже подходит.
+  /** Парсер цен в российских рублях. */
   val priceRUBp: PriceP_t = {
-    val currRUBp: Parser[_] = "(?iu)р(уб(л?[а-я]*)?.?".r | "(?i)(r(ou|u|uo)b(le?)|RUR)".r | "?(i)p\\.?".r
-    (floatP <~ opt(currRUBp)) ^^ { price => price -> currRUBc }
+    val rubl: Parser[_]   = "(?iu)[рp]([уy][б6](л[а-я]*)?)?\\.?".r
+    val rouble: Parser[_] = "(?i)(r(ou|u|uo)b(le?s?)?|RUR)".r
+    val currRUBp: Parser[_] = rubl | rouble
+    (floatP <~ currRUBp) ^^ { price => price -> currRUBc }
   }
 
-  // Доллары США
+
+  val currUSDc = Currency.getInstance("USD")
+
+  /** Парсер цен в долларах США. */
   val priceUSDp: PriceP_t = {
-    val currUSDc = Currency.getInstance("USD")
     val dsign: Parser[_] = "$"
     val usd: Parser[_] = "(?i)USD".r
-    val dollar: Parser[_] = "?(iu)долл?ар[а-я]{0,3}".r
+    val dollar: Parser[_] = "(?iu)(д[оo]лл?[аaоo][рpr]|[б6][aа][kк]+[cс])[а-я]{0,3}".r
     val prefixPrice = (dsign | usd) ~> floatP
     val postfixPrice = floatP <~ (dsign | usd | dollar)
     (prefixPrice | postfixPrice) ^^ { floatPrice => (floatPrice, currUSDc) }
   }
 
-  // Евро
+
+  val currEURc = Currency.getInstance("EUR")
+
+  /** Парсер цен в евро. */
   val priceEURp: PriceP_t = {
-    val currEURc = Currency.getInstance("EUR")
     val esign: Parser[_] = "€"
-    val eur: Parser[_] = "(?i)EUR".r
-    val evro: Parser[_] = "(?iu)(й?[еэ])[ву]ро".r
-    val prefixPrice = (esign | eur) ~> floatP
-    val postixPrice = floatP <~ (esign | eur | evro)
+    val eur: Parser[_] = "(?i)EURo?".r
+    val evro: Parser[_] = "(?iu)(й?[еэe])[вуuv][рr][оo]".r
+    val prefixPrice = (eur | esign) ~> floatP
+    val postixPrice = floatP <~ (eur | evro | esign)
     (prefixPrice | postixPrice) ^^ { price => (price, currEURc) }
   }
 
 
-  // Парсинг цен. Нужно разбирать, что там юзеры вдалбливают в качестве цены. Это не обязательно является числом.
-  /** Парсер произвольной строки с ценой. Цена может содержать валюту, и точки-запятые в качестве разделей десятичной части. */
+  /** Парсер произвольной строки с ценой в России.
+    * Цена должна содержать валюту, и точки-запятые в качестве разделей десятичной части. */
   val currPriceParser: PriceP_t = {
     priceRUBp | priceUSDp | priceEURp
   }
---
+++ src/test/scala/io/suggest/ym/parsers/PriceParsersTest.scala
@@ -0,0 +1,72 @@
+package io.suggest.ym.parsers
+
+import org.scalatest._
+import java.util.Currency
+
+/**
+ * Suggest.io
+ * User: Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
+ * Created: 27.03.14 20:31
+ * Description: Тесты для парсеров цен.
+ */
+class PriceParsersTest extends FlatSpec with Matchers {
+
+  import PriceParsers._
+
+  "PriceParsers" should "parse RUB prices" in {
+    val rp = rpF(priceRUBp, currRUBc)
+    // русский
+    rp("10 руб.", 10F)
+    rp("10.5 руб", 10.5F)
+    rp("10,5 руб", 10.5F)
+    rp("1000р.", 1000F)
+    rp("1000 РублеЙ", 1000F)
+    // английский
+    rp("100.50 RUB", 100.50F)
+    rp("1234 roubles", 1234F)
+    rp("1234.5 p.", 1234.5F)
+  }
+
+  it should "parse USD prices" in {
+    val dp = rpF(priceUSDp, currUSDc)
+    dp("$1", 1F)
+    dp("$ 1", 1F)
+    dp("1$", 1F)
+    dp("1050 USD", 1050F)
+    dp("10. $", 10F)
+    dp("USD 15.40", 15.40F)
+    // русский
+    dp("10 долларов", 10F)
+    dp("5.50 доллара", 5.50F)
+    dp("5,50 доллара", 5.50F)
+    dp("5 баксов", 5F)
+  }
+
+  it should "parse EUR prices" in {
+    val ep = rpF(priceEURp, currEURc)
+    ep("€5.40", 5.40F)
+    ep("5,5€", 5.5F)
+    ep("6.66 EUR", 6.66F)
+    ep("6,66 EUR", 6.66F)
+    ep("6 euro", 6F)
+    // русский
+    ep("4,6 евро", 4.6F)
+    ep("4.6 еуро", 4.6F)
+  }
+
+
+  /** Генератор функций тестирования.
+    * @param p парсер денег.
+    * @param curr правильная валюта результата.
+    * @param lag допустимая точность float.
+    * @return Функция тестирования, принимающая исходную строку и результат.
+    */
+  private def rpF(p: PriceP_t, curr: Currency, lag: Float = 0.02F) = {
+    (s: String, result: Float) =>
+      val pres = parse(p, s: String)
+      pres.successful shouldBe true
+      val (price, curr) = pres.get
+      curr shouldBe curr
+      price shouldBe result +- 0.02F
+  }
+}
