5a3170a880f7b7ea6b5c9828be6fe9c11c970956
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2014-05-06 18:45:10 +0400

    + block11Tpl

--- app/util/blocks/BlocksConf.scala
+++ app/util/blocks/BlocksConf.scala
@@ -653,6 +653,52 @@ object BlocksConf extends Enumeration {
   }
 
 
+  val Block11 = new Val(11, "promoNarrow11") with SaveBgImg {
+    val heightBf = BfInt(BlockMeta.HEIGHT_ESFN, BlocksEditorFields.Height, minValue = 300, maxValue=460, defaultValue = Some(300))
+    val titleBf = BfText("title", BlocksEditorFields.TextArea, maxLen = 256)
+    val descrBf = BfText("descr", BlocksEditorFields.TextArea, maxLen = 256)
+
+    /** Описание используемых полей. На основе этой спеки генерится шаблон формы редактора. */
+    override def blockFields: List[BlockFieldT] = List(
+      heightBf, bgImgBf, titleBf, descrBf
+    )
+
+    /** Набор маппингов для обработки данных от формы. */
+    override def strictMapping: Mapping[BlockMapperResult] = mapping(
+      heightBf.getStrictMappingKV,
+      bgImgBf.getStrictMappingKV,
+      titleBf.getOptionalStrictMappingKV,
+      descrBf.getOptionalStrictMappingKV
+    )
+    {(height, bgBim, titleOpt, descrOpt) =>
+      val blk = AOBlock(
+        n = 0,
+        text1 = titleOpt,
+        text2 = descrOpt
+      )
+      val bd = BlockDataImpl(
+        blockMeta = BlockMeta(
+          height = height,
+          blockId = id
+        ),
+        offers = List(blk)
+      )
+      BlockMapperResult(bd, bgBim)
+    }
+    {bmr =>
+      val height = bmr.bd.blockMeta.height
+      val bgBim: BlockImgMap = bmr.bim.filter(_._1 == bgImgBf.name)
+      val offerOpt = bmr.bd.offers.headOption
+      val title = offerOpt.flatMap(_.text1)
+      val descr = offerOpt.flatMap(_.text2)
+      Some( (height, bgBim, title, descr) )
+    }
+
+    /** Шаблон для рендера. */
+    override def template = _block11Tpl
+  }
+
+
   /** Сортированные значения. Обращение напрямую к values порождает множество с неопределённым порядком,
     * а тут - сразу отсортировано по id. */
   val valuesSorted = values.toSeq.sortBy(_.id)
