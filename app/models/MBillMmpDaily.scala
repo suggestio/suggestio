e8d47e81278a98448845dbffd42f2dc2f89fccaf
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2014-06-02 11:51:59 +0400

    Поддержка задания и сохранения календарей праздников в MBillMmpDaily.

--- app/models/MBillMmpDaily.scala
+++ app/models/MBillMmpDaily.scala
@@ -17,12 +17,13 @@ object MBillMmpDaily extends FindByContract[MBillMmpDaily] {
   val TABLE_NAME = "bill_mmp_daily"
 
   val rowParser = get[Pk[Int]]("id") ~ get[Int]("contract_id") ~ get[Float]("mmp_weekday") ~ get[Float]("mmp_weekend") ~
-    get[Float]("mmp_primetime") ~ get[String]("currency_code") ~ get[Float]("on_start_page") map {
-    case id ~ contractId ~ mmpWeekday ~ mmpWeekend ~ mmpPrimetime ~ currencyCode ~ onStartPage =>
+    get[Float]("mmp_primetime") ~ get[String]("currency_code") ~ get[Float]("on_start_page") ~
+    get[String]("weekend_cal_id") ~ get[String]("prime_cal_id") map {
+    case id ~ contractId ~ mmpWeekday ~ mmpWeekend ~ mmpPrimetime ~ currencyCode ~ onStartPage ~ weekendCalId ~ primeCalId =>
       MBillMmpDaily(
         id = id, contractId = contractId, currencyCode = currencyCode,
         mmpWeekday = mmpWeekday, mmpWeekend = mmpWeekend, mmpPrimetime = mmpPrimetime,
-        onStartPage = onStartPage
+        onStartPage = onStartPage, weekendCalId = weekendCalId, primeCalId = primeCalId
       )
   }
 
@@ -45,6 +46,8 @@ case class MBillMmpDaily(
   mmpWeekend    : Float,
   mmpPrimetime  : Float,
   onStartPage   : Float,
+  weekendCalId  : String,
+  primeCalId    : String,
   currencyCode  : String = CurrencyCodeOpt.CURRENCY_CODE_DFLT,
   id            : Pk[Int] = NotAssigned
 ) extends SqlModelSave[MBillMmpDaily] with CurrencyCode with MBillContractSel with SqlModelDelete {
@@ -53,18 +56,19 @@ case class MBillMmpDaily(
   override def companion = MBillMmpDaily
 
   override def saveInsert(implicit c: Connection): MBillMmpDaily = {
-    SQL("INSERT INTO " + TABLE_NAME + "(contract_id, currency_code, mmp_weekday, mmp_weekend, mmp_primetime, on_start_page) " +
-      "VALUES ({contractId}, {currencyCode}, {mmpWeekday}, {mmpWeekend}, {mmpPrimetime}, {onStartPage})")
+    SQL("INSERT INTO " + TABLE_NAME + "(contract_id, currency_code, mmp_weekday, mmp_weekend, mmp_primetime, on_start_page, weekend_cal_id, prime_cal_id) " +
+      "VALUES ({contractId}, {currencyCode}, {mmpWeekday}, {mmpWeekend}, {mmpPrimetime}, {onStartPage}, {weekendCalId}, {primeCalId})")
       .on('contractId -> contractId, 'currencyCode -> currencyCode, 'mmpWeekday -> mmpWeekday, 'mmpWeekend -> mmpWeekend,
-          'mmpPrimetime -> mmpPrimetime, 'onStartPage -> onStartPage)
+          'mmpPrimetime -> mmpPrimetime, 'onStartPage -> onStartPage, 'weekendCalId -> weekendCalId, 'primeCalId -> primeCalId)
       .executeInsert(rowParser single)
   }
 
   override def saveUpdate(implicit c: Connection): Int = {
     SQL("UPDATE " + TABLE_NAME + " SET currency_code = {currencyCode}, mmp_weekday = {mmpWeekday}, " +
-      "mmp_weekend = {mmpWeekend}, mmp_primetime = {mmpPrimetime}, on_start_page = {onStartPage} WHERE id = {id}")
+      "mmp_weekend = {mmpWeekend}, mmp_primetime = {mmpPrimetime}, on_start_page = {onStartPage}," +
+      "weekend_cal_id = {weekendCalId}, prime_cal_id = {primeCalId} WHERE id = {id}")
       .on('id -> id.get, 'currencyCode -> currencyCode, 'mmpWeekday -> mmpWeekday, 'mmpWeekend -> mmpWeekend,
-          'mmpPrimetime -> mmpPrimetime, 'onStartPage -> onStartPage)
+          'mmpPrimetime -> mmpPrimetime, 'onStartPage -> onStartPage, 'weekendCalId -> weekendCalId, 'primeCalId -> primeCalId)
       .executeUpdate()
   }
 }
