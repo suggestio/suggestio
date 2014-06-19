e8287b8472aca4bac1173ee8c36f84c584cbb1fc
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2014-06-19 15:10:26 +0400

    Продолжается запил модерации s.io.

--- app/controllers/SysMdr.scala
+++ app/controllers/SysMdr.scala
@@ -1,10 +1,13 @@
 package controllers
 
+import util.PlayMacroLogsImpl
 import util.acl.IsSuperuser
 import scala.concurrent.ExecutionContext.Implicits.global
 import util.SiowebEsUtil.client
 import util.event.SiowebNotifier.Implicts.sn
 import views.html.sys1.mdr._
+import play.api.Play.{current, configuration}
+import models._
 
 /**
  * Suggest.io
@@ -12,11 +15,33 @@ import views.html.sys1.mdr._
  * Created: 19.06.14 10:45
  * Description: Sys Moderation - контроллер, заправляющий s.io-модерацией рекламных карточек.
  */
-object SysMdr extends SioController {
+object SysMdr extends SioController with PlayMacroLogsImpl {
+
+  import LOGGER._
+
+  /** Сколько карточек на одну страницу модерации. */
+  val FREE_ADVS_PAGE_SZ: Int = configuration.getInt("mdr.freeAdvs.page.size") getOrElse 10
+
 
   /** Отобразить начальную страницу раздела модерации рекламных карточек. */
-  def index = IsSuperuser.async { implicit request =>
+  def index = IsSuperuser { implicit request =>
     Ok(mdrIndexTpl())
+  }
+
+  /** Страница с бесплатно-размещёнными рекламными карточками, подлежащими модерации s.io. */
+  def freeAdvs(page: Int) = IsSuperuser.async { implicit request =>
+    MAd.findSelfAdvNonMdr(maxResults = FREE_ADVS_PAGE_SZ, offset = page * FREE_ADVS_PAGE_SZ) flatMap { mads =>
+      MAdnNodeCache.multigetByIdCached( mads.map(_.producerId) ) map { producers =>
+        val prodsMap = producers.map { p => p.id.get -> p }.toMap
+        Ok(freeAdvsTpl(mads, prodsMap, page))
+      }
+    }
+  }
+
+
+  /** Модерация одной карточки. */
+  def freeAdvMdr(adId: String) = IsSuperuser.async { implicit request =>
+    ???
   }
 
 }
