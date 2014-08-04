4e0b0be5bcd22c37c2d0701a176a60f2f4cc8adf
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2014-08-04 15:19:22 +0400

    Начат запил страницы техподдержки в ЛК.

---
+++ app/controllers/MarketLkSupport.scala
@@ -0,0 +1,52 @@
+package controllers
+
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import util.event.SiowebNotifier.Implicts.sn
+import util.SiowebEsUtil.client
+import util.PlayLazyMacroLogsImpl
+import util.acl.IsAdnNodeAdmin
+import views.html.market.lk.support._
+
+/**
+ * Suggest.io
+ * User: Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
+ * Created: 04.08.14 14:39
+ * Description: Контроллер для обратной связи с техподдержкой s.io в личном кабинете узла.
+ */
+object MarketLkSupport extends SioController with PlayLazyMacroLogsImpl {
+
+  import LOGGER._
+
+
+  /** Маппинг для формы обращения в саппорт. */
+  private val supportFormM = {
+    import play.api.data._, Forms._
+    import util.FormUtil._
+    Form(tuple(
+      "phone" -> phoneOptM,
+      "email" -> optional(email),
+      "msg"   -> text2048M
+    ))
+  }
+
+
+  /** Отрендерить форму с запросом помощи.
+    * @param adnId id узла.
+    * @return 200 Ок и страница с формой.
+    */
+  def supportForm(adnId: String) = IsAdnNodeAdmin(adnId).async { implicit request =>
+    // TODO Брать дефолтовое значение email'а по сессии
+    val form = supportFormM
+    Ok(supportFormTpl(adnId, form))
+  }
+
+
+  /**
+   * Сабмит формы обращения за помощью.
+   * @param adnId id узла.
+   */
+  def supportFormSubmit(adnId: String) = IsAdnNodeAdmin(adnId).async { implicit request =>
+    ???
+  }
+
+}
