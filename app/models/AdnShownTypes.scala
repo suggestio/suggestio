bb3961c8b9ffddc515bf77d887eb6767a31826fb
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2014-09-05 13:28:49 +0400

    _advFormTpl: не рендерить название города, если узел сам является городом.

--- app/models/AdnShownTypes.scala
+++ app/models/AdnShownTypes.scala
@@ -8,23 +8,25 @@ package models
  */
 object AdnShownTypes extends Enumeration {
 
-  type AdnShownType = Value
+  protected sealed case class Val(name: String, showWithTown: Boolean = true) extends super.Val(name)
+
+  type AdnShownType = Val
 
   // Штатные (исторические) типы узлов
-  val MART: AdnShownType              = Value( AdNetMemberTypes.MART.name )
-  val SHOP: AdnShownType              = Value( AdNetMemberTypes.SHOP.name )
-  val RESTAURANT: AdnShownType        = Value( AdNetMemberTypes.RESTAURANT.name )
-  val RESTAURANT_SUP: AdnShownType    = Value( AdNetMemberTypes.RESTAURANT_SUP.name )
+  val MART: AdnShownType              = Val( AdNetMemberTypes.MART.name )
+  val SHOP: AdnShownType              = Val( AdNetMemberTypes.SHOP.name )
+  val RESTAURANT: AdnShownType        = Val( AdNetMemberTypes.RESTAURANT.name )
+  val RESTAURANT_SUP: AdnShownType    = Val( AdNetMemberTypes.RESTAURANT_SUP.name )
 
   // Пользовательские типы узлов. Для id'шников можно использовать идентификаторы, не использованные в вышеуказанных вещах.
   // При совпадении двух id будет ошибка после запуска при первом обращении к этой модели.
-  val TRANSPORT_NODE: AdnShownType    = Value("a")    // Вокзалы, аэропорты и др. более-менее крупные транспортные узлы.
-  val METRO_STATION: AdnShownType     = Value("b")    // Станция метро
-  val TOWN_DISTRICT: AdnShownType     = Value("c")    // Район города
-  val TOWN: AdnShownType              = Value("d")    // Город
+  val TRANSPORT_NODE: AdnShownType    = Val("a")    // Вокзалы, аэропорты и др. более-менее крупные транспортные узлы.
+  val METRO_STATION: AdnShownType     = Val("b")    // Станция метро
+  val TOWN_DISTRICT: AdnShownType     = Val("c")    // Район города
+  val TOWN: AdnShownType              = Val("d", showWithTown = false)    // Город
 
   // При добавлении новых элементов, нужно добавлять в conf/messages.* соответствующие "amt.of.type.X" и "amts.of.type.X".
-  def maybeWithName(n: String) = {
+  def maybeWithName(n: String): Option[AdnShownType] = {
     try {
       Some(withName(n))
     } catch {
@@ -32,4 +34,8 @@ object AdnShownTypes extends Enumeration {
     }
   }
 
+  implicit def value2val(x: Value): AdnShownType = x.asInstanceOf[AdnShownType]
+
+  implicit def adnInfo2val(adnInfo: AdNetMemberInfo): AdnShownType = shownTypeId2val(adnInfo.shownTypeId)
+  implicit def shownTypeId2val(sti: String): AdnShownType = withName(sti)
 }
