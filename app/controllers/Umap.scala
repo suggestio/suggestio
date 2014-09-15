5803b13fd296e4f710d115cd935d53c065b57eb3
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2014-09-15 14:56:01 +0400

    Umap-контроллер: экшен для набега на энтерпрайз

--- app/controllers/Umap.scala
+++ app/controllers/Umap.scala
@@ -5,6 +5,7 @@ import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import util.SiowebEsUtil.client
 import util.acl.IsSuperuser
 import views.html.umap._
+import play.api.libs.json._
 
 /**
  * Suggest.io
@@ -16,9 +17,38 @@ object Umap extends SioController with PlayMacroLogsImpl {
 
   import LOGGER._
 
-  /** Рендер карты с внутренней географией узлов. */
+  /** Рендер статической карты, которая запросит и отобразит географию узлов. */
   def getAdnNodesMap = IsSuperuser { implicit request =>
     Ok(mapBaseTpl())
+  }
+
+  def getDatalayerGeojson = IsSuperuser { implicit request =>
+    val feature1 = JsObject(Seq(
+      "geometry" -> JsObject(Seq(
+        "type" -> JsString("Polygon"),
+        "coordinates" -> JsArray(Seq(
+          JsArray(Seq(JsNumber( -4.6142578125), JsNumber(69.9679672584945))),
+          JsArray(Seq(JsNumber(2.6806640625), JsNumber(70.1552878601035))),
+          JsArray(Seq(JsNumber(1.2744140625), JsNumber(67.9581478610158))),
+          JsArray(Seq(JsNumber(-4.2626953125), JsNumber(68.00757101804))),
+          JsArray(Seq(JsNumber(-4.6142578125), JsNumber(69.9679672584945)))
+        ))
+      )),
+      "properties" -> JsObject(Seq.empty),
+      "type" -> JsString("feature")
+    ))
+    val features = JsArray(Seq(feature1))
+    val storage = JsObject(Seq(
+      "name" -> JsString("Узлы ADN"),
+      "displayOnLoad" -> JsBoolean(true),
+      "id" -> JsNumber(666)
+    ))
+    val resp = JsObject(Seq(
+      "type"      -> JsString("FeatureCollection"),
+      "_storage"  -> storage,
+      "features"  -> features
+    ))
+    Ok(resp)
   }
 
   def saveMapSettingsSubmit = IsSuperuser { implicit request =>
