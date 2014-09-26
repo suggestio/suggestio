6d4d51f03fed7516db9ecc4b0186719702165a0c
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2014-09-26 14:08:38 +0400

    SvgUtilSpec: Использовать PlaySpec вместо толстой FlatSpec.

--- test/util/img/SvgUtilSpec.scala
+++ test/util/img/SvgUtilSpec.scala
@@ -1,6 +1,8 @@
 package util.img
 
-import org.scalatest._
+import java.io.File
+
+import org.scalatestplus.play._
 
 /**
  * Suggest.io
@@ -8,7 +10,7 @@ import org.scalatest._
  * Created: 26.09.14 13:37
  * Description: Тесты для SvgUtil.
  */
-class SvgUtilSpec extends FlatSpec with Matchers {
+class SvgUtilSpec extends PlaySpec {
 
   private val TEST_RSC_DIR = "/util/img/svg_util"
 
@@ -16,43 +18,53 @@ class SvgUtilSpec extends FlatSpec with Matchers {
     testFilePath(s"$TEST_RSC_DIR/$filename", isSvg)
   }
 
+  /** Тестируем isSvgValid() и isSvgFileValid(). */
   private def testFilePath(filepath: String, isSvg: Boolean): Unit = {
     val is = getClass.getResourceAsStream(filepath)
     assert(is != null)
     try {
-      SvgUtil.isSvgValid(is)  shouldBe  isSvg
+      SvgUtil.isSvgValid(is)  mustBe  isSvg
     } finally {
       is.close()
     }
+
+    val fpath = getClass.getResource(filepath).getFile
+    val f = new File(fpath)
+    SvgUtil.isSvgFileValid(f) mustBe isSvg
   }
 
 
-  "isSvgValid()" should "see a valid idented svg file: valid_svg1_idented.svg" in {
-    testFile("valid_svg1_idented.svg", isSvg = true)
-  }
+  "isSvgValid()" must {
 
-  it should "accept shrinked version of previous file too (valid_svg1_shrinked.svg)" in {
-    testFile("valid_svg1_shrinked.svg", isSvg = true)
-  }
+    "see a valid idented svg file: valid_svg1_idented.svg" in {
+      testFile("valid_svg1_idented.svg", isSvg = true)
+    }
 
-  it should "accept valid svg file: valid_svg2_shrinked.svg" in {
-    testFile("valid_svg2_shrinked.svg", isSvg = true)
-  }
+    "accept shrinked version of previous file too (valid_svg1_shrinked.svg)" in {
+      testFile("valid_svg1_shrinked.svg", isSvg = true)
+    }
 
-  it should "refuse text file (lorem_ipsum.txt)" in {
-    testFile("lorem_ipsum.txt", isSvg = false)
-  }
+    "accept valid svg file: valid_svg2_shrinked.svg" in {
+      testFile("valid_svg2_shrinked.svg", isSvg = true)
+    }
 
-  it should "refuse raster graphics (1214545755807.jpg)" in {
-    testFilePath("/util/img/1214545755807.jpg", isSvg = false)
-  }
+    "refuse text file (lorem_ipsum.txt)" in {
+      testFile("lorem_ipsum.txt", isSvg = false)
+    }
 
-  it should "refuse invalid svg (invalid_svg1_idented.svg)" in {
-    testFile("invalid_svg1_idented.svg", isSvg = false)
-  }
+    "refuse raster graphics (1214545755807.jpg)" in {
+      testFilePath("/util/img/1214545755807.jpg", isSvg = false)
+    }
 
-  it should "refuse valid xml non-svg file (uca.xml)" in {
-    testFile("uca.xml", isSvg = false)
-  }
+    "refuse invalid svg (invalid_svg1_idented.svg)" in {
+      testFile("invalid_svg1_idented.svg", isSvg = false)
+    }
+
+    "refuse valid xml non-svg file (uca.xml)" in {
+      testFile("uca.xml", isSvg = false)
+    }
+
+  } // isSvgValid()
+
 
 }
