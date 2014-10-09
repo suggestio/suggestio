b119dd3a44111ca10e19a9fff663393b0e9dcb8b
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2014-10-09 11:42:13 +0400

    DevPixelRatios: внутреннее разделение bg и fg настроек сжатия, чтение этих настроек из конфига.

--- app/models/im/DevPixelRatios.scala
+++ app/models/im/DevPixelRatios.scala
@@ -1,43 +1,15 @@
 package models.im
 
+import play.api.Play.{current, configuration}
+
 /**
  * Suggest.io
  * User: Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
  * Created: 06.10.14 13:51
- * Description: Модель, хранящая стандартные пиксельны плотности экранов.
+ * Description: Модель, хранящая стандартные пиксельны плотности экранов для разных режимов картинки.
  * @see [[http://www.devicepixelratio.com/]].
  */
 object DevPixelRatios extends Enumeration {
-
-  private val Some005: Option[Float] = Some(0.05F)
-
-  /**
-   * Настройки сжатия.
-   * @param imQualityInt quality (0..100). Основной параметр для jpeg'ов.
-   * @param chromaSubSampling Цветовая субдискретизация. 2x2 по дефолту.
-   * @param imBlur Желаемая размывка.
-   */
-  sealed case class ImCompression(
-    imQualityInt      : Int,
-    chromaSubSampling : ImSamplingFactor = ImSamplingFactors.SF_2x2,
-    imBlur            : Option[Float] = Some005
-  ) {
-
-    /** Значение quality. */
-    def imQuality = imQualityInt.toDouble
-
-    /** Сгенерить экземпляр quality ImOp. */
-    def imQualityOp = QualityOp(imQuality)
-
-    /** Опционально сгенерить GaussBlurOp, если требуется размывка. */
-    def imGaussBlurOp: Option[GaussBlurOp] = {
-      imBlur.map { blurFloat =>
-        GaussBlurOp(blurFloat)
-      }
-    }
-
-  }
-
 
   /**
    * Экземпляр модели.
@@ -46,8 +18,8 @@ object DevPixelRatios extends Enumeration {
   protected abstract sealed class Val(val name: String) extends super.Val(name) {
     def pixelRatio: Float
 
-    def bgCompression: ImCompression
-    def fgCompression: ImCompression = bgCompression
+    val bgCompression: ImCompression
+    val fgCompression: ImCompression
   }
 
 
@@ -57,35 +29,70 @@ object DevPixelRatios extends Enumeration {
   /** Единичная плотность пикселей. Некогда (до середины 2013 года) самые дефолтовые девайсы. */
   val MDPI: DevPixelRatio = new Val("MDPI") {
     override def pixelRatio: Float = 1.0F
+
+    // Используется SF_1x1 (т.е. откл.), иначе на контрастных переходах появляются заметные "тучи" на монотонных кусках.
     override val bgCompression = ImCompression(
-      imQualityInt = 75,
-      chromaSubSampling = ImSamplingFactors.SF_2x1
+      name = name,
+      mode = "bg",
+      qDflt = 75,
+      chromaSsDflt = ImSamplingFactors.SF_1x1
+    )
+    override val fgCompression = ImCompression(
+      name = name,
+      mode = "fg",
+      qDflt = 89,
+      chromaSsDflt = ImSamplingFactors.SF_1x1
     )
   }
 
+
   // DPR=1.3 слишком дробное, и не нашло популярности на рынке устройств.
+
 
   /** Только на андройдах есть такое. */
   val HDPI: DevPixelRatio = new Val("HDPI") {
     override def pixelRatio: Float = 1.5F
     override val bgCompression = ImCompression(
-      imQualityInt = 70
+      name = name,
+      mode = "bg",
+      qDflt = 70
+    )
+    override val fgCompression = ImCompression(
+      name = name,
+      mode = "fg",
+      qDflt = 75
     )
   }
+
 
   /** Андройд-девайсы и т.ч. retina, т.е. iphone4+ и прочие яблодевайсы после 2013 г. */
   val XHDPI: DevPixelRatio = new Val("XHDPI") {
     override def pixelRatio: Float = 2.0F
     override val bgCompression = ImCompression(
-      imQualityInt = 65
+      name = name,
+      mode = "bg",
+      qDflt = 64
+    )
+    override val fgCompression = ImCompression(
+      name = name,
+      mode = "fg",
+      qDflt = 70
     )
   }
+
 
   /** На середину 2014 года, это только топовые андройды. Разрешение экрана соотвествует HD1080. */
   val DPR3: DevPixelRatio = new Val("DPR3") {
     override def pixelRatio: Float = 3.0F
     override val bgCompression = ImCompression(
-      imQualityInt = 55
+      name = name,
+      mode = "bg",
+      qDflt = 55
+    )
+    override val fgCompression = ImCompression(
+      name = name,
+      mode = "fg",
+      qDflt = 65
     )
   }
 
@@ -104,9 +111,64 @@ object DevPixelRatios extends Enumeration {
     values
       .find { v =>
         val dpi: DevPixelRatio = v
-        (dpi.pixelRatio >= ratio) || (dpi.pixelRatio * 1.05 > ratio)
+        (dpi.pixelRatio >= ratio) || (dpi.pixelRatio * 1.1 > ratio)
       }
       .fold [DevPixelRatio] { values.last } { v => v }
   }
 
 }
+
+
+object ImCompression {
+
+  /**
+   * Сборка экземпляра ImCompression с использованием конфига.
+   * @param name Название, например MDPI.
+   * @param mode Режим: fg, bg или иные.
+   * @param qDflt Дефолтовое значение качества.
+   * @param chromaSsDflt Дефолтовая передискретизация цветов [2x2].
+   * @param imBlurDflt Дефолтованая размывка, если требуется [None].
+   * @return Экземпляр ImCompression, готовый к эксплуатации.
+   */
+  def apply(name: String, mode: String, qDflt: Int, chromaSsDflt: ImSamplingFactor = ImSamplingFactors.SF_2x2,
+            imBlurDflt: Option[Float] = None): ImCompression = {
+    ImCompression(
+      imQualityInt      = configuration.getInt(s"dpr.$name.$mode.quality")
+        .getOrElse(qDflt),
+      chromaSubSampling = configuration.getString(s"dpr.$name.$mode.chroma.ss")
+        .fold(chromaSsDflt)(ImSamplingFactors.withName),
+      imBlur            = configuration.getDouble(s"dpr.$name.$mode.blur.gauss")
+        .map(_.toFloat)
+    )
+  }
+
+}
+
+
+/**
+ * Настройки сжатия.
+ * @param imQualityInt quality (0..100). Основной параметр для jpeg'ов.
+ * @param chromaSubSampling Цветовая субдискретизация. 2x2 по дефолту.
+ * @param imBlur Желаемая размывка.
+ */
+sealed case class ImCompression(
+  imQualityInt      : Int,
+  chromaSubSampling : ImSamplingFactor,
+  imBlur            : Option[Float]
+) {
+
+  /** Значение quality. */
+  def imQuality = imQualityInt.toDouble
+
+  /** Сгенерить экземпляр quality ImOp. */
+  def imQualityOp = QualityOp(imQuality)
+
+  /** Опционально сгенерить GaussBlurOp, если требуется размывка. */
+  def imGaussBlurOp: Option[GaussBlurOp] = {
+    imBlur.map { blurFloat =>
+      GaussBlurOp(blurFloat)
+    }
+  }
+
+}
+
