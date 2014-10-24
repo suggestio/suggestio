89a66524ff17dce8e8113afd91b42feb18f1ef1a
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2014-10-24 10:33:08 +0400

    Удаление страниц /help и /help/registration.

--- app/controllers/Static.scala
+++ app/controllers/Static.scala
@@ -17,16 +17,16 @@ import Application.http404AdHoc
 
 object Static extends Controller with ContextT {
 
-  private def rdr2booklet = MovedPermanently( routes.Market.marketBooklet().url )
+  private def booklet = routes.Market.marketBooklet().url
 
   /** Страница /about. Раньше там были слайд-презентация s.io live search. */
   def about = Action { implicit request =>
-    rdr2booklet
+    MovedPermanently(booklet)
   }
 
   /** Страница /showcase. Там была плитка из превьюшек сайтов, которые используют s.io live search. */
   def showcase = Action { implicit request =>
-    rdr2booklet
+    MovedPermanently(booklet)
   }
 
 
@@ -35,14 +35,15 @@ object Static extends Controller with ContextT {
   }
 
 
+  /** Страница /help. Пока редирект на буклет. Когда помощь появится, то её корень лучше всего сделать тут. */
   def help = MaybeAuth { implicit request =>
-    Ok(indexTpl())
+    Redirect(booklet)
   }
 
 
   def helpPage(page:String) = MaybeAuth { implicit request =>
+    // 2014.oct.24: в sio1 live search тут были страницы: "registration", "search_settings", "images_settings", "design_settings", "setup".
     page match {
-      case "registration"     => Ok(registrationTpl())
       case "search_settings"  => Ok(searchSettingsTpl())
       case "images_settings"  => Ok(imagesSettingsTpl())
       case "design_settings"  => Ok(designSettingsTpl())
