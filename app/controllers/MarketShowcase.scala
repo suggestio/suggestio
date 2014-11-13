cc22df7b2bda7038af80584f40477b8e98d74448
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2014-11-13 17:59:08 +0300

    ctl.MarketShowcase.nodesList() унесён в sc.ScNodesList и распилен в трейт.

--- app/controllers/MarketShowcase.scala
+++ app/controllers/MarketShowcase.scala
@@ -1,20 +1,13 @@
 package controllers
 
-import java.util.NoSuchElementException
-
-import _root_.util.showcase._
 import controllers.sc._
 import io.suggest.event.SNStaticSubscriberDummy
 import util._
 import util.acl._
 import views.html.market.showcase._
-import play.api.libs.json._
-import play.api.libs.Jsonp
 import models._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import SiowebEsUtil.client
-import scala.concurrent.Future
-import play.api.Play, Play.{current, configuration}
 
 /**
  * Suggest.io
@@ -25,18 +18,12 @@ import play.api.Play, Play.{current, configuration}
  */
 object MarketShowcase extends SioController with PlayMacroLogsImpl with SNStaticSubscriberDummy
 with ScSiteNode with ScSiteGeo with ScNodeInfo with ScIndexGeo with ScIndexNode with ScSyncSiteGeo
-with ScAdsTile with ScFocusedAds
+with ScAdsTile with ScFocusedAds with ScNodesList
 {
 
   import LOGGER._
 
   val JSONP_CB_FUN = "siomart.receive_response"
-
-  /** Кеш ответа findNodes() на клиенте. Это существенно ускоряет навигацию. */
-  val FIND_NODES_CACHE_SECONDS: Int = configuration.getInt("market.showcase.nodes.find.result.cache.seconds") getOrElse {
-    if (Play.isProd)  120  else  10
-  }
-
 
 
   /**
@@ -58,75 +45,6 @@ with ScAdsTile with ScFocusedAds
         warn(s"AdId $adId not found.")
         http404AdHoc
     }
-  }
-
-
-  /** Поиск узлов в рекламной выдаче. */
-  def findNodes(args: SimpleNodesSearchArgs) = MaybeAuth.async { implicit request =>
-    lazy val logPrefix = s"findNodes(${System.currentTimeMillis}): "
-    trace(logPrefix + "Starting with args " + args + " ; remote = " + request.remoteAddress + " ; path = " + request.path + "?" + request.rawQueryString)
-    // Для возможной защиты криптографических функций, использующий random, округяем и загрубляем timestamp.
-    val tstamp = System.currentTimeMillis() / 50L
-
-    // Запуск детектора текущей ноды, если необходимо. Асинхронно возвращает (lvl, node) или экзепшен.
-    // Экзепшен означает, что переключение нод отключено или не удалось найти текущую ноду.
-    val gsiOptFut = args.geoMode.geoSearchInfoOpt
-    val nextNodeSwitchFut: Future[GeoDetectResult] = if (args.isNodeSwitch) {
-      ShowcaseNodeListUtil.detectCurrentNode(args.geoMode, gsiOptFut)
-    } else {
-      Future failed new NoSuchElementException("Node detect disabled")
-    }
-
-    // Нода, которая будет отображена как текущая при следующем набеге на выдачу.
-    val nextNodeFut = ShowcaseNodeListUtil.detectRecoverGuessCurrentNode(gsiOptFut, args.currAdnId)(nextNodeSwitchFut)
-
-    // Привести nextNodeFut к формату nextNodeSwitchFut.
-    val nextNodeWithLayerFut = ShowcaseNodeListUtil.nextNodeWithLvlOptFut(nextNodeSwitchFut, nextNodeFut)
-
-    // Когда все данные будут собраны, нужно отрендерить результат в виде json.
-    val resultFut = for {
-      nextNodeGdr <- nextNodeWithLayerFut
-      nodesLays5  <- ShowcaseNodeListUtil.collectLayers(args.geoMode, nextNodeGdr.node, nextNodeGdr.ngl)
-    } yield {
-      import nextNodeGdr.{node => nextNode}
-      // Рендер в json следующего узла, если он есть.
-      val nextNodeJson = JsObject(Seq(
-        "name"  -> JsString(nextNode.meta.name),
-        "_id"   -> JsString(nextNode.id getOrElse "")
-      ))
-      // Список узлов, который надо рендерить юзеру.
-      val nodesRendered: Seq[GeoNodesLayer] = if (args.isNodeSwitch && nodesLays5.nonEmpty) {
-        // При переключении узла, переключение идёт на наиболее подходящий узел, который первый в списке.
-        // Тогда этот узел НЕ надо отображать в списке узлов.
-        val nodes0 = nodesLays5.head.nodes
-        nodesLays5.head.copy(nodes = nodes0.tail) :: nodesLays5.toList.tail
-      } else {
-        // Нет переключения узлов. Рендерим все подходящие узлы.
-        nodesLays5
-      }
-      val json = JsObject(Seq(
-        "action"      -> JsString("findNodes"),
-        "status"      -> JsString("ok"),
-        "first_node"  -> nextNodeJson,
-        "nodes"       -> _geoNodesListTpl(nodesRendered, Some(nextNode)),
-        "timestamp"   -> JsNumber(tstamp)
-      ))
-      // Без кеша, ибо timestamp.
-      Ok( Jsonp(JSONP_CB_FUN, json) )
-        .withHeaders(
-          CACHE_CONTROL -> s"public, max-age=$FIND_NODES_CACHE_SECONDS"
-        )
-    }
-
-    // Одновременно собираем статистику по запросу:
-    ScNodeListingStat(args, gsiOptFut)
-      .saveStats
-      .onFailure { case ex =>
-        warn(logPrefix + "Failed to save stats", ex)
-      }
-
-    // Вернуть асинхронный результат.
-    resultFut
   }
 
 }
---
+++ app/controllers/sc/ScNodesList.scala
@@ -0,0 +1,145 @@
+package controllers.sc
+
+import java.util.NoSuchElementException
+import play.twirl.api.Html
+import util.PlayMacroLogsI
+import util.SiowebEsUtil.client
+import _root_.util.showcase._
+import util.acl._
+import views.html.market.showcase._
+import play.api.libs.json._
+import play.api.libs.Jsonp
+import models._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import scala.concurrent.Future
+import play.api.Play, Play.{current, configuration}
+
+/**
+ * Suggest.io
+ * User: Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
+ * Created: 13.11.14 18:10
+ * Description: Поддержка экшена доступа к списку узлов и логики генерации этого списка.
+ */
+trait ScNodesList extends ScController with PlayMacroLogsI {
+
+  /** Кеш ответа findNodes() на клиенте. Это существенно ускоряет навигацию. */
+  protected val FIND_NODES_CACHE_SECONDS: Int = {
+    configuration.getInt("market.showcase.nodes.find.result.cache.seconds") getOrElse {
+      if (Play.isProd)  120  else  10
+    }
+  }
+
+
+  /** Поиск узлов в рекламной выдаче. */
+  def findNodes(args: SimpleNodesSearchArgs) = MaybeAuth.async { implicit request =>
+    // Для возможной защиты криптографических функций, использующий random, округяем и загрубляем timestamp.
+    val tstamp = System.currentTimeMillis() / 50L
+
+    lazy val logPrefix = s"findNodes(${System.currentTimeMillis}): "
+    LOGGER.trace(logPrefix + "Starting with args " + args + " ; remote = " + request.remoteAddress + " ; path = " + request.path + "?" + request.rawQueryString)
+
+    val logic = new FindNodesLogic {
+      override def _request = request
+      override def _nsArgs = args
+    }
+
+    // Запускаем получение  результатов из nodelist-логики
+    val renderedFut = logic.nodesListRenderedFut
+    val nextNodeJsonFut = logic.nextNodeWithLayerFut map { nextNodeGdr =>
+      val nn = nextNodeGdr.node
+      JsObject(Seq(
+        "name"  -> JsString(nn.meta.name),
+        "_id"   -> JsString(nn.id getOrElse "")
+      ))
+    }
+
+    // заворачиваем в json результаты работы логики.
+    val resultFut = for {
+      nextNodeJson <- nextNodeJsonFut
+      rendered     <- renderedFut
+    } yield {
+      val json = JsObject(Seq(
+        "action"      -> JsString("findNodes"),
+        "status"      -> JsString("ok"),
+        "first_node"  -> nextNodeJson,
+        "nodes"       -> rendered,
+        "timestamp"   -> JsNumber(tstamp)
+      ))
+      // Без кеша, ибо timestamp.
+      Ok( Jsonp(JSONP_CB_FUN, json) )
+        .withHeaders(
+          CACHE_CONTROL -> s"public, max-age=$FIND_NODES_CACHE_SECONDS"
+        )
+    }
+
+    // Одновременно собираем статистику по текущему запросу:
+    ScNodeListingStat(args, logic.gsiOptFut)
+      .saveStats
+      .onFailure { case ex =>
+        LOGGER.warn("Failed to save stats", ex)
+      }
+
+    // Вернуть асинхронный результат.
+    resultFut
+  }
+
+
+
+  /** Гибкая логика обработки запроса сбора списка узлов. */
+  trait FindNodesLogic {
+
+    def _nsArgs: SimpleNodesSearchArgs
+    implicit def _request: AbstractRequestWithPwOpt[_]
+
+    val gsiOptFut = _nsArgs.geoMode.geoSearchInfoOpt
+
+    // Запуск детектора текущей ноды, если необходимо. Асинхронно возвращает (lvl, node) или экзепшен.
+    // Экзепшен означает, что переключение нод отключено или не удалось найти текущую ноду.
+    lazy val nextNodeSwitchFut: Future[GeoDetectResult] = if (_nsArgs.isNodeSwitch) {
+      ShowcaseNodeListUtil.detectCurrentNode(_nsArgs.geoMode, gsiOptFut)
+    } else {
+      Future failed new NoSuchElementException("Node detect disabled")
+    }
+
+    /** Нода, которая будет отображена как текущая на грядущем шаге. */
+    def nextNodeFut = ShowcaseNodeListUtil.detectRecoverGuessCurrentNode(gsiOptFut, _nsArgs.currAdnId)(nextNodeSwitchFut)
+
+    /** Привести nextNodeFut к формату nextNodeSwitchFut. */
+    lazy val nextNodeWithLayerFut = ShowcaseNodeListUtil.nextNodeWithLvlOptFut(nextNodeSwitchFut, nextNodeFut)
+
+    /** Сырые навигационные слои узлов. */
+    def nglsFut: Future[Seq[GeoNodesLayer]] = {
+      nextNodeWithLayerFut.flatMap { nextNodeGdr =>
+        ShowcaseNodeListUtil.collectLayers(_nsArgs.geoMode, nextNodeGdr.node, nextNodeGdr.ngl)
+      }
+    }
+
+    /** Почищенные навигационные слои узлов, которые нужно отрендерить юзеру. */
+    def gnls4RenderFut: Future[Seq[GeoNodesLayer]] = {
+      nglsFut map { nodesLays5 =>
+        if (_nsArgs.isNodeSwitch && nodesLays5.nonEmpty) {
+          // При переключении узла, переключение идёт на наиболее подходящий узел, который первый в списке.
+          // Тогда этот узел НЕ надо отображать в списке узлов.
+          val nodes0 = nodesLays5.head.nodes
+          nodesLays5.head.copy(nodes = nodes0.tail) :: nodesLays5.toList.tail
+        } else {
+          // Нет переключения узлов. Рендерим все подходящие узлы.
+          nodesLays5
+        }
+      }
+    }
+
+    /** Асинхронный результат рендера списка узлов. */
+    def nodesListRenderedFut: Future[Html] = {
+      val _gnls4RenderFut = gnls4RenderFut
+      for {
+        nextNodeGdr <- nextNodeWithLayerFut
+        gnls4Render <- _gnls4RenderFut
+      } yield {
+        _geoNodesListTpl(gnls4Render, Some(nextNodeGdr.node))
+      }
+    }
+
+  }
+
+}
