7d7487fad6a26431c924ded6187618343b02fecb
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-02-03 15:22:50 +0300

    grunt clean dist должен также удалять ui/twirl/*

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -150,7 +150,8 @@ module.exports = function (grunt) {
           src: [
             '.tmp',
             '<%= yeoman.dist %>/**/*',
-            '!<%= yeoman.dist %>/.git{,*/}*'
+            '!<%= yeoman.dist %>/.git{,*/}*',
+	    '<%= yeoman.dist %>/../twirl/**/*'
           ]
         }]
       },
