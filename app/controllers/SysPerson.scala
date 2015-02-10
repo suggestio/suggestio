a1134fe9faf75a2a3f0d35df30414839279968e5
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-02-10 18:32:55 +0300

    sys/person: начато движение в сторону насущного раздела про пользователей s.io Там пока ссылки из предыдущего коммита: на email-сообщения реги и восстановления пароля.

--- app/controllers/SysPerson.scala
+++ app/controllers/SysPerson.scala
@@ -5,6 +5,7 @@ import util.PlayMacroLogsImpl
 import util.acl.IsSuperuser
 import views.html.ident.reg.email.emailRegMsgTpl
 import views.html.ident.recover.emailPwRecoverTpl
+import views.html.sys1.person._
 
 /**
  * Suggest.io
@@ -16,12 +17,19 @@ object SysPerson extends SioController with PlayMacroLogsImpl {
 
   import LOGGER._
 
+  /** Генерация экземпляра EmailActivation с бессмысленными данными. */
   private def dummyEa = EmailActivation(email = "admin@suggest.io", key = "keyKeyKeyKeyKey", id = Some("IdIdIdIdId888"))
 
+  def index = IsSuperuser { implicit request =>
+    Ok(indexTpl())
+  }
+
+  /** Отрендерить на экран email-сообщение регистрации юзера. */
   def showRegEmail = IsSuperuser { implicit request =>
     Ok(emailRegMsgTpl(dummyEa))
   }
 
+  /** Отрендерить email-сообщение восстановления пароля. */
   def showRecoverEmail = IsSuperuser { implicit request =>
     Ok(emailPwRecoverTpl(dummyEa))
   }
