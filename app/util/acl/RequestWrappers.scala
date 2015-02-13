112d88c6df4aa7ad6c85fb6b48e59626111e6b2c
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-02-13 17:07:26 +0300

    RequestWrappers: откат улучшения CSRF и кеширования.

--- app/util/acl/RequestWrappers.scala
+++ app/util/acl/RequestWrappers.scala
@@ -258,14 +258,14 @@ case class RequestHeaderAsRequest(underlying: RequestHeader) extends Request[Not
 /** Аддон для action-builder'ов, добавляющий выставление CSRF-токена в сессию. */
 trait CsrfGet[R[_]] extends ActionBuilder[R] {
 
-  abstract override def invokeBlock[A](request: Request[A], block: (R[A]) => Future[Result]): Future[Result] = {
+  /*abstract override def invokeBlock[A](request: Request[A], block: (R[A]) => Future[Result]): Future[Result] = {
     super.invokeBlock(request, block).map { result =>
       result.withHeaders(
         HeaderNames.CACHE_CONTROL -> "private, max-age=1, must-revalidate",
         HeaderNames.VARY          -> "Set-Cookie,Cookie"
       )
     }
-  }
+  }*/
 
   override protected def composeAction[A](action: Action[A]): Action[A] = {
     CSRFAddToken( super.composeAction(action) )
@@ -275,13 +275,13 @@ trait CsrfGet[R[_]] extends ActionBuilder[R] {
 /** Аддон для action-builder'ов, добавляющий проверку CSRF-токена перед запуском экшена на исполнение. */
 trait CsrfPost[R[_]] extends ActionBuilder[R] {
 
-  abstract override def invokeBlock[A](request: Request[A], block: (R[A]) => Future[Result]): Future[Result] = {
+  /*abstract override def invokeBlock[A](request: Request[A], block: (R[A]) => Future[Result]): Future[Result] = {
     super.invokeBlock(request, block).map { result =>
       result.withHeaders(
         HeaderNames.CACHE_CONTROL -> "private, no-cache"
       )
     }
-  }
+  }*/
 
   override protected def composeAction[A](action: Action[A]): Action[A] = {
     CSRFCheck( super.composeAction(action) )
