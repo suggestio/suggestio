6494508921d37697cbd9423bad5cc911ee17da0b
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-02-16 16:45:59 +0300

    Начато выпиливание контракта sio-market.

--- app/controllers/Market.scala
+++ app/controllers/Market.scala
@@ -19,32 +19,6 @@ import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 object Market extends SioController with SiteMapXmlCtl {
 
-  /**
-   * Раздача страниц с текстами договоров sio-market.
-   * @param clang Язык договора.
-   * @return 200 Ok
-   *         404 если текст договора не доступен на указанном языке.
-   */
-  def contractOfferText(clang: String) = MaybeAuth { implicit request =>
-    import views.html.market.contract._
-    val clangNorm = clang.toLowerCase.trim
-    val ctx = implicitly[Context]
-    val textRenderOpt: Option[(Html, String)] = if (clangNorm startsWith "ru") {
-      val render = textRuTpl()(ctx)
-      Some(render -> "ru")
-    } else {
-      None
-    }
-    textRenderOpt match {
-      case Some((render, clang2)) =>
-        val fullRender = contractBase(clang2)(render)(ctx)
-        Ok(fullRender)
-      case None =>
-        http404ctx(ctx)
-    }
-  }
-
-
   /** Статическая страничка, описывающая суть sio market для владельцев WiFi. */
   def aboutMarket = MaybeAuth { implicit request =>
     // 2014.oct.22: Удаление старого about'а, который уже постарел совсем и потерял актуальность.
