55b6e3a2abc00e2e5409076689434f1f84a7c956
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-02-17 10:55:02 +0300

    marketBase и marketBookletTpl перемещены под контроль grunt'а. MAdContentCtx: дефолтовая длина дескрипшена увеличена в 3 раза.

--- app/models/adv/js/ctx/MAdContentCtx.scala
+++ app/models/adv/js/ctx/MAdContentCtx.scala
@@ -20,7 +20,7 @@ object MAdContentCtx {
   val DESCR_FN        = "descr"
 
   /** Желаемая длина дескрипшена, отправляемого в контекст. */
-  val FROM_AD_DESCR_LEN = configuration.getInt("adv.ext.ad.content.descr.len") getOrElse 64
+  val FROM_AD_DESCR_LEN = configuration.getInt("adv.ext.ad.content.descr.len") getOrElse 192
 
   /** Делаем из инстанса рекламной карточки класс, пригодный для js-контекста.
     * @param mad Рекламная карточка.
