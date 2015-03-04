74e4d483cbaab528992a1317360cfec1e17635f9
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-03-04 17:08:32 +0300

    PopupChecker: реагировать на отложенное закрытие попапа.

--- src/main/scala/io/suggest/xadv/ext/js/runner/c/PopupChecker.scala
+++ src/main/scala/io/suggest/xadv/ext/js/runner/c/PopupChecker.scala
@@ -1,6 +1,9 @@
 package io.suggest.xadv.ext.js.runner.c
 
 import org.scalajs.dom
+import org.scalajs.dom.raw.Window
+
+import scala.scalajs.js
 
 /**
  * Suggest.io
@@ -13,8 +16,9 @@ object PopupChecker {
   /** Синхронная проверка на способность браузера не блокировать окна. */
   def isPopupAvailable(): Boolean = {
     // Запуск проверки того, открылось ли окно на деле. http://stackoverflow.com/a/27725432
+    var popup: Window = null
     try {
-      val popup = dom.window.open(
+      popup = dom.window.open(
         url       = routes.controllers.Static.popupCheckContent().url,
         target    = "sio-popup-test",
         features  = "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, copyhistory=yes, width=400, height=300"
@@ -25,7 +29,15 @@ object PopupChecker {
 
     } catch {
       case ex: Throwable =>
-        dom.console.error("Popup windows are unavailable: %s: %s", ex.getClass.getSimpleName, ex.getMessage)
+        dom.window.addEventListener("message", {evt: dom.MessageEvent =>
+          dom.console.warn("dom msg received: %s", evt)
+          if (evt.data == ("reloaded" : js.Any)) {
+            // TODO Нужно реагировать на разблокировку попапов: рестартовать текущий процесс БЕЗ перезагрузки страницы.
+            dom.document.location.reload()
+          }
+        }, false)
+        // Надо попробовать задействовать https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage
+        dom.console.error("Popup window are unavailable: %s: %s", ex.getClass.getSimpleName, ex.getMessage)
         false
     }
   }
