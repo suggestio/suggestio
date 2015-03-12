ab47504a299490322a8c229a068788b5998a9f06
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-03-12 17:16:55 +0300

    ext adv: Запрет wide-постинга карточек, если wide-флаг не выставлен.

--- app/util/adv/ExtTargetActor.scala
+++ app/util/adv/ExtTargetActor.scala
@@ -98,8 +98,10 @@ case class ExtTargetActor(args: IExtAdvTargetActorArgs)
     val szMult1 = hDiff * service.szMult
     // Вычислить необходимость и ширину широкого отображения.
     val wideWidthOpt = service.advPostMaxSz
+      .filter { _ => mad.blockMeta.wide }
       .map(_.width)
-      .filter { pmWidth =>  mad.blockMeta.wide || pmWidth.toFloat > mad.blockMeta.width * 1.1F }
+      // Заменено на упрощенный фильтр, указанный выше.
+      //.filter { pmWidth => mad.blockMeta.wide || pmWidth.toFloat > mad.blockMeta.width * 1.1F }
     PicInfo(
       wide   = wideWidthOpt.map { OneAdWideQsArgs.apply },
       width  = wideWidthOpt.getOrElse((mad.blockMeta.width * szMult1).toInt),
