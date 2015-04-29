74c2b4e8c817a75f9fc74cf59e8072658947f6ca
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-04-29 18:21:16 +0300

    VerticalLines: вынос констант в common.

--- src/main/scala/io/suggest/sjs/common/controller/vlines/VerticalLines.scala
+++ src/main/scala/io/suggest/sjs/common/controller/vlines/VerticalLines.scala
@@ -1,6 +1,7 @@
 package io.suggest.sjs.common.controller.vlines
 
 import io.suggest.sjs.common.controller.InitRouter
+import io.suggest.vlines.VLines._
 import org.scalajs.dom.Element
 import org.scalajs.jquery.{JQuery, jQuery}
 import scala.scalajs.concurrent.JSExecutionContext.Implicits.runNow
@@ -17,9 +18,7 @@ import scala.concurrent.Future
 
 object VerticalLines {
 
-  def JSVL_CSS_SELECTOR = ".js-vertical-line"
-
-  def INHERNIT_HEIGHT_ATTR = "inherit-height"
+  def JSVL_CSS_SELECTOR = "." + JSVL_CLASS
 
   def resetVLinesHeightsIn(parent: JQuery): Unit = {
     val sel = parent.find(JSVL_CSS_SELECTOR)
@@ -32,12 +31,13 @@ object VerticalLines {
   }
 
   def resetVLinesHeight(lines: JQuery): Unit = {
+    val v = INHERNIT_HEIGHT_ENABLED
     lines.each { (index: Any, el: Element) =>
       val jqel = jQuery(el)
       var h = jqel.parent().height()
       // Если не выставлен data-inherit-height, то надо немного уменьшить высоту.
-      val inhOpt = Option( jqel.data(INHERNIT_HEIGHT_ATTR) )
-      if ( !inhOpt.contains(true.toString) )
+      val inhOpt = Option( jqel.data(INHERNIT_HEIGHT_DATA_ATTR) )
+      if ( !inhOpt.contains(v) )
         h -= 10
       jqel.height(h)
     }
