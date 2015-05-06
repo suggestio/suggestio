609c6a247866e89a5ec87b635824070efe025324
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-05-06 19:11:56 +0300

    Продолжается пиление заливки фоток на новый js с исправленными архитектурными косяками. Происходит вынос дублирующихся данных в шаблоны.

--- src/main/scala/io/suggest/js/UploadConstants.scala
+++ src/main/scala/io/suggest/js/UploadConstants.scala
@@ -9,9 +9,15 @@ package io.suggest.js
 object UploadConstants {
 
   /** css-класс-пометка для input'а, занимающегося загрузкой файлов. */
-  def JS_FILE_UPLOAD_CLASS  = "js-file-upload"
+  def JS_FILE_UPLOAD_CLASS        = "js-file-upload"
 
   /** css-класс для контейнеров загрузки. */
-  def JS_IMG_UPLOAD_CLASS   = "js-image-upload"
+  def JS_IMG_UPLOAD_CLASS         = "js-image-upload"
+
+  /** Название аттрибута, где лежит счетчик для множественной загрузки элементов. */
+  def ATTR_MULTI_INDEX_COUNTER    = "data-name-index"
+
+  /** Название параметра в qs ссылки, где содержится индекс для генерируемого имени поля формы. */
+  def NAME_INDEX_QS_NAME          = "_ni"
 
 }
