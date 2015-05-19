305e49be5ebaf5143b9cb4cb39338e8d52eacaf6
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-05-19 19:28:48 +0300

    Продолжается пиление выдачи на scala.js: - web21: ресурс для раздачи js-роутера выдачи. - common-sjs: CommonPage распилена на SafeDocument и саму CommonPage. - sc-sjs: запилена подготовки js-роутера, но пока никуда не подцеплена.

--- src/main/scala/io/suggest/xadv/ext/js/runner/c/adp/AsyncInitAdp.scala
+++ src/main/scala/io/suggest/xadv/ext/js/runner/c/adp/AsyncInitAdp.scala
@@ -1,6 +1,6 @@
 package io.suggest.xadv.ext.js.runner.c.adp
 
-import io.suggest.sjs.common.view.CommonPage
+import io.suggest.sjs.common.view.SafeDocument
 import io.suggest.xadv.ext.js.runner.c.IActionContext
 import io.suggest.xadv.ext.js.runner.m.ex.{UrlLoadTimeoutException, DomUpdateException, ApiInitException}
 import io.suggest.xadv.ext.js.runner.m.{MJsCtxT, IAdapter}
@@ -55,7 +55,9 @@ trait AsyncInitAdp extends IAdapter {
 
   /** Добавление указанного (обычно script) тега в DOM. */
   def appendScriptTag(tag: Element): Unit = {
-    CommonPage.body.appendChild(tag)
+    SafeDocument()
+      .body
+      .appendChild(tag)
   }
 
   /** Собрать и добавить тег скрипта в DOM. */
