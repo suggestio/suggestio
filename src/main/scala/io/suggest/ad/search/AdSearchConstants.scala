82c3e6dc036165bfb776662913d91e60abe0928a
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-05-26 12:25:58 +0300

    Подготовка к добалению поддержки списка findAds в sc-sjs. Рефакторинг в qsb, подготовка к запиливания js unbind для AdSearch.

---
+++ src/main/scala/io/suggest/ad/search/AdSearchConstants.scala
@@ -0,0 +1,23 @@
+package io.suggest.ad.search
+
+/**
+ * Suggest.io
+ * User: Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
+ * Created: 26.05.15 11:27
+ * Description: Констатны поиска рекламных карточек.
+ */
+object AdSearchConstants {
+
+  def PRODUSER_ID_FN     = "shopId"
+  def CAT_ID_FN          = "catId"
+  def LEVEL_ID_FN        = "level"
+  def FTS_QUERY_FN       = "q"
+  def RESULTS_LIMIT_FN   = "size"
+  def RESULTS_OFFSET_FN  = "offset"
+  def RECEIVER_ID_FN     = "rcvr"
+  def FIRST_AD_ID_FN     = "firstAdId"
+  def GENERATION_FN      = "gen"
+  def GEO_MODE_FN        = "geo"
+  def SCREEN_INFO_FN     = "screen"
+
+}
