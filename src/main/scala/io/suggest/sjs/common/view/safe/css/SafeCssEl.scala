87ea332fceb70fc864880cdece0d478534316a56
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-05-27 19:33:40 +0300

    Продолжается пиление grid ads в новой выдаче.

--- src/main/scala/io/suggest/sjs/common/view/safe/css/SafeCssEl.scala
+++ src/main/scala/io/suggest/sjs/common/view/safe/css/SafeCssEl.scala
@@ -20,20 +20,40 @@ trait SafeCssElT extends ISafe {
 
   override type T <: Element
 
-
-  protected def getCssClass(): Option[String] = {
-    Option( _underlying.getAttribute("class") )
+  protected def _subStrRegex(name: String): String = {
+    "(^|.*\\s)" + Pattern.quote(name) + "(\\s.*|$)"
   }
 
-  protected def _containsClassViaAttr(name: String, css0: Option[String] = getCssClass()): Boolean = {
+  protected def _containsClassViaAttr(name: String, css0: Option[String]): Boolean = {
     css0
       .filter { !_.isEmpty }
       .exists { css =>
-        val regex = "(^|.*\\s)" + Pattern.quote(name) + "(\\s.*|$)"
-        css.matches(regex)
+        css.matches( _subStrRegex(name) )
       }
   }
 
+  /** Вспомогательная подсистема для отработки разных вариантов.  */
+  protected trait Helper[T] {
+    /** Есть classList. */
+    def withClassList(classList: DOMTokenList): T
+
+    /** Нету classList, но возможно есть значение аттрибута. */
+    def withAttrValue(classAttr: Option[String]): T
+
+    def execute(): T = {
+      val el1 = SafeCssElStub(_underlying)
+      val cllOrUndef = el1.classList
+      if (cllOrUndef.isDefined) {
+        // Std-compilant browser
+        val cll = cllOrUndef.get
+        withClassList(cll)
+      } else {
+        // В обход через аттрибуты тега.
+        val attr = Option(_underlying.getAttribute("class"))
+        withAttrValue( attr )
+      }
+    }
+  }
 
   /**
    * Содержится ли указанный css-класс у текущего элемента?
@@ -42,48 +62,66 @@ trait SafeCssElT extends ISafe {
    *         Иначе false.
    */
   def containsClass(name: String): Boolean = {
-    val el1 = SafeCssElStub(_underlying)
-    val cllOrUndef = el1.classList
-    if (cllOrUndef.isDefined) {
-      // Std-compilant browser
-      val cll = cllOrUndef.get
-      cll.contains(name)
-    } else {
-      // В обход через аттрибуты тега.
-      _containsClassViaAttr(name)
+    // Собираем helper для проверки наличия класса.
+    val h = new Helper[Boolean] {
+      override def withClassList(classList: DOMTokenList): Boolean = {
+        classList contains name
+      }
+      override def withAttrValue(classAttr: Option[String]): Boolean = {
+        _containsClassViaAttr(name, classAttr)
+      }
     }
+    h.execute()
   }
+
 
   /**
    * Дописать css-класс, если указанный класс ещё не задан.
    * @param names Названия добавляемых классов.
    */
   def addClasses(names: String*): Unit = {
-    val el = _underlying
-    val el1 = SafeCssElStub(el)
-    val cllOrUndef = el1.classList
-    if (cllOrUndef.isDefined) {
-      // Стандартный браузер.
-      val cll = cllOrUndef.get
-      names.foreach { name =>
-        cll.add(name)
+    val h = new Helper[Unit] {
+      override def withClassList(classList: DOMTokenList): Unit = {
+        names.foreach { classList.add }
       }
-    } else {
-      // В обход через аттрибуты тега.
-      val oldCssOpt = getCssClass()
-      val iter = names.iterator
-        .filter { name =>
-          !_containsClassViaAttr(name, oldCssOpt)
+
+      override def withAttrValue(classAttr: Option[String]): Unit = {
+        val iter = names.iterator
+          .filter { name =>
+            !_containsClassViaAttr(name, classAttr)
+          }
+        if (iter.nonEmpty) {
+          val css2 = iter.mkString(" ")
+          val css1 = classAttr match {
+            case None       => css2
+            case Some(css0) => css0 + " " + css2
+          }
+          _underlying.setAttribute("class", css1)
         }
-      if (iter.nonEmpty) {
-        val css2 = iter.mkString(" ")
-        val css1 = oldCssOpt match {
-          case None       => css2
-          case Some(css0) => css0 + " " + css2
-        }
-        el.setAttribute("class", css1)
       }
     }
+    h.execute()
+  }
+
+  /**
+   * Удалить css-класс из текущего тега.
+   * @param name Название удаляемого класса.
+   */
+  def removeClass(name: String): Unit = {
+    val h = new Helper[Unit] {
+      override def withClassList(classList: DOMTokenList): Unit = {
+        classList.remove(name)
+      }
+
+      override def withAttrValue(classAttr: Option[String]): Unit = {
+        classAttr.foreach { classAttr1 =>
+          val re = _subStrRegex(classAttr1)
+          val v = classAttr1.replaceAll(re, " ")
+          _underlying.setAttribute("class", v)
+        }
+      }
+    }
+    h.execute()
   }
 
 }
