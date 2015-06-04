0cde8f0e3a76d1b70932702aa43f804a30cb480d
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-06-04 19:26:41 +0300

    Заимплеменчен конечный автомат выдачи и html5 history api. Немного продолжается пиление поисковой панели.

--- src/main/scala/io/suggest/sjs/common/view/safe/wnd/SafeWindow.scala
+++ src/main/scala/io/suggest/sjs/common/view/safe/wnd/SafeWindow.scala
@@ -1,7 +1,9 @@
 package io.suggest.sjs.common.view.safe.wnd
 
+import io.suggest.sjs.common.view.safe.evtg.SafeEventTargetT
 import io.suggest.sjs.common.view.safe.wnd.cs.SafeGetComputedStyleT
 import io.suggest.sjs.common.view.safe.wnd.dpr.SafeWndDpr
+import io.suggest.sjs.common.view.safe.wnd.hist.SafeHistoryApiT
 import org.scalajs.dom
 import org.scalajs.dom.Window
 
@@ -12,7 +14,7 @@ import org.scalajs.dom.Window
  * Description: Безопасный доступ к необязательным полям window, таким как devicePixelRatio.
  * @see [[http://habrahabr.ru/post/159419/ Расовое авторитетное мнение Мицгола о devicePixelRatio, например.]]
  */
-trait SafeWindowT extends SafeWndDpr with SafeGetComputedStyleT {
+trait SafeWindowT extends SafeWndDpr with SafeGetComputedStyleT with SafeHistoryApiT with SafeEventTargetT {
   override type T <: Window
 }
 
