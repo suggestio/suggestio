4cbbbdf3ef1ea5b53925d2a0c27d278ca04954ca
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-06-17 19:04:33 +0300

    Вынос API FsmActor в common. Начало запиливания FocVm в виде FSM. Толком ничего не сделано из-за слишком совещательного рабочего дня.

---
+++ src/main/scala/io/suggest/fsm/AbstractFsm.scala
@@ -0,0 +1,66 @@
+package io.suggest.fsm
+
+/**
+ * Suggest.io
+ * User: Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
+ * Created: 17.06.15 11:48
+ * Description: Абстрактное базовое API для ускорения написания FSM.
+ */
+
+trait AbstractFsm {
+
+  /** Тип обработчика приходящих событий. */
+  type Receive
+  
+  /** Текущее состояние. */
+  protected var _state: FsmState
+
+  /** Ресивер для всех состояний. */
+  protected def allStatesReceiver: Receive
+
+  /**
+   * Переключение на новое состояние. Старое состояние будет отброшено.
+   * @param nextState Новое состояние.
+   */
+  protected def become(nextState: FsmState): Unit = {
+    _state.beforeUnbecome()
+    _state = nextState
+    _installReceiver(_state.receiver)
+    _state.afterBecome()
+  }
+  
+  /** Выставление указанного ресивера в качестве обработчика событий. */
+  protected def _installReceiver(newReceiver: Receive): Unit
+
+  /** Ресивер, добавляемый к конец reveive() для всех состояний, чтобы выводить в логи сообщения,
+    * которые не были отработаны актором. */
+  protected def unexpectedReceiver: Receive
+
+  protected def combineReceivers(rcvrs: TraversableOnce[Receive]): Receive
+
+  /** Интерфейс одного состояния. */
+  protected trait FsmState {
+    def name = getClass.getSimpleName
+    def receiverPart: Receive
+    def superReceiver = allStatesReceiver
+    def receiver: Receive = combineReceivers(Seq(receiverPart, superReceiver, unexpectedReceiver))
+    override def toString: String = name
+
+    /** Действия, которые вызываются, когда это состояние выставлено в актор. */
+    def afterBecome() {}
+
+    /** Действия, которые вызываются, перед тем как это состояние слетает из актора. */
+    def beforeUnbecome() {}
+  }
+
+}
+
+
+object AbstractFsmUtil {
+
+  /** Собрать несколько partial-функций в одну. */
+  def combinePfs[A,B](pfs: TraversableOnce[PartialFunction[A, B]]): PartialFunction[A, B] = {
+    pfs.reduceLeft(_ orElse _)
+  }
+
+}
