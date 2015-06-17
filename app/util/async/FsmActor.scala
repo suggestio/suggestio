7610856cf652973eadcc934e347fd4eb675a92d7
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-06-17 19:04:33 +0300

    Вынос API FsmActor в common. Начало запиливания FocVm в виде FSM. Толком ничего не сделано из-за слишком совещательного рабочего дня.

--- app/util/async/FsmActor.scala
+++ app/util/async/FsmActor.scala
@@ -1,60 +1,38 @@
 package util.async
 import _root_.util.PlayMacroLogsI
 import akka.actor.Actor
+import io.suggest.fsm.{AbstractFsmUtil, AbstractFsm}
 
 /**
  * Suggest.io
  * User: Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
  * Created: 22.01.15 12:22
- * Description: FsmActor -- это трейт для akka.Actor'а, который позволяет быстро описывать акторы конечные автоматы,
- * переключающиеся между акторами-состояниями.
+ * Description: FsmActor -- это трейт для akka.Actor'а, который позволяет быстро описывать
+ * конечные автоматы на базе обычных akka-акторов.
  */
 
-trait FsmActor extends Actor with PlayMacroLogsI {
-
-  /** Текущее состояние. */
-  protected var _state: FsmState
-
-  /** Ресивер для всех состояний. */
-  def allStatesReceiver: Receive
+trait FsmActor extends Actor with PlayMacroLogsI with AbstractFsm {
 
   /**
    * Переключение на новое состояние. Старое состояние будет отброшено.
    * @param nextState Новое состояние.
    */
-  def become(nextState: FsmState): Unit = {
+  override protected def become(nextState: FsmState): Unit = {
     LOGGER.trace(s"become(): fsm mode switch ${_state} -> $nextState")
-    _state.beforeUnbecome()
-    _state = nextState
+    super.become(nextState)
+  }
+
+
+  /** Выставление указанного ресивера в качестве обработчика событий. */
+  override protected def _installReceiver(newReceiver: Receive): Unit = {
     context.become(_state.receiver, discardOld = true)
-    _state.afterBecome()
   }
 
   /** Ресивер, добавляемый к конец reveive() для всех состояний, чтобы выводить в логи сообщения,
     * которые не были отработаны актором. */
-  protected val unexpectedReceiver: Receive = {
+  override protected val unexpectedReceiver: Receive = {
     case other =>
       LOGGER.warn(s"${_state.name} Unexpected message dropped [${other.getClass.getName}]:\n  $other")
-  }
-
-  /** Интерфейс одного состояния. */
-  trait FsmState {
-    def name = getClass.getSimpleName
-    def receiverPart: Receive
-    def superReceiver = allStatesReceiver
-    def maybeSuperReceive(msg: Any): Unit = {
-      val sr = superReceiver
-      if (sr isDefinedAt msg)
-        sr(msg)
-    }
-    def receiver = receiverPart orElse superReceiver orElse unexpectedReceiver
-    override def toString: String = name
-
-    /** Действия, которые вызываются, когда это состояние выставлено в актор. */
-    def afterBecome() {}
-
-    /** Действия, которые вызываются, перед тем как это состояние слетает из актора. */
-    def beforeUnbecome() {}
   }
 
   /** Состояние-заглушка. Не делает ровным счётом ничего. */
@@ -67,6 +45,10 @@ trait FsmActor extends Actor with PlayMacroLogsI {
   protected def harakiri(): Unit = {
     LOGGER.trace("harakiri(). Last state was " + _state.name)
     context stop self
+  }
+
+  override protected def combineReceivers(rcvrs: TraversableOnce[Receive]): Receive = {
+    AbstractFsmUtil.combinePfs(rcvrs)
   }
 
 }
