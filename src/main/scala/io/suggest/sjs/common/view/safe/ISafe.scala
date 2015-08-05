01ad5cf4242d92244c7f8746907403d7dc8f315a
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-08-05 16:38:22 +0300

    web21: splash-логотип suggest.io заменен на isuggest sc-sjs: продолжается запиливание vm'ов панели поиска. Уже под хеш-теги, хотя это пока только в названиях.

--- src/main/scala/io/suggest/sjs/common/view/safe/ISafe.scala
+++ src/main/scala/io/suggest/sjs/common/view/safe/ISafe.scala
@@ -10,6 +10,21 @@ import scala.scalajs.js
  * Created: 25.05.15 14:23
  * Description: Заготовка для разработки совместимых safe-врапперов.
  */
+
+object ISafe {
+
+  /**
+   * Генератор анонимной фунции-экстрактора underlying-элемента.
+   * @tparam Tmin Минимальный возвращаемый тип underlying-элемента.
+   */
+  def extractorF[Tmin <: js.Object] = {
+    { el: ISafe { type T <: Tmin} =>
+      el._underlying
+    }
+  }
+
+}
+
 trait ISafe extends TypeT with IUnderlying {
 
   override type T <: js.Object
---
+++ src/main/scala/io/suggest/sjs/common/view/safe/display/SetDisplayEl.scala
@@ -0,0 +1,26 @@
+package io.suggest.sjs.common.view.safe.display
+
+import io.suggest.sjs.common.view.safe.ISafe
+import org.scalajs.dom.raw.HTMLElement
+
+/**
+ * Suggest.io
+ * User: Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
+ * Created: 05.08.15 11:27
+ * Description: Быстрый доступ к _underlying.style.display и значениям.
+ */
+trait SetDisplayEl extends ISafe {
+  
+  override type T <: HTMLElement
+  
+  protected def setDisplay(newDisplay: String): Unit = {
+    _underlying.style.display = newDisplay
+  }
+
+  /** Скрыть из отображения текущий DOM-элемент. */
+  protected def displayNone() = setDisplay("none")
+
+  /** Отобразить текущий DOM-элемент блочно. */
+  protected def displayBlock() = setDisplay("block")
+  
+}
