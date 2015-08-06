c0b8b5dc4358af565d340c68a87ac0e5338e7ee9
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-08-06 10:08:52 +0300

    ScIndexAdOpen: Исправлена ошибка с endless loop при обращении к карточке, у которой уже удалён продьюсер.

--- app/controllers/sc/ScIndexAdOpen.scala
+++ app/controllers/sc/ScIndexAdOpen.scala
@@ -1,8 +1,8 @@
 package controllers.sc
 
-import models.{GeoMode, MAdnNodeCache}
+import models.{MAdnNode, MAdnNodeCache}
 import models.im.DevScreen
-import models.msc.{RenderedAdBlock, MScApiVsn, ScReqArgsDflt, ScReqArgs}
+import models.msc.{MScApiVsn, ScReqArgsDflt, ScReqArgs}
 import play.api.mvc.Result
 import util.acl.AbstractRequestWithPwOpt
 import play.api.Play.{current, configuration}
@@ -53,7 +53,18 @@ trait ScIndexAdOpen extends ScFocusedAds with ScIndexNodeCommon {
       prodIdOpt.exists( logic.is3rdPartyProducer )
     }.flatMap { prodIdOpt =>
       // Для этого продьюсера нужно выполнить переброску в index.
-      _goToProducerIndex(prodIdOpt.get, logic)
+      val fut = MAdnNodeCache.maybeGetByIdCached(prodIdOpt)
+        .map { _.get }
+        .flatMap { producer => _goToProducerIndex(producer, logic) }
+      // Как выяснилось, бывают карточки-сироты (продьюсер удален, карточка -- нет). Нужно сообщать об этой ошибке.
+      fut onFailure { case ex =>
+        val msg = s"Producer node does not exist: adnId=$prodIdOpt for adIds=${logic._adSearch.forceFirstIds}"
+        if (ex.isInstanceOf[NoSuchElementException])
+          LOGGER.error(msg)
+        else
+          LOGGER.error(msg, ex)
+      }
+      fut
     }.recoverWith { case ex: Throwable =>
       // Продьюсер неизвестен, не подходит под критерии или переброска отключена.
       if (!ex.isInstanceOf[NoSuchElementException])
@@ -66,18 +77,16 @@ trait ScIndexAdOpen extends ScFocusedAds with ScIndexNodeCommon {
   /**
    * Решено, что юзера нужно перебросить на выдачу другого узла с возможностью возрата на исходный узел
    * через кнопку навигации.
-   * @param adnId id узла.
+   * @param producer Узел-продьюсер, на который необходимо переключиться.
    * @param focLogic Закешированная focused-логика, собранная в экшене.
    * @param request Исходный реквест.
    * @return Фьючерс с http-результатом.
    */
-  private def _goToProducerIndex(adnId: String, focLogic: FocusedAdsLogicHttp)(implicit request: AbstractRequestWithPwOpt[_]): Future[Result] = {
+  private def _goToProducerIndex(producer: MAdnNode, focLogic: FocusedAdsLogicHttp)(implicit request: AbstractRequestWithPwOpt[_]): Future[Result] = {
     // Извлекаем MAdnNode втупую. exception будет перехвачен в recoverWith.
-    val mnodeFut = MAdnNodeCache.getById(adnId)
-        .map { _.get }
     val idxLogic = new ScIndexNodeSimpleHelper {
       override def geoListGoBackFut   = Future successful Some(true)
-      override def adnNodeFut         = mnodeFut
+      override def adnNodeFut         = Future successful producer
       override def isGeo              = false
       override implicit def _request  = request
       override def _reqArgs: ScReqArgs = new ScReqArgsDflt {
@@ -91,11 +100,6 @@ trait ScIndexAdOpen extends ScFocusedAds with ScIndexNodeCommon {
 
     }
     idxLogic.result
-      // Should never happen.
-      .recoverWith { case ex =>
-        LOGGER.warn("Failed to make node on-ad-open switch, because producer node does not exists: " + adnId, ex)
-        _focusedAds(focLogic)
-      }
   }
 
 }
