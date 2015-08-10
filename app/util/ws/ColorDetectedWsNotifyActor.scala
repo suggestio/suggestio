0ec297bd63763ff966601db52a634fafad6bb56e
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-08-10 19:13:20 +0300

    ws-поддержка в редакторе карточек переписана на scala.js и оптимизирована немного. Хотя есть ряд проблем пока что.

--- app/util/ws/ColorDetectedWsNotifyActor.scala
+++ app/util/ws/ColorDetectedWsNotifyActor.scala
@@ -2,10 +2,11 @@ package util.ws
 
 import akka.actor.{ActorRef, Actor}
 import models.im.Histogram
-import play.api.libs.json.{JsArray, JsString, JsObject}
+import play.api.libs.json.{JsValue, JsArray, JsString, JsObject}
 import util.PlayMacroLogsI
 import util.img.MainColorDetector
 import util.img.MainColorDetector.ImgBgColorUpdateAction
+import io.suggest.ad.form.AdFormConstants.{WS_MSG_DATA_FN, WS_MSG_TYPE_FN, TYPE_COLOR_PALETTE}
 
 /**
  * Suggest.io
@@ -23,20 +24,14 @@ trait ColorDetectedWsNotifyActor extends Actor with PlayMacroLogsI {
     // Пришла гистограмма с палитрой предлагаемых цветов.
     case h: Histogram =>
       //LOGGER.trace(s"Forwarding color histogram info to $out\n$h")
-      val respJson = JsObject(Seq(
-        "type" -> JsString("colorPalette"),
-        "data" -> JsArray( h.sorted.map(c => JsString(c.colorHex)) )
-      ))
-      out ! respJson
+      val data = JsArray( h.sorted.map(c => JsString(c.colorHex)) )
+      out ! _toWsMsgJson(TYPE_COLOR_PALETTE, data)
 
     // Пришло сообщение, что успешно выявлен цвет картинки
     case MainColorDetector.Update(newColorHex) =>
       //LOGGER.trace(s"Sending detected $newColorHex color to client via ws...")
-      val respJson = JsObject(Seq(
-        "type"  -> JsString("imgColor"),
-        "data"  -> JsString(newColorHex)
-      ))
-      out ! respJson
+      val data = JsString(newColorHex)
+      out ! _toWsMsgJson("imgColor", data)
 
     // Пришло сообщение о каких-то странностях при определении цвета:
     case detectedColorInfo: ImgBgColorUpdateAction =>
@@ -46,6 +41,13 @@ trait ColorDetectedWsNotifyActor extends Actor with PlayMacroLogsI {
       }
       // do nothing
 
+  }
+
+  protected def _toWsMsgJson(typ: String, data: JsValue): JsValue = {
+    JsObject(Seq(
+      WS_MSG_TYPE_FN  -> JsString(typ),
+      WS_MSG_DATA_FN  -> data
+    ))
   }
 
 }
