4a5e699baef872911102d285ea26b92200b7d1ac
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-08-13 19:07:15 +0300

    web21: Серверная FocusedV2 выдача допилена до ещё более новой архитектуры: - Разделение рендера на controls (header, arrows) и ad body. - Дополнение протокола focAd полями producerId, madId. - withHeadAd теперь необязательная. sc-sjs: продолжается пиление focused-выдачи (осталось ещё много), но focused-запросы-ответы уже соответствуют gridAds-запросам-ответам.

--- src/main/scala/io/suggest/sjs/common/view/safe/evtg/SafeEventTarget.scala
+++ src/main/scala/io/suggest/sjs/common/view/safe/evtg/SafeEventTarget.scala
@@ -1,6 +1,7 @@
 package io.suggest.sjs.common.view.safe.evtg
 
 import io.suggest.sjs.common.view.safe.ISafe
+import org.scalajs.dom
 import org.scalajs.dom.{Event, EventTarget}
 
 import scala.scalajs.js
@@ -14,6 +15,42 @@ import scala.scalajs.js.UndefOr
  * Оно нужно только для IE8 и ранее, поэтому потом можно удалить.
  * @see [[http://www.w3schools.com/jsref/met_document_addeventlistener.asp]]
  */
+object SafeEventTarget {
+
+  /** Интерфейс плагина для вешанья событий. */
+  sealed trait IFacade {
+    def addEventListener[T <: Event](eventTarget: EventTarget, eventType: String, listener: js.Function1[T,_]): Unit
+  }
+  /** Standard-compilant вешалка событий. */
+  private class StdFacade extends IFacade {
+    override def addEventListener[T <: Event](eventTarget: EventTarget, eventType: String, listener: js.Function1[T, _]): Unit = {
+      eventTarget.addEventListener(eventType, listener)
+    }
+  }
+  /** Вешалка событий на старые IE. */
+  private class IeFacade extends IFacade {
+    override def addEventListener[T <: Event](eventTarget: EventTarget, eventType: String, listener: js.Function1[T, _]): Unit = {
+      val ae = AttachEventStub(eventTarget)
+      ae.attachEvent[T]("on" + eventType, listener)
+    }
+  }
+
+  /** Проверка и её результат сохраняется тут. */
+  val FACADE: IFacade = {
+    val e = SafeEventTargetStub( dom.document )
+    val ael = e.addEventListener
+    if (ael.isDefined) {
+      new StdFacade
+    } else if (e.attachEvent.isDefined) {
+      new IeFacade
+    } else {
+      throw new UnsupportedOperationException("E01")
+    }
+  }
+
+}
+
+
 trait SafeEventTargetT extends ISafe {
 
   override type T <: EventTarget
@@ -25,22 +62,15 @@ trait SafeEventTargetT extends ISafe {
    * @tparam T Тип события, передаваемого в listener.
    */
   def addEventListener[T <: Event](eventType: String)(listener: js.Function1[T, _]): Unit = {
-    val e = _underlying.asInstanceOf[SafeEventTargetStub]
-    val ael = e.addEventListener
-    if (ael.isDefined) {
-      _underlying.addEventListener(eventType, listener)
-    } else {
-      val ae = e.attachEvent[T]
-      if (ae.isDefined) {
-        ae.get("on" + eventType, listener)
-      }
-    }
+    SafeEventTarget.FACADE
+      .addEventListener(_underlying, eventType, listener)
   }
 
 }
 
 
 /** Дефолтовая реалзизация враппера [[SafeEventTargetT]]. */
+// TODO Этот код не используется.
 case class SafeEventTarget(override val _underlying: EventTarget)
   extends SafeEventTargetT {
   override type T = EventTarget
@@ -49,9 +79,22 @@ case class SafeEventTarget(override val _underlying: EventTarget)
 
 /** Безопасный интерфейс для доступа к DOM-элементу в области вешанья событий. */
 sealed trait SafeEventTargetStub extends js.Object {
-
   def addEventListener: UndefOr[_] = js.native
+  def attachEvent: UndefOr[_] = js.native
+}
+object SafeEventTargetStub {
+  def apply(e: EventTarget): SafeEventTargetStub = {
+    e.asInstanceOf[SafeEventTargetStub]
+  }
+}
 
-  def attachEvent[T <: Event]: UndefOr[js.Function2[String, js.Function1[T,_], Unit]] = js.native
 
+/** IE-костыли для навешивания событий. */
+sealed trait AttachEventStub extends js.Object {
+  def attachEvent[T <: Event](eventType: String, listener: js.Function1[T,_]): Unit = js.native
+}
+object AttachEventStub {
+  def apply(e: EventTarget): AttachEventStub = {
+    e.asInstanceOf[AttachEventStub]
+  }
 }
