3ec0f0e6580a4b9b14fe23b833233c707a8a4ee9
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-08-21 19:12:59 +0300

    sc-sjs: focused-выдача получила наконец touch-навигацию в полном объеме. Состояния touch+preload тоже вроде работают. TouchUtil: исправлена мегасерьезная ошибка в isTouchDevice, из-за чего тот всегда возвращал false. Похожий косяк был в OnClickT. Можно считать, что focused-выдача допилена на 90% в новой архитектуре, хотя... - надо скрестить preload и shift left/right: сейчас клики просто дропаются, а надо бы прелоадер отобразить. - И при появлении focused-выдачи на loader отображать.

---
+++ src/main/scala/io/suggest/common/geom/coord/Coord.scala
@@ -0,0 +1,40 @@
+package io.suggest.common.geom.coord
+
+/**
+ * Suggest.io
+ * User: Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
+ * Created: 21.08.15 11:16
+ * Description: Сборка хранения абстрактных координат в n-мерном эвклидовом пространстве.
+ */
+trait ICoord[V]
+
+trait ICoord1d[V] extends ICoord[V] {
+
+  type This1 = ICoord1d[V]
+
+  /** Значение по оси абсцисс. */
+  def x: V
+
+  def deltaX(p2: This1)(implicit evidence: ICoord1dOps[V, This1]): V = {
+    evidence.deltaX(this, p2)
+  }
+
+}
+
+
+trait ICoord2d[V] extends ICoord1d[V] {
+
+  type This2 = ICoord2d[V]
+
+  /** Значение по оси ординат. */
+  def y: V
+
+  def deltaY(p2: This2)(implicit evidence: ICoord2dOps[V, This2]): V = {
+    evidence.deltaY(this, p2)
+  }
+
+  def distance(p2: This2)(implicit evidence: ICoord2dOps[V, This2]): V = {
+    evidence.distance(this, p2)
+  }
+
+}
