c482169d448352d092c71b18b20cd58a4dec7900
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-08-27 12:39:39 +0300

    Инициализация jsRouter в новой выдаче ещё ускорена и немного отрефакторена. jsRoutes script-тег рендерится прямо в шаблоне, а sc-sjs только подстраивается под него, не добавяя. Удалены шрифты оставшиеся от twitter bootstrap.

--- ui/app/views/sc/jsRouterTpl.scala.js
+++ ui/app/views/sc/jsRouterTpl.scala.js
@@ -2,12 +2,12 @@
 
 @* Код js-роутера, который раздается контроллером, либо включается в верстку соотв.страницы. *@
 
-@import io.suggest.sc.ScConstants._
+@import io.suggest.sc.ScConstants.JsRouter._
 @import ctx.request
 
 "use strict";
 
-@play.api.routing.JavaScriptReverseRouter(JS_ROUTER_NAME)(
+@play.api.routing.JavaScriptReverseRouter(NAME)(
   routes.javascript.MarketShowcase.geoShowcase,
   routes.javascript.MarketShowcase.showcase,
   routes.javascript.MarketShowcase.findAds,
@@ -16,6 +16,6 @@
 );
 
 @* Когда скрипт загрузился и выполнился, будет вызвана функция инициализации с указанным именем, если задана. *@
-if(typeof window.@JS_ROUTER_ASYNC_INIT_FNAME == 'function') {
-  window.@(JS_ROUTER_ASYNC_INIT_FNAME)();
+if(typeof window.@ASYNC_INIT_FNAME == 'function') {
+  window.@(ASYNC_INIT_FNAME)();
 }
