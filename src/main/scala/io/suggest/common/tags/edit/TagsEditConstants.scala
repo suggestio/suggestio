4e67c98eb7ccde11a590f01891489ee913f86680
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-09-07 17:55:18 +0300

    Продолжаются попытки запилить поддержку тегов в редакторе карточек, и идёт сопуствующий рефакторинг везде и вокруг sjs-проектов. Из sc-sjs в common-sjs вынесена базовая логика основного FSM, куски базовых моделей и vm'ок. web21: MExtTarget: поддержка новой десериализации.

---
+++ src/main/scala/io/suggest/common/tags/edit/TagsEditConstants.scala
@@ -0,0 +1,23 @@
+package io.suggest.common.tags.edit
+
+/**
+ * Suggest.io
+ * User: Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
+ * Created: 07.09.15 15:29
+ * Description: Контейнер констант для редактора тегов.
+ */
+object TagsEditConstants {
+
+  /** id контейнера уже созданных тегов. */
+  def EXIST_TAGS_CONT_ID          = "tagsExistCont"
+
+  /** id контейнера с инпутами для добавления нового тега. */
+  def ADD_FORM_ID                 = "tagsAddForm"
+
+  /** id инпута для ввода имени нового тега. */
+  def ADD_NAME_INPUT_ID           = "tagAddNameInput"
+
+  /** id кнопки добавления нового тега. */
+  def ADD_BTN_ID                  = "tagAddBtn"
+
+}
