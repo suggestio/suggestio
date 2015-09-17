b557c71380904bc455965825e399a9dc2af8c03c
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-09-17 18:58:37 +0300

    Продолжение запиливания поддержки тегов в выдаче: запилен экшен web21 контроллера, пока неслинкованный никуда.

---
+++ src/main/scala/io/suggest/sc/TagSearchConstants.scala
@@ -0,0 +1,36 @@
+package io.suggest.sc
+
+/**
+ * Suggest.io
+ * User: Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
+ * Created: 17.09.15 17:33
+ * Description: Константы протокола поиска тегов.
+ */
+object TagSearchConstants {
+
+  object Req {
+
+    /** Имя поля-префикса имён полей тега. */
+    def FN_PREFIX = "t"
+
+    /** Поле запроса полнотекстового запроса поиска тегов по имени. */
+    def FACE_FTS_QUERY_FN = "q"
+
+    /** Кол-во возвращаемых результатов. */
+    def LIMIT_FN          = "l"
+
+    /** Абсолютный сдвиг в результатах поиска. */
+    def OFFSET_FN         = "o"
+
+  }
+
+
+  object Resp {
+
+    def RENDERED_FN     = "a"
+
+    def FOUND_COUNT_FN  = "b"
+
+  }
+
+}
