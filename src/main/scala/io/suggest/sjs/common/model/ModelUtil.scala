a3feac000e5a418e52ff2581d4a86ce285ca9af2
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-09-18 21:37:21 +0300

    sc-sjs: модель для поиска тегов на сервере, модели аргументов и результата, рефакторинг других srv-моделей: дедубликция кода, унификация интерфейсов. common: Унификация поля API_VSN_FN для выдачи. web21: экшен tagsSearch подвязан к routes, поддержка в jsRoutes выдачи, дополнительная устойчивость к ошибкам кассандры в выдаче. util: перехват и обработак ошибок кассанды, в т.ч. синхронных.

--- src/main/scala/io/suggest/sjs/common/model/ModelUtil.scala
+++ src/main/scala/io/suggest/sjs/common/model/ModelUtil.scala
@@ -1,7 +1,6 @@
 package io.suggest.sjs.common.model
 
-import scala.scalajs.js
-import scala.scalajs.js.Dictionary
+import scala.scalajs.js.{Dictionary, Any, JSON}
 
 /**
  * Suggest.io
@@ -12,13 +11,13 @@ import scala.scalajs.js.Dictionary
 
 trait FromJsonT {
   type T
-  def fromJson(raw: js.Any): T
+  def fromJson(raw: Any): T
 }
 
 trait MaybeFromJsonT extends FromJsonT {
-  def maybeFromJson(raw: js.Any): Option[T]
+  def maybeFromJson(raw: Any): Option[T]
 
-  override def fromJson(raw: js.Any): T = {
+  override def fromJson(raw: Any): T = {
     maybeFromJson(raw).get
   }
 }
@@ -27,25 +26,31 @@ trait MaybeFromJsonT extends FromJsonT {
 /** Добавить метод fromString() в статическую модель, поддерживающую десериализацию fromDyn(). */
 trait FromStringT extends FromJsonT {
   def fromString(s: String): T = {
-    fromJson( js.JSON.parse(s) )
+    fromJson( JSON.parse(s) )
   }
 }
 
 
 /** Интерфейс сериализации в JSON. */
 trait IToJson {
-  def toJson: js.Any
+  def toJson: Any
+
+  override def toString: String = {
+    toJson.toString
+  }
 }
 
 /** Интерфейс сериализации в JSON object. */
 trait IToJsonDict extends IToJson {
-  def toJson: js.Dictionary[js.Any]
+  def toJson: Dictionary[Any]
 }
 
 
 /** Пустая реализация [[IToJsonDict]]. */
-class ToJsonDictDummy extends IToJsonDict {
-  override def toJson: Dictionary[js.Any] = {
+trait ToJsonDictDummyT extends IToJsonDict {
+  override def toJson: Dictionary[Any] = {
     Dictionary.empty
   }
 }
+class ToJsonDictDummy
+  extends ToJsonDictDummyT
