f70e19abc59628a9ab6f9b8dc8d56f016cd069a0
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-10-01 14:02:03 +0300

    MImg3: поддержка сохранения нового элемента в MMedia и её storage.

--- app/models/im/MImg3.scala
+++ app/models/im/MImg3.scala
@@ -3,8 +3,11 @@ package models.im
 import java.util.UUID
 
 import io.suggest.model.img.{ImgSzDated, IImgMeta}
+import io.suggest.model.n2.media.storage.{CassandraStorage, IMediaStorage}
+import io.suggest.model.n2.media.{MPictureMeta, MFileMeta}
 import io.suggest.util.UuidUtil
 import models._
+import models.mfs.FileUtil
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.iteratee.Enumerator
 import util.PlayLazyMacroLogsImpl
@@ -45,24 +48,21 @@ object MImg3 {
 }
 
 
-case class MImg3(override val rowKeyStr: String,
-                 override val dynImgOps: Seq[ImOp])
-  extends MImgT
-  with PlayLazyMacroLogsImpl
-{
+/** Трейт одного элемента модели [[MImg3]].
+  * Появился из-за необходимости простого переключения между cassandra и seaweedfs. */
+abstract class MImg3T extends MImgT {
+
+  override type MImg_t <: MImg3T
 
   override lazy val rowKey: UUID = {
     UuidUtil.base64ToUuid(rowKeyStr)
   }
 
-  override type MImg_t = MImg3
-  override def thisT: MImg_t = this
-  override def toWrappedImg = this
-
   lazy val _mediaId = MMedia.mkId(rowKeyStr, qOpt)
 
-  lazy val _mediaOptFut = MMedia.getById(_mediaId)
-  lazy val _mediaFut = _mediaOptFut.map(_.get)
+  // Не val потому что результат может меняться с None на Some() в результате сохранения картинки.
+  def _mediaOptFut = MMedia.getById(_mediaId)
+  def _mediaFut = _mediaOptFut.map(_.get)
 
   override protected lazy val _getImgMeta: Future[Option[IImgMeta]] = {
     _mediaOptFut map { mmediaOpt =>
@@ -92,10 +92,48 @@ case class MImg3(override val rowKeyStr: String,
     Enumerator.flatten(fut)
   }
 
-  /** Сохранить в хранилище, при необходимости создать MMedia. */
-  override protected def _doSaveToPermanent(loc: MLocalImgT): Future[_] = {
-    _mediaFut.flatMap { mm =>
-      mm.storage.write( loc.imgBytesEnumerator )
+  /** Подготовить и вернуть новое медиа-хранилище для модели. */
+  protected def _newMediaStorage: Future[IMediaStorage]
+
+  /** Сохранить в постоянное хранилище, (пере-)создать MMedia. */
+  override protected def _doSaveToPermanent(loc: MLocalImgT): Future[MMedia] = {
+    val mli = toLocalInstance
+    _mediaFut.recoverWith { case ex: Throwable =>
+      // Перезаписывать нечего, т.к. элемент ещё не существует в MMedia.
+      val whOptFut = mli.getImageWH
+      val mimeFut = Future(mli.mime)
+      val sha1Fut = Future(FileUtil.sha1(mli.file))
+      val storFut = _newMediaStorage
+      val szB = mli.file.length()
+      for {
+        whOpt <- whOptFut
+        mime <- mimeFut
+        sha1 <- sha1Fut
+        stor <- storFut
+        mm <- {
+          val _mm = MMedia(
+            nodeId = rowKeyStr,
+            id = Some(_mediaId),
+            file = MFileMeta(
+              mime = mime,
+              sizeB = szB,
+              isOriginal = isOriginal,
+              sha1 = Some(sha1)
+            ),
+            picture = whOpt.map(MPictureMeta.apply),
+            storage = stor
+          )
+          _mm.save
+            .map { _mmId => _mm }
+        }
+      } yield {
+        mm
+      }
+
+    }.flatMap { mm =>
+      mm.storage
+        .write( mli.imgBytesEnumerator )
+        .map { _ => mm }
     }
   }
 
@@ -104,10 +142,6 @@ case class MImg3(override val rowKeyStr: String,
     // Необходимость апдейта метаданных возникает, когда обнаруживается, что нет метаднных.
     // В случае N2 MMedia, метаданные без блоба существовать не могут, и необходимость не должна наступать.
     LOGGER.warn(s"_updateMetaWith($localWh, $localImg) ignored and not implemented")
-  }
-
-  override def withDynOps(dynImgOps2: Seq[ImOp]): MImg3 = {
-    copy(dynImgOps = dynImgOps2)
   }
 
   override def delete: Future[_] = {
@@ -122,6 +156,35 @@ case class MImg3(override val rowKeyStr: String,
       case None =>
         Future successful false
     }
+  }
+
+}
+
+
+case class MImg3(override val rowKeyStr: String,
+                 override val dynImgOps: Seq[ImOp])
+  extends MImg3T
+  with PlayLazyMacroLogsImpl
+  with I3Cassandra
+{
+
+  override type MImg_t = MImg3
+  override def thisT: MImg_t = this
+  override def toWrappedImg = this
+
+  override def withDynOps(dynImgOps2: Seq[ImOp]): MImg3 = {
+    copy(dynImgOps = dynImgOps2)
+  }
+
+}
+
+
+/** Использовать кассандру для сохранения новых файлов. */
+trait I3Cassandra extends MImg3T {
+
+  override protected def _newMediaStorage: Future[IMediaStorage] = {
+    val stor = CassandraStorage(rowKey, qOpt)
+    Future successful stor
   }
 
 }
