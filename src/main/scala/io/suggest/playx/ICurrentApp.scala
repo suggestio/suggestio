cfc9dc8ae0498a5b275b83dac74784ac4c774e44
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-10-12 13:05:25 +0300

    web21: усилено использование DI в контроллерах. Это небольшая отдалённая подготовка к использованию DI swfs клиента.

---
+++ src/main/scala/io/suggest/playx/ICurrentApp.scala
@@ -0,0 +1,25 @@
+package io.suggest.playx
+
+import play.api.Application
+
+/**
+ * Suggest.io
+ * User: Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
+ * Created: 12.10.15 11:56
+ * Description: Трейт для DI-
+ */
+trait ICurrentApp {
+
+  /** Экземпляр Application, вброшенный в класс через DI. */
+  implicit protected[this] def current: Application
+
+}
+
+
+/** Трейт для быстрого доступа к play config через DI Application. */
+trait ICurrentConf extends ICurrentApp {
+
+  /** Доступ к play config через экземпляр Application, вброшенный через DI. */
+  protected[this] def configuration = current.configuration
+
+}
