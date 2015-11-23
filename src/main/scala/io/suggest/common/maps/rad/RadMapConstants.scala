6bd2e2ab0f30cbde37c7bbdadac3f8286cc47c11
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2015-11-23 22:42:36 +0300

    Продолжается пиление карты размещения: - scalajs-leaflet расширение API событиями и прочими кошерными вещами. - web21: улучшена карта, все параметры маркера круга передаются через аттрибуты. - map-rad-sjs: инициализация круга охвата рекламного размещения. - common: сопутствующие изменения.

--- src/main/scala/io/suggest/common/maps/rad/RadMapConstants.scala
+++ src/main/scala/io/suggest/common/maps/rad/RadMapConstants.scala
@@ -13,8 +13,26 @@ object RadMapConstants {
   /** id элемента-контейнера карты. */
   def CONTAINER_ID  = _ID_PREFIX + "cont"
 
-  def INPUT_ID_LAT  = _ID_PREFIX + "a"
-  def INPUT_ID_LON  = _ID_PREFIX + "o"
-  def INPUT_ID_ZOOM = _ID_PREFIX + "z"
+  def INPUT_ID_LAT  = _ID_PREFIX + "ma"
+  def INPUT_ID_LON  = _ID_PREFIX + "mo"
+  def INPUT_ID_ZOOM = _ID_PREFIX + "mz"
+
+  def INPUT_ID_CIRCLE_LAT     = _ID_PREFIX + "ca"
+  def INPUT_ID_CIRCLE_LON     = _ID_PREFIX + "co"
+  def INPUT_ID_CIRCLE_RADIUS  = _ID_PREFIX + "cr"
+
+
+  /** id img-тега, хранящего адресок картинки с геомаркером карты. */
+  def IMG_ID_MARKER        = _ID_PREFIX + "im"
+  /** id img-тега, хранящего вариант retina-картинки геомаркера. */
+  def IMG_ID_MARKER_RETINA = _ID_PREFIX + "imr"
+  /** id img-тега с тенью маркера. */
+  def IMG_ID_MARKER_SHADOW = _ID_PREFIX + "is"
+
+  def ATTR_PREFIX = "data-"
+  def ATTR_IMG_WIDTH        = ATTR_PREFIX + "w"
+  def ATTR_IMG_HEIGHT       = ATTR_PREFIX + "h"
+  def ATTR_IMG_ANCHOR_X     = ATTR_PREFIX + "x"
+  def ATTR_IMG_ANCHOR_Y     = ATTR_PREFIX + "y"
 
 }
