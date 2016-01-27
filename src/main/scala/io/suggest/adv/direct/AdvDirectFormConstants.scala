6bf2764890814c9eb65b046edae86783c682b570
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2016-01-27 22:34:44 +0300

    Пиление формы прямого размещения продолжается. Восстановлено и улучшено функционирование галочек, но рассчет цены пока не готов. В верстке формы исправлен ряд проблем юзабилити и прочего.

--- src/main/scala/io/suggest/adv/direct/AdvDirectFormConstants.scala
+++ src/main/scala/io/suggest/adv/direct/AdvDirectFormConstants.scala
@@ -10,6 +10,9 @@ import io.suggest.common.html.HtmlConstants._
  */
 object AdvDirectFormConstants {
 
+  /** Разделитель кусков составных id. */
+  def ID_DELIM                = "."
+
   /** Префикс id ряда элементов только данной формы. */
   def PREFIX                  = "adi"
 
@@ -26,7 +29,7 @@ object AdvDirectFormConstants {
   def CITIES_BODIES_CONT_ID   = NODES_BAR_ID + "CB"
 
   /** Префикс id таба города. */
-  def CITY_TAB_BODY_PREFIX    = CITIES_HEADS_CONT_ID + "b."
+  def CITY_TAB_BODY_PREFIX    = CITIES_HEADS_CONT_ID + "b" + ID_DELIM
 
   /** Класс-пометка о том, что данный элемент является "кнопкой" города. */
   def CITY_TAB_HEAD_CLASS     = "adv-management_city-i"
@@ -35,7 +38,7 @@ object AdvDirectFormConstants {
   def CITY_CURR_TITLE_ID      = CITIES_HEADS_CONT_ID + "T"
 
   /** id таба одного города. */
-  def CITY_TAB_HEAD_ID(cityId: String) = CITIES_HEADS_CONT_ID + "." + cityId
+  def CITY_TAB_HEAD_ID(cityId: String) = CITIES_HEADS_CONT_ID + ID_DELIM + cityId
 
   /** сборка id тела данных по одному городу. */
   def CITY_TAB_BODY_ID(cityId: String) = CITY_TAB_BODY_PREFIX + cityId
@@ -51,32 +54,45 @@ object AdvDirectFormConstants {
 
 
   /** Префикс id'шников заголовков табов групп узлов. */
-  def CITY_NODES_TAB_HEAD_ID_PREFIX = CITIES_HEADS_CONT_ID + "Ns."
+  def CITY_NODES_TAB_HEAD_ID_PREFIX = CITIES_HEADS_CONT_ID + "Ns" + ID_DELIM
 
   /** Сборка id контейнеров заголовков табов нод в рамках города. */
   def CITY_NODES_TAB_HEAD_ID(cityId: String, catId: Option[String] = None): String = {
     val id0 = CITY_NODES_TAB_HEAD_ID_PREFIX + cityId
-    catId.fold(id0)(id0 + "." + _)
+    catId.fold(id0)(id0 + ID_DELIM + _)
   }
+
+
+  /** Префикс id'шников чекбоксов на уровне табов. */
+  def CITY_NODES_TAB_HEAD_CHECKBOX_ID_PREFIX = "Cb" + CITY_NODES_TAB_HEAD_ID_PREFIX
+  /** Сборка id'шников чекбоксов на уровне табов. */
+  def CITY_NODES_TAB_HEAD_CHECKBOX_ID(cityId: String, catId: Option[String] = None): String = {
+    val id0 = CITY_NODES_TAB_HEAD_CHECKBOX_ID_PREFIX + cityId
+    catId.fold(id0)(id0 + ID_DELIM + _)
+  }
+
+  def CITY_CAT_NODES_ID_PREFIX = "Ccn" + CITY_TAB_BODY_PREFIX + ID_DELIM
 
   /** Сборка id контейнера одного тела нод в рамках группы узлов города. */
   def CITY_NODES_TAB_BODY_ID(cityId: String, catId: String): String = {
-    CITY_TAB_BODY_ID(cityId) + ".Ns." + catId
+    CITY_CAT_NODES_ID_PREFIX + cityId + ID_DELIM + catId
   }
 
+
+  def NGRPS_CITY_CONT_ID_PREFIX = NGRPS_CONT_ID + ID_DELIM
   /** id контейнера всех групп узлов в рамках одного города. */
   def NGRPS_CITY_CONT_ID(cityId: String): String = {
-    NGRPS_CONT_ID + "." + cityId
+    NGRPS_CITY_CONT_ID_PREFIX + cityId
   }
 
-  def NODE_ROW_ID_PREFIX = PREFIX + "O."
+  def NODE_ROW_ID_PREFIX = PREFIX + "O" + ID_DELIM
 
   /** id контейнера одного узла в списке узлов. */
   def NODE_ROW_ID(nodeId: String): String = {
     NODE_ROW_ID_PREFIX + nodeId
   }
 
-  def NODE_CHECK_BOX_ID_PREFIX = NODE_ROW_ID_PREFIX + ".cb."
+  def NODE_CHECK_BOX_ID_PREFIX = NODE_ROW_ID_PREFIX + ID_DELIM + "cb" + ID_DELIM
 
   def NODE_CHECK_BOX_ID(nodeId: String): String = {
     NODE_CHECK_BOX_ID_PREFIX + nodeId
