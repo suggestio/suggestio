fa025f16e2ae99bfa67fe7b302f914121c188476
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2016-01-28 22:12:07 +0300

    Пиление формы прямого размещения и сопутствующих вещей: - Аргументы инициализации jq.dateTimePicker задаются на сервере, что решает кучу проблем с локалями и локализациями. - jq.dateTimePicker вычищен и перемещён в assets для минификации при stage. - ctl.MarketAdv.getAdvPriceSubmit: возвращать JSON с несколькими рендерами внутри. - Рефакторинг в шаблонах рендера периода. Вынос в отдельный пакет. - Рефакторинг в маппинге формы прямого размещения. - другие разные изменения.

--- src/main/scala/io/suggest/model/sc/common/SlNameTokenStr.scala
+++ src/main/scala/io/suggest/model/sc/common/SlNameTokenStr.scala
@@ -1,9 +1,9 @@
 package io.suggest.model.sc.common
 
-import io.suggest.ym.model.common.AdnSinks
 
 /** Поле name для поиска по полю sls 2014-aug, которое содержит ngram'мы различной длины.
-  * Можно искать по sink-name из [[AdnSinks]], по [[AdShowLevels]] и по полному имени [[SinkShowLevels]]. */
+  * Можно искать по sink-name из [[io.suggest.ym.model.common.AdnSinks]],
+  * по [[AdShowLevels]] и по полному имени [[SinkShowLevels]]. */
 trait SlNameTokenStr {
 
   /** Название уровня отображения. */
