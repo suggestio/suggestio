1122e35fdebaeb57a57b6545f4754a840476a034
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2016-03-25 18:16:19 +0300

    EnumJsonReadsT: Не зависеть от Enumeration, а дергать maybeWithName(). При проблемах вместо экзепшенов возвращаться JsError.

--- src/main/scala/io/suggest/model/menum/EnumJsonReadsT.scala
+++ src/main/scala/io/suggest/model/menum/EnumJsonReadsT.scala
@@ -1,6 +1,7 @@
 package io.suggest.model.menum
 
-import io.suggest.common.menum.EnumValue2Val
+import io.suggest.common.menum.IMaybeWithName
+import play.api.data.validation.ValidationError
 import play.api.libs.json._
 
 /**
@@ -9,11 +10,16 @@ import play.api.libs.json._
  * Created: 24.02.15 17:03
  * Description: Поддержка json reads для play.json десериализации.
  */
-trait EnumJsonReadsT extends EnumValue2Val {
+
+trait EnumJsonReadsT extends IMaybeWithName {
 
   implicit def reads: Reads[T] = {
     __.read[String]
-      .map(withName)
+      // Чтобы не было экзепшенов, надо дергать maybeWithName() и смотреть значение Option.
+      .map(maybeWithName)
+      // TODO Хз как передать некорректное значение внутрь ValidationError... А надо бы, очень надо.
+      .filter( ValidationError("error.unknown.name") )(_.nonEmpty)
+      .map(_.get)
   }
 
 }
