dcea01d8be82a648af24aa0d1d13cc5fefd51f67
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2016-03-29 15:21:07 +0300

    lk.adv.direct.vm.Form: Использовать Adv4FreeInside вместо ручной инициализации.

--- src/main/scala/io/suggest/lk/adv/direct/vm/Form.scala
+++ src/main/scala/io/suggest/lk/adv/direct/vm/Form.scala
@@ -2,7 +2,7 @@ package io.suggest.lk.adv.direct.vm
 
 import io.suggest.adv.direct.AdvDirectFormConstants
 import io.suggest.lk.adv.direct.vm.nbar.Root
-import io.suggest.lk.adv.vm.Adv4Free
+import io.suggest.lk.adv.vm.{Adv4FreeInside, Adv4Free}
 import io.suggest.sjs.dt.period.vm.Container
 import io.suggest.sjs.common.fsm.{SjsFsm, IInitLayoutFsm}
 import io.suggest.sjs.common.vm.find.FindElT
@@ -27,20 +27,19 @@ import Form.Dom_t
 
 trait FormT
   extends FormDataVmT
-  with IInitLayoutFsm
+  with Adv4FreeInside
 {
 
   override type T = Dom_t
 
   def root          = Root.find()
   def intervalCont  = Container.find()
-  def adv4free      = Adv4Free.find()
 
   override def initLayout(fsm: SjsFsm): Unit = {
+    super.initLayout(fsm)
     val f = IInitLayoutFsm.f(fsm)
     root.foreach(f)
     intervalCont.foreach(f)
-    adv4free.foreach(f)
   }
 
 }
