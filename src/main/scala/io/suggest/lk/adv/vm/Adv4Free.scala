438d01fbe5d470ff552986a0c4a6c95a5d64c39d
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2016-03-29 15:20:25 +0300

    adv.vm.Adv4FreeInside: Трейт для быстрого внедрения поддержки галочки в над-vm.

--- src/main/scala/io/suggest/lk/adv/vm/Adv4Free.scala
+++ src/main/scala/io/suggest/lk/adv/vm/Adv4Free.scala
@@ -2,7 +2,7 @@ package io.suggest.lk.adv.vm
 
 import io.suggest.adv.AdvConstants.Su
 import io.suggest.lk.adv.m.Adv4FreeChanged
-import io.suggest.sjs.common.fsm.InitLayoutFsmChange
+import io.suggest.sjs.common.fsm.{IInitLayoutFsm, SjsFsm, IInitLayoutFsmDummy, InitLayoutFsmChange}
 import io.suggest.sjs.common.vm.find.FindElT
 import io.suggest.sjs.common.vm.input.Checked
 import org.scalajs.dom.raw.HTMLInputElement
@@ -33,3 +33,17 @@ trait Adv4FreeT extends Checked with InitLayoutFsmChange {
 
 case class Adv4Free(override val _underlying: Dom_t)
   extends Adv4FreeT
+
+
+/** Быстрое добавление поддержки SU-галочки в vm формы размещения. */
+trait Adv4FreeInside extends IInitLayoutFsmDummy {
+
+  def adv4free = Adv4Free.find()
+
+  override def initLayout(fsm: SjsFsm): Unit = {
+    super.initLayout(fsm)
+    val f = IInitLayoutFsm.f(fsm)
+    adv4free.foreach(f)
+  }
+
+}
