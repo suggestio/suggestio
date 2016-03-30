d24f8eed981cab732ae39385ae7621c3c6853fdb
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2016-03-30 22:10:50 +0300

    Поддержка галочки Adv4Free перенесена сюда из common-sjs.

---
+++ src/main/scala/io/suggest/lk/adv/fsm/AdvFormChangedReceiver.scala
@@ -0,0 +1,28 @@
+package io.suggest.lk.adv.fsm
+
+import io.suggest.lk.adv.m.IAdvFormChanged
+import io.suggest.sjs.common.fsm.SjsFsm
+
+/**
+  * Suggest.io
+  * User: Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
+  * Created: 30.03.16 16:46
+  * Description: Ресивер сигналов обновления формы.
+  */
+trait AdvFormChangedReceiver extends SjsFsm with IUpdatePriceDataStart {
+
+  /** Подмешать это в FSM-состояние, чтобы в ресивере появился вызов  */
+  trait AdvFormChangedReceiverStateT extends FsmEmptyReceiverState {
+    override def receiverPart: PartialFunction[Any, Unit] = super.receiverPart orElse {
+      case msg: IAdvFormChanged =>
+        _handleAdvFormChanged(msg)
+    }
+
+    /** Реакция на получение сообщений об изменении формы.
+      * Изначально -- вызывать апдейт серверных данных формы. */
+    protected def _handleAdvFormChanged(msg: IAdvFormChanged): Unit = {
+      _upStart()
+    }
+  }
+
+}
