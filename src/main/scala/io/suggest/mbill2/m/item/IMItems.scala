cd7047978df3349356d912ffd736fe37c79701c0
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2016-04-07 21:10:17 +0300

    Фиксы в scaladoc.

--- src/main/scala/io/suggest/mbill2/m/item/IMItems.scala
+++ src/main/scala/io/suggest/mbill2/m/item/IMItems.scala
@@ -4,11 +4,11 @@ package io.suggest.mbill2.m.item
  * Suggest.io
  * User: Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
  * Created: 23.12.15 12:04
- * Description: Интерфейс для доступа к DI-полю с инстансом [[MItems]].
+ * Description: Интерфейс для доступа к DI-полю с инстансом [[io.suggest.mbill2.m.item.MItems]].
  */
 trait IMItems {
 
-  /** DI-экземпляр slick-модели [[MItems]]. */
+  /** DI-экземпляр slick-модели [[io.suggest.mbill2.m.item.MItems]]. */
   def mItems: MItems
 
 }
