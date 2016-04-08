bac824137329dbfb4a0c9d1ccede4038cb626072
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2016-04-08 23:38:27 +0300

    AdvGeoConstants: + разные константы для чтения JSON'а текущих размещений в форме adv geo.

--- src/main/scala/io/suggest/adv/geo/AdvGeoConstants.scala
+++ src/main/scala/io/suggest/adv/geo/AdvGeoConstants.scala
@@ -21,4 +21,43 @@ object AdvGeoConstants {
 
   }
 
+
+  /** Константы инфы о текущих гео-размещениях.
+    * Для передачи данных в Leaflet используется GeoJSON FeatureCollection.
+    * Feature.properties содержит инфу по рендеру кругов и попапов.
+    * Featore.geometry содержит точки для кругов.
+    */
+  object GjFtPropsC {
+
+    /** Имя поля радиуса круга. */
+    def RADIUS_FN         = "r"
+
+    /** Имя поля для содержимое попапа. */
+    def POPUP_CONTENT_FN  = "p"
+
+    /** Имя поля для цвета заливки геом.фигуры. */
+    def FILL_COLOR_FN     = "f"
+
+    /** Имя поля непрозрачности заливки шейпа. */
+    def FILL_OPACITY_FN   = "o"
+
+  }
+
+  /** Константы заливки шейпов на карте гео-размещений. */
+  object CurrShapes {
+
+    /** id элемента, содержащего JSON-описалово текущих размещений. */
+    def DATA_CONTAINER_ID = PREFIX + "csi"
+
+    /** Цвет заливки запрошенных размещений. */
+    def REQ_COLOR = "#4C88A9"
+
+    /** Цвет заливки принятых размещений. */
+    def OK_COLOR  = "#1ABC8A"
+
+    /** Прозрачность заливки info-шейпов. */
+    def OPACITY   = 0.81F
+
+  }
+
 }
