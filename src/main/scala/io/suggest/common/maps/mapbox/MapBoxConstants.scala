44c85b1f18a930c0148b8c16fecc99e5cac3d8e1
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2016-04-13 22:47:44 +0300

    MapBoxConstants: кое-какое барахло для стилей, слоёв и прочего.

--- src/main/scala/io/suggest/common/maps/mapbox/MapBoxConstants.scala
+++ src/main/scala/io/suggest/common/maps/mapbox/MapBoxConstants.scala
@@ -13,4 +13,37 @@ object MapBoxConstants {
   /** id input-элемента, в котором сервер сообщает access token для доступа к карте. */
   def ACCESS_TOKEN_INPUT_ID = "acto" + SUFFIX
 
+  /** Обыденные названия слоёв разных. */
+  object Layers {
+
+    def MY_GEOLOC_LAYER_ID = "a"
+
+    object MyGeoLoc {
+
+      def INNER_RADIUS_PX = 5
+
+      def INNER_COLOR     = "#007cbf"
+
+    }
+
+
+    object Circle {
+
+      object Paint {
+
+        def PREFIX = "circle-"
+
+        def RADIUS    = PREFIX + "radius"
+        def COLOR     = PREFIX + "color"
+        def BLUR      = PREFIX + "blur"
+        def OPACITY   = PREFIX + "opacity"
+        def TRANSLATE = PREFIX + "translate"
+        def TRANSLATE_ANCHOR = TRANSLATE + "-anchor"
+
+      }
+
+    }
+
+  }
+
 }
