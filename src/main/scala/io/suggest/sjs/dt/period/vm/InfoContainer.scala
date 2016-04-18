e2cc5417cf3dbe59f520dfac6dee67f7f934a088
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2016-04-18 15:14:34 +0300

    Дедубликация кода с использованием OfHtmlElDomIdRelated

--- src/main/scala/io/suggest/sjs/dt/period/vm/InfoContainer.scala
+++ src/main/scala/io/suggest/sjs/dt/period/vm/InfoContainer.scala
@@ -3,8 +3,8 @@ package io.suggest.sjs.dt.period.vm
 import io.suggest.dt.interval.DatesIntervalConstants
 import io.suggest.sjs.common.vm.content.{ReplaceWith, SetInnerHtml}
 import io.suggest.sjs.common.vm.find.FindDiv
-import io.suggest.sjs.common.vm.of.{OfDiv, OfNodeHtmlEl, OfHtml, OfHtmlElement}
-import org.scalajs.dom.raw.HTMLElement
+import io.suggest.sjs.common.vm.of.{OfDiv, OfHtml}
+import io.suggest.sjs.common.vm.util.OfHtmlElDomIdRelated
 
 /**
  * Suggest.io
@@ -14,15 +14,10 @@ import org.scalajs.dom.raw.HTMLElement
  * В новой (этой) архитектуре оно всегда рендерится на сервере.
  * В mx_cof оно кое-как рендерилось на клиенте.
  */
-object InfoContainer extends FindDiv with OfDiv with OfHtml {
+object InfoContainer extends FindDiv with OfDiv with OfHtml with OfHtmlElDomIdRelated {
 
   override type T     = InfoContainer
   override def DOM_ID = DatesIntervalConstants.INFO_CONT_ID
-
-  /** Тестирование на пригодность к заворачиванию в инстанс этой VM. */
-  override def _isWantedHtmlEl(el: HTMLElement): Boolean = {
-    super._isWantedHtmlEl(el) && el.id == DOM_ID
-  }
 
 }
 
