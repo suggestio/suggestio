c70a8d85bfe83e1c4db6a1ab07909626b9d8a1f1
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2016-05-18 11:37:03 +0300

    play -> 2.5.3, sc: блюрринг плитки на узких экранах при боковых панелях.

--- project/Common.scala
+++ project/Common.scala
@@ -20,7 +20,7 @@ object Common {
   )
 
   /** Версия play. */
-  val playVsn         = "2.5.2"
+  val playVsn         = "2.5.3"
 
   /** Версия play-slick прослойки. */
   val playSlickVsn    = "2.0.2"
