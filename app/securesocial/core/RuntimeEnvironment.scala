f3c72ea1f0f232ab1ea7e413e3d54d802ca5cf65
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2016-08-16 19:54:21 +0300

    Deleted Concur, Weibo, Xing providers.

--- app/securesocial/core/RuntimeEnvironment.scala
+++ app/securesocial/core/RuntimeEnvironment.scala
@@ -77,17 +77,13 @@ object RuntimeEnvironment {
       include(new GitHubProvider(routes, cacheService, oauth2ClientFor(GitHubProvider.GitHub))),
       include(new GoogleProvider(routes, cacheService, oauth2ClientFor(GoogleProvider.Google))),
       include(new InstagramProvider(routes, cacheService, oauth2ClientFor(InstagramProvider.Instagram))),
-      include(new ConcurProvider(routes, cacheService, oauth2ClientFor(ConcurProvider.Concur))),
       include(new SoundcloudProvider(routes, cacheService, oauth2ClientFor(SoundcloudProvider.Soundcloud))),
       //include(new LinkedInOAuth2Provider(routes, cacheService,oauth2ClientFor(LinkedInOAuth2Provider.LinkedIn))),
       include(new VkProvider(routes, cacheService, oauth2ClientFor(VkProvider.Vk))),
       include(new DropboxProvider(routes, cacheService, oauth2ClientFor(DropboxProvider.Dropbox))),
-      include(new WeiboProvider(routes, cacheService, oauth2ClientFor(WeiboProvider.Weibo))),
-      include(new ConcurProvider(routes, cacheService, oauth2ClientFor(ConcurProvider.Concur))),
       // oauth 1 client providers
       include(new LinkedInProvider(routes, cacheService, oauth1ClientFor(LinkedInProvider.LinkedIn))),
       include(new TwitterProvider(routes, cacheService, oauth1ClientFor(TwitterProvider.Twitter))),
-      include(new XingProvider(routes, cacheService, oauth1ClientFor(XingProvider.Xing))),
       // username password
       include(new UsernamePasswordProvider[U](userService, avatarService, viewTemplates, passwordHashers))
     )
