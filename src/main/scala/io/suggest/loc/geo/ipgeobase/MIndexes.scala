f51a1403cbacad46899752696b9fe125837fe9b6
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2016-09-23 12:20:31 +0300

    MIndexes: вынос утили в трейты в util model.es.

--- src/main/scala/io/suggest/loc/geo/ipgeobase/MIndexes.scala
+++ src/main/scala/io/suggest/loc/geo/ipgeobase/MIndexes.scala
@@ -3,15 +3,9 @@ package io.suggest.loc.geo.ipgeobase
 import java.time.ZonedDateTime
 
 import com.google.inject.{Inject, Singleton}
-import io.suggest.model.es.IEsModelDiVal
+import io.suggest.model.es._
 import io.suggest.util.MacroLogsImpl
-import io.suggest.util.SioEsUtil.laFuture2sFuture
-import org.elasticsearch.ResourceNotFoundException
 import org.elasticsearch.common.settings.Settings
-
-import scala.concurrent.Future
-import scala.collection.JavaConversions._
-import scala.util.{Failure, Success}
 
 /**
   * Suggest.io
@@ -37,14 +31,17 @@ object MIndexes {
 /** Статическая инжектируемая модель управления ES-индексами IpGeoBase. */
 @Singleton
 class MIndexes @Inject() (
-  mCommonDi: IEsModelDiVal
+  override val mCommonDi: IEsModelDiVal
 )
-  extends MacroLogsImpl
+  extends EsIndexStaticAlias
+  with EsIndexDynCreate
+  with EsIndexDynDelete
+  with EsIndexOptimizeAfterBulk
+  with MacroLogsImpl
 {
 
   import mCommonDi._
   import LOGGER._
-  import MIndexes._
 
 
   /**
@@ -66,71 +63,10 @@ class MIndexes @Inject() (
     }
   }
 
-  /** Выставить алиас на текущий индекс. */
-  def installIndexAlias(newIndexName: String): Future[_] = {
-    val aliasName = INDEX_ALIAS_NAME
-    val fut = esClient.admin().indices()
-      .prepareAliases()
-      // Удалить все алиасы с необходимым именем.
-      .removeAlias("*", aliasName)
-      // Добавить алиас на новый индекс.
-      .addAlias(newIndexName, aliasName)
-      .execute()
+  override protected def INDEX_ALIAS_NAME = MIndexes.INDEX_ALIAS_NAME
 
-    // Подключить логгирование к работе...
-    lazy val logPrefix = s"installIndexAliase($newIndexName <= $aliasName):"
-    fut.onComplete {
-      case Success(r)  => debug(s"$logPrefix OK, result = $r")
-      case Failure(ex) => error(s"$logPrefix Failed to update index alias $aliasName", ex)
-    }
-
-    fut
-  }
-
-
-  /** Узнать имя индекса, сокрытого за глобальным алиасом INDEX_ALIAS_NAME. */
-  def getAliasedIndexName(): Future[Set[String]] = {
-    val fut = esClient.admin().indices()
-      .prepareGetAliases(INDEX_ALIAS_NAME)
-      .execute()
-
-    val logPrefix = "getAliasesIndexName():"
-    fut
-      .map { resp =>
-        trace(s"$logPrefix Ok, found ${resp.getAliases.size()} indexes.")
-        resp.getAliases
-          .keysIt()
-          .toSet
-      }
-      .recover { case ex: ResourceNotFoundException =>
-        // Если алиасов не найдено, ES обычно возвращает ошибку 404. Это тоже отработать надо бы.
-        warn(s"$logPrefix 404, suppressing error to empty result.", ex)
-        Set.empty
-      }
-  }
-
-
-  /** Собрать новый индекс для заливки туда моделей ipgeobase. */
-  def createIndex(newIndexName: String): Future[_] = {
-    val fut: Future[_] = {
-      esClient.admin().indices()
-        .prepareCreate(newIndexName)
-        // Надо сразу отключить index refresh в целях оптимизации bulk-заливки в индекс.
-        .setSettings( indexSettingsCreate() )
-        .execute()
-    }
-
-    lazy val logPrefix = s"createIndex($newIndexName):"
-    fut.onComplete {
-      case Success(res) => debug(s"$logPrefix Ok, $res")
-      case Failure(ex)  => error(s"$logPrefix failed", ex)
-    }
-
-    fut
-  }
-
-  /** Сгенерить настройки для индекса. */
-  def indexSettingsCreate(): Settings = {
+  /** Сгенерить настройки для создаваемого индекса. */
+  override def indexSettingsCreate: Settings = {
     Settings.builder()
       // Индекс ipgeobase не обновляется после заливки, только раз в день полной перезаливкой. Поэтому refresh не нужен.
       .put("index.refresh",  -1)
@@ -140,70 +76,12 @@ class MIndexes @Inject() (
   }
 
   /** Сгенерить настройки для индекса. */
-  def indexSettingsReady(): Settings = {
-    Settings.builder()
+  override def indexSettingsAfterBulk: Option[Settings] = {
+    val settings = Settings.builder()
       // Индекс ipgeobase не обновляется после заливки, только раз в день полной перезаливкой. Поэтому refresh не нужен.
       .put("index.number_of_replicas", REPLICAS_COUNT)
       .build()
-  }
-
-  /** Логика удаления старого ненужного индекса. */
-  def deleteIndex(oldIndexName: String): Future[_] = {
-    val fut: Future[_] = esClient.admin().indices()
-      .prepareDelete(oldIndexName)
-      .execute()
-
-    val logPrefix = s"deleteIndex($oldIndexName):"
-
-    // Отрабатывать ситуацию, когда индекс не найден.
-    val fut1 = fut.recover { case ex: ResourceNotFoundException =>
-      debug(s"$logPrefix Looks like, index not exist, already deleted?", ex)
-    }
-
-    // Логгировать завершение команды.
-    fut1.onComplete {
-      case Success(res) => debug(s"$logPrefix Index deleted ok: $res")
-      case Failure(ex)  => error(s"$logPrefix Failed to delete index", ex)
-    }
-
-    fut1
-  }
-
-
-  /** Когда заливка данных закончена, выполнить подготовку индекса к эсплуатации.
-    * elasticsearch 2.0+: переименовали операцию optimize в force merge. */
-  def optimizeAfterBulk(newIndexName: String): Future[_] = {
-    val startedAt = System.currentTimeMillis()
-
-    // Запустить оптимизацию всего ES-индекса.
-    val inxOptFut: Future[_] = {
-      esClient.admin().indices()
-        .prepareForceMerge(newIndexName)
-        .setMaxNumSegments(1)
-        .setFlush(true)
-        .execute()
-    }
-
-    lazy val logPrefix = s"optimizeAfterBulk($newIndexName):"
-
-    // Потом нужно выставить не-bulk настройки для готового к работе индекса.
-    val inxSettingsFut = inxOptFut.flatMap { _ =>
-      val updInxSettingsAt = System.currentTimeMillis()
-      val fut2: Future[_] = {
-        esClient.admin().indices()
-          .prepareUpdateSettings(newIndexName)
-          .setSettings( indexSettingsReady() )
-          .execute()
-      }
-
-      trace(s"$logPrefix Optimize took ${updInxSettingsAt - startedAt} ms")
-      for (_ <- fut2)
-        trace(s"$logPrefix Update index settings took ${System.currentTimeMillis() - updInxSettingsAt} ms.")
-
-      fut2
-    }
-
-    inxSettingsFut
+    Some(settings)
   }
 
 }
