187b261f32f6595bdaaf9c0de905355b7282eaca
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2016-11-09 16:40:25 +0300

    MGeoPoint: поддержка сериализации/десериализации в url-safe строку.

--- src/main/scala/io/suggest/sjs/common/model/loc/MGeoPoint.scala
+++ src/main/scala/io/suggest/sjs/common/model/loc/MGeoPoint.scala
@@ -1,6 +1,7 @@
 package io.suggest.sjs.common.model.loc
 
-import io.suggest.geo.{GeoConstants, IGeoPoint}
+import io.suggest.geo.IGeoPoint
+import io.suggest.geo.GeoConstants.Qs
 import io.suggest.sjs.common.geo.json.{GjGeometry, GjTypes}
 import org.scalajs.dom.Coordinates
 
@@ -22,7 +23,26 @@ object MGeoPoint {
     )
   }
 
+  def fromString(str: String): Option[MGeoPoint] = {
+    str.split(Qs.LAT_LON_DELIM_FN) match {
+      case Array(latStr, lonStr) =>
+        try {
+          val gp = MGeoPoint(
+            lat = latStr.toDouble,
+            lon = lonStr.toDouble
+          )
+          Some(gp)
+        } catch { case ex: Throwable =>
+          None
+        }
+
+      case other =>
+        None
+    }
+  }
+
 }
+
 
 case class MGeoPoint(
   override val lat: Double,
@@ -35,13 +55,17 @@ case class MGeoPoint(
   def toJsArray = toArray.asInstanceOf[ js.Array[js.Any] ]
 
   def toJsObject = js.Dictionary[Double](
-    GeoConstants.Qs.LAT_FN -> lat,
-    GeoConstants.Qs.LON_FN -> lon
+    Qs.LAT_FN -> lat,
+    Qs.LON_FN -> lon
   )
 
   def toGjPoint = GjGeometry(
     gtype       = GjTypes.Geom.POINT,
     coordinates = toJsArray
   )
+
+  override def toString: String = {
+    lat.toString + Qs.LAT_LON_DELIM_FN + lon.toString
+  }
 
 }
