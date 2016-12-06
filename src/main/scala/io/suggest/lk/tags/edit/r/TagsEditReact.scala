52ccc26f5975205bc10d5f294a65025542c89106
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2016-12-06 22:44:21 +0300

    Начато реальное внедрение react.js в код формы георазмещения, нет сил больше это терпеть. web21: Поддержка client-side messages. Реализована черновая react.js поддержка для виджета выбора периода размещения и кое-каких ещё вещей.

---
+++ src/main/scala/io/suggest/lk/tags/edit/r/TagsEditReact.scala
@@ -0,0 +1,24 @@
+package io.suggest.lk.tags.edit.r
+
+import io.suggest.css.Css
+import io.suggest.lk.r.{Forms, PropTable}
+import io.suggest.lk.vm.LkMessagesWindow.lkJsMessages
+import io.suggest.sjs.common.dt.interval.{QuickAdvPeriod, QuickAdvPeriods}
+import japgolly.scalajs.react.BackendScope
+import japgolly.scalajs.react.vdom.prefix_<^._
+
+/**
+  * Suggest.io
+  * User: Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
+  * Created: 06.12.16 22:12
+  * Description: Реализация редактора тегов на базе react.js.
+  */
+object TagsEditReact {
+
+  object TagsEditor {
+
+    //class Backed($: BackendScope[])
+
+  }
+
+}
