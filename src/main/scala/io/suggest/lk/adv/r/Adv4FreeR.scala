26bd0ef29d95d49398cc482c2b069a51044c8cf4
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2016-12-07 23:43:29 +0300

    Первая отрендеренная форма на react.js: lk-adv-geo Пока не пашут все запросы к серверу, карта вообще, отчёт о дате размещения, рассчётная цена. Но уже работают операции с тегами, базово работает выбор периода.

---
+++ src/main/scala/io/suggest/lk/adv/r/Adv4FreeR.scala
@@ -0,0 +1,13 @@
+package io.suggest.lk.adv.r
+
+/**
+  * Suggest.io
+  * User: Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
+  * Created: 07.12.16 18:17
+  * Description: Реализация reusable-компонента поддержки бесплатного размещения для суперюзеров.
+  */
+object Adv4FreeR {
+
+  ??? // TODO Запилить сюда верстку и всё такое.
+
+}
