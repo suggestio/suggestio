9e8cec1b804136f9bf7d5e3e84a2e24a91c2fc61
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2016-12-09 22:48:48 +0300

    Ещё немного пиления react-формы adv-geo. locate control отвалился в ходе впиливания поддержки onLocationFound.

--- src/main/scala/io/suggest/lk/adv/geo/r/AdvGeoFormR.scala
+++ src/main/scala/io/suggest/lk/adv/geo/r/AdvGeoFormR.scala
@@ -3,20 +3,30 @@ package io.suggest.lk.adv.geo.r
 import io.suggest.adv.geo.AdvGeoConstants
 import io.suggest.common.maps.leaflet.LeafletConstants
 import io.suggest.css.Css
+import io.suggest.lk.adv.geo.tags.m.MMapGjResp
+import io.suggest.lk.adv.geo.tags.vm.AdIdInp
 import io.suggest.lk.adv.m.IAdv4FreeProps
 import io.suggest.lk.adv.r.Adv4FreeR
 import io.suggest.lk.router.jsRoutes
 import io.suggest.lk.tags.edit.r.TagsEditR
 import io.suggest.lk.vm.LkMessagesWindow.Messages
+import io.suggest.sjs.common.xhr.Xhr
 import io.suggest.sjs.dt.period.m.IDatesPeriodInfo
 import io.suggest.sjs.dt.period.r._
+import io.suggest.sjs.common.async.AsyncUtil.defaultExecCtx
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.prefix_<^._
 import react.leaflet.lmap.LMapR
 import react.leaflet.popup.PopupR
 import io.suggest.sjs.leaflet.L
+import io.suggest.sjs.leaflet.event.LocationEvent
+import io.suggest.sjs.leaflet.marker.Marker
 import react.leaflet.control.LocateControlR
 import react.leaflet.layer.TileLayerR
+
+import scala.concurrent.Future
+import scala.scalajs.js
+import scala.scalajs.js.UndefOr
 
 /**
   * Suggest.io
@@ -38,9 +48,18 @@ object AdvGeoFormR {
     method          : String = "POST"
   )
 
-  /** Модель состояния. */
+  /** Модель состояния.
+    * @param rcvrMarkers Маркеры точек-ресиверов для карты ресиверов, полученные с сервера.
+    *                    null -- дефолтовое состояние. Почти сразу же заменяется на
+    *                    Left(Future[Resp]) -- Запущен XHR за списком маркеров.
+    *                    Right([Markers]) -- массив маркеров, передаваемых в MarkerCluster при рендере.
+    * @param locationFound false -- значит callback нужен для геолокации.
+    *                      true -- геолокация уже была, действия уже были приняты.
+    */
   case class State(
-    onMainScreen : Boolean = true
+    onMainScreen : Boolean = true,
+    rcvrMarkers  : Either[Future[_], js.Array[Marker]] = null,
+    locationFound: Boolean = false
     //datesPeriodInfo : Option[IDatesPeriodInfo] = None
   ) {
     def withOnMainScreen(oms2: Boolean) = copy(onMainScreen = oms2)
@@ -59,6 +78,10 @@ object AdvGeoFormR {
 
     def adv4freeChanged(): Unit = {
       println("adv4freeChanged()")
+    }
+
+    def onLocationFound(): Unit = {
+      println("loc found")
     }
 
     def onMainScreenChanged(e: ReactEventI): Callback = {
@@ -146,7 +169,9 @@ object AdvGeoFormR {
             attribution   = LeafletConstants.Tiles.ATTRIBUTION_OSM
           )(),
 
-          LocateControlR()(),
+          LocateControlR(
+            onLocationFound = UndefOr.any2undefOrA({ _: LocationEvent => onLocationFound() })
+          )(),
 
           PopupR( position = L.latLng(50, 50) )(
             <.div(
@@ -161,11 +186,51 @@ object AdvGeoFormR {
 
     }
 
+
+    /** Запуск запроса маркеров ресиверов для рендера на карте. */
+    def _doMarkersRequest(): Unit = {
+      for {
+        adIdInp <- AdIdInp.find()
+        adId    <- adIdInp.adId
+      } {
+        val route = jsRoutes.controllers.LkAdvGeo.advRcvrsGeoJson(adId)
+
+        // Надо запустить запрос на сервер для получения списка узлов.
+        val gjFut = Xhr.requestJson(route)
+          .map(MMapGjResp.apply)
+
+        for {
+          gj <- gjFut
+        } yield {
+          // TODO Отрендерить маркеры.
+        }
+      }
+
+    }
+
+    /**
+      * После окончания рендера надо запустить XHR за точками.
+      * XHR-запросы рекомендуется запускать только после рендера.
+      *
+      * @see [[http://stackoverflow.com/a/27139507]]
+      */
+    def componentDidMount(): Callback = {
+      $.state >>= { state =>
+        if (state.rcvrMarkers == null) {
+          // Требуется запустить запрос маркеров
+          Callback.TODO("_doMarkersRequest()")
+        } else {
+          Callback.empty
+        }
+      }
+    }
+
   }
 
   protected val component = ReactComponentB[Props]("AdvGeoForm")
     .initialState( State() )
     .renderBackend[Backend]
+    .componentDidMount( _.backend.componentDidMount() )
     .build
 
   def apply(props: Props) = component(props)
