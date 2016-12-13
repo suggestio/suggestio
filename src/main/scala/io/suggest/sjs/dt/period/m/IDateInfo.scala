a85bb1b6e33b399013a769a46972cee9a927d8d0
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2016-12-13 19:49:11 +0300

    Запилена поддержка попапов в react-форму lk-adv-geo. Удалено немного старого кода. Надо теперь форму связать увязать сервером и даты починить.

--- src/main/scala/io/suggest/sjs/dt/period/m/IDateInfo.scala
+++ src/main/scala/io/suggest/sjs/dt/period/m/IDateInfo.scala
@@ -1,5 +1,10 @@
 package io.suggest.sjs.dt.period.m
 
+import io.suggest.dt.interval.DatesIntervalConstants.Json._
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSName
+
 /**
   * Suggest.io
   * User: Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
@@ -7,23 +12,29 @@ package io.suggest.sjs.dt.period.m
   * Description: Интефейс модели инфы о какой-то дате.
   */
 
-trait IDateInfo {
+@js.native
+trait IDateInfo extends js.Object {
 
   /** Локализованная дата. */
+  @JSName( DATE_FN )
   def date: String
 
   /** Локализованный день недели. */
+  @JSName( DOW_FN )
   def dow: String
 
 }
 
 
-trait IDatesPeriodInfo {
+@js.native
+trait IDatesPeriodInfo extends js.Object {
 
   /** Описание даты начала размещения. */
+  @JSName( START_FN )
   def start: IDateInfo
 
   /** Описание даты окончания размещения. */
+  @JSName( END_FN )
   def end: IDateInfo
 
 }
