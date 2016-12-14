f5a41edb795c40b5bea6c05ab9783a52d9489251
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2016-12-14 22:01:09 +0300

    Ещё пиление новой формы adv-geo: начато впиливание diode из-за сложности с голой react-формой. Начата унификация модели GeoPoint/MGeoPoint на уровне [common], чтобы можно было удобно впилить boopickle на клиенте и сервере.

--- src/main/scala/io/suggest/geo/IGeoPoint.scala
+++ src/main/scala/io/suggest/geo/IGeoPoint.scala
@@ -1,5 +1,7 @@
 package io.suggest.geo
 
+import io.suggest.geo.GeoConstants.Qs
+
 /**
  * Suggest.io
  * User: Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
@@ -14,9 +16,21 @@ trait IGeoPoint {
   /** Долгота. */
   def lon: Double
 
-  override def toString: String = "GeoPoint(" + lat + "," + lon + ")"
+  override def toString: String = {
+    lat.toString + Qs.LAT_LON_DELIM_FN + lon.toString
+  }
 
   /** Сериализованное представление координат точки. */
+  // TODO Оно используется только в устаревшем GeoMode.
   def toQsStr = lat.toString + "," + lon.toString
 
 }
+
+
+
+/** Дефолтовая, пошаренная между клиентом и сервером, реализация [[IGeoPoint]]. */
+case class MGeoPoint(
+  override val lat: Double,
+  override val lon: Double
+)
+  extends IGeoPoint
