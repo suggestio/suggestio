8c4563ca7273ccb2e3546f16c0894f3863eac36e
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2016-12-30 12:12:44 +0300

    lk-adv-geo-form: Попап для гео-области успешно открывается и рендерится. Осталось отработать закрытие попапов, и можно связывать форму с сервером.

--- src/main/scala/io/suggest/sjs/dt/period/r/DtpResult.scala
+++ src/main/scala/io/suggest/sjs/dt/period/r/DtpResult.scala
@@ -3,10 +3,9 @@ package io.suggest.sjs.dt.period.r
 import diode.react.ModelProxy
 import io.suggest.css.Css
 import io.suggest.dt.interval.MRangeYmd
-import io.suggest.dt.MYmd
 import io.suggest.lk.vm.LkMessagesWindow.Messages
-import io.suggest.sjs.common.dt.MYmdJs
 import io.suggest.common.html.HtmlConstants.SPACE
+import io.suggest.react.r.RangeYmdR
 import japgolly.scalajs.react.ReactComponentB
 import japgolly.scalajs.react.vdom.prefix_<^._
 
@@ -42,17 +41,6 @@ object DtpResult {
     }
     .build
 
-
-  /** Рендер title-аттрибута для даты. */
-  private def _dateTitle(ymd: MYmd) = {
-    val jsDate = MYmdJs.toJsDate(ymd)
-    val sioDow = jsDate.getDay() + 1
-    ^.title := Messages("DayOfWeek.N." + sioDow)
-  }
-
-  /** Рендер месяца в человеко-читабельном виде. */
-  private def _dateMonth(ymd: MYmd) = Messages("ofMonth.N." + ymd.month)
-
   type Props = ModelProxy[MRangeYmd]
 
   // Этот компонент надо использовать через proxy.connect().
@@ -60,47 +48,19 @@ object DtpResult {
     .stateless
     .render_P { p =>
       val v = p()
-      val sameYear = v.dateStart.year == v.dateEnd.year
 
       <.div(
         ^.`class` := Css.Dt.RESULT_VALUE,
 
         // с [дата начала]
         SPACE,
-        Messages( "from._date" ),
-        SPACE,
-
-        <.span(
-          // В подсказке содержится день недели.
-          _dateTitle(v.dateStart),
-          // В теле содержится дата:
-          v.dateStart.day,
-
-          // Дата начала может быть отрендерена неполностью. Если год/месяц совпадают с датой окончания:
-          (!sameYear || v.dateStart.month != v.dateEnd.month) ?= <.span(
-            SPACE,
-            _dateMonth(v.dateStart),
-            !sameYear ?= <.span(SPACE, v.dateStart.year)
+        RangeYmdR(
+          RangeYmdR.Props(
+            capFirst = false,
+            rangeYmd = v,
+            Messages = Messages
           )
-        ),
-
-        // до [дата окончания]
-        SPACE,
-        Messages( "till._date" ),
-        SPACE,
-        <.span(
-          // В подсказке содержится день недели.
-          _dateTitle(v.dateEnd),
-          v.dateEnd.day,
-          SPACE,
-          _dateMonth(v.dateEnd),
-          SPACE,
-          v.dateEnd.year
-        ),
-
-        // г.
-        SPACE,
-        Messages("year_abbrevated")
+        )
       )
     }
     .build
