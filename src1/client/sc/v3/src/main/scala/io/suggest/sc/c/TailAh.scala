4d7289264b94683820344ab165326cfaea400999
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2018-03-15 17:44:09 +0300

    TailAh: серьёзная оптимизация и рефакторинг обработки роут.
    Если от RouteTo нет изменений, то noChange.
    Все сравнения происходят только один раз и только с текущим снимком
    MainScreen, а не методом гуляния по всему состоянию.

--- src1/client/sc/v3/src/main/scala/io/suggest/sc/c/TailAh.scala
+++ src1/client/sc/v3/src/main/scala/io/suggest/sc/c/TailAh.scala
@@ -14,6 +14,9 @@ import io.suggest.sc.m.search.{MSearchTabs, SwitchTab, TagClick}
 import io.suggest.sjs.common.async.AsyncUtil.defaultExecCtx
 import io.suggest.sjs.common.controller.DomQuick
 import japgolly.univeq._
+import io.suggest.ueq.UnivEqUtil._
+
+import scala.concurrent.Future
 
 /**
   * Suggest.io
@@ -29,36 +32,44 @@ class TailAh[M](
   extends ActionHandler(modelRW)
 { ah =>
 
+  def getMainScreenSnapShot(v0: MScRoot = value): MainScreen = {
+    val inxState = v0.index.state
+    val searchOpened = v0.index.search.isShown
+    val currRcvrId = inxState.currRcvrId
+    MainScreen(
+      nodeId        = currRcvrId,
+      // Не рендерить координаты в URL, если находишься в контексте узла, закрыта панель поиска и нет выбранного тега.
+      // Это улучшит кэширование, возможно улучшит приватность при обмене ссылками.
+      locEnv        = OptionUtil.maybe {
+        currRcvrId.isEmpty || v0.index.search.isShown || v0.index.search.tags.selectedId.nonEmpty
+      }(v0.index.search.mapInit.state.center),
+      generation    = Some( inxState.generation ),
+      searchOpened  = searchOpened,
+      searchTab     = OptionUtil.maybe(searchOpened)( v0.index.search.currTab ),
+      tagNodeId     = v0.index.search.tags.selectedId,
+      menuOpened    = v0.index.menu.opened
+    )
+  }
+
   override protected val handle: PartialFunction[Any, ActionResult[M]] = {
 
     // Заставить роутер собрать новую ссылку.
     case ResetUrlRoute =>
-      val v0 = value
-      val inxState = v0.index.state
-      val searchOpened = v0.index.search.isShown
-      val currRcvrId = inxState.currRcvrId
-      val m = MainScreen(
-        nodeId        = currRcvrId,
-        // Не рендерить координаты в URL, если находишься в контексте узла, закрыта панель поиска и нет выбранного тега.
-        // Это улучшит кэширование, возможно улучшит приватность при обмене ссылками.
-        locEnv        = OptionUtil.maybe {
-          currRcvrId.isEmpty || v0.index.search.isShown || v0.index.search.tags.selectedId.nonEmpty
-        }(v0.index.search.mapInit.state.center),
-        generation    = Some( inxState.generation ),
-        searchOpened  = searchOpened,
-        searchTab     = OptionUtil.maybe(searchOpened)( v0.index.search.currTab ),
-        tagNodeId     = v0.index.search.tags.selectedId,
-        menuOpened    = v0.index.menu.opened
-      )
-      routerCtlF()
-        .set( m )
-        .runNow()
+      val m = getMainScreenSnapShot()
+      // Уведомить в фоне роутер, заодно разблокировав интерфейс.
+      Future {
+        routerCtlF()
+          .set( m )
+          .runNow()
+      }
+
       noChange
 
 
     // js-роутер заливает в состояние данные из URL.
     case m: RouteTo =>
       val v0 = value
+      val currMainScreen = getMainScreenSnapShot(v0)
 
       // Считаем, что js-роутер уже готов. Если нет, то это сообщение должно было быть перехвачено в JsRouterInitAh.
 
@@ -72,7 +83,7 @@ class TailAh[M](
       var fxsAcc = List.empty[Effect]
 
       // Проверка id узла. Если отличается, то надо перезаписать.
-      if (m.mainScreen.nodeId !=* inx.state.currRcvrId) {
+      if (m.mainScreen.nodeId !=* currMainScreen.nodeId) {
         nodeIndexNeedsReload = true
         inx = inx.withState {
           inx.state.withRcvrNodeId( m.mainScreen.nodeId.toList )
@@ -82,7 +93,7 @@ class TailAh[M](
       // Проверка поля generation
       for {
         generation2 <- m.mainScreen.generation
-        if generation2 !=* inx.state.generation
+        if !currMainScreen.generation.contains( generation2 )
       } {
         // generation не совпадает. Надо будет перезагрузить плитку.
         gridNeedsReload = true
@@ -92,23 +103,32 @@ class TailAh[M](
       }
 
       // Текущий открытый таб на панели поиска
-      for (currSearchTab <- m.mainScreen.searchTab)
-        fxsAcc ::= Effect.action( SwitchTab( currSearchTab ) )
+      for {
+        mSearchTab <- m.mainScreen.searchTab
+        if !currMainScreen.searchTab.contains( mSearchTab )
+      }
+        fxsAcc ::= Effect.action( SwitchTab( mSearchTab ) )
 
       // Проверка поля searchOpened
-      if (m.mainScreen.searchOpened !=* v0.index.search.isShown) {
+      if (m.mainScreen.searchOpened !=* currMainScreen.searchOpened) {
         // Вместо патчинга состояния имитируем клик: это чтобы возможные сайд-эффекты обычного клика тоже отработали.
         fxsAcc ::= Effect.action( SearchOpenClose(m.mainScreen.searchOpened) )
       }
 
       // Смотрим координаты текущей точки.
-      for (currGeoPoint <- m.mainScreen.locEnv) {
+      for {
+        currGeoPoint <- m.mainScreen.locEnv
+        if !currMainScreen.locEnv.contains(currGeoPoint)
+      } {
         needUpdateUi = true
         inx = withMapCenter(currGeoPoint, inx)
       }
 
       // Смотрим текущий выделенный тег
-      for (tagNodeId <- m.mainScreen.tagNodeId) {
+      for {
+        tagNodeId <- m.mainScreen.tagNodeId
+        if !currMainScreen.tagNodeId.contains( tagNodeId )
+      } {
         // Имитируем клик по тегу, да и всё.
         fxsAcc ::= Effect.action {
           TagClick( tagNodeId )
@@ -116,15 +136,20 @@ class TailAh[M](
       }
 
       // Сверить панель меню открыта или закрыта
-      if (m.mainScreen.menuOpened !=* v0.index.menu.opened) {
+      if (m.mainScreen.menuOpened !=* currMainScreen.menuOpened) {
         fxsAcc ::= Effect.action( MenuOpenClose(m.mainScreen.menuOpened) )
       }
 
       // Обновлённое состояние, которое может быть и не обновлялось:
       lazy val v2 = v0.withIndex( inx )
 
-      // Принять решение о перезагрузке выдачи, если возможно.
-      if (v0.internals.geoLockTimer.nonEmpty) {
+      // Принять решение о перезагрузке выдачи, если необходимо.
+      if ( fxsAcc.isEmpty && !needUpdateUi && !gridNeedsReload && inx ===* v0.index) {
+        // Роутер шлёт RouteTo на каждый чих, даже просто в ответ на выставление этой самой роуты.
+        // TODO надо как-то отучить его от этой привычки.
+        noChange
+
+      } else if (v0.internals.geoLockTimer.nonEmpty) {
         // Блокирование загрузки.
         val fxOpt = fxsAcc.mergeEffectsSet
         ah.updatedSilentMaybeEffect(v2, fxOpt)
