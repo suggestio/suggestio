e469471023fd288ba93b8a910188494bcb34161a
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2019-04-10 20:21:11 +0300

    + Контейнер для seaweedfs.
    Перенос контейнеров из doc в src1/server/docker/images/

---
+++ src1/server/docker/images/seaweedfs/entrypoint.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+case "$1" in
+
+  'master')
+  	ARGS="-ip `hostname -i` -mdir /data"
+	# Is this instance linked with an other master? (Docker commandline "--link master1:master")
+	if [ -n "$MASTER_PORT_9333_TCP_ADDR" ] ; then
+		ARGS="$ARGS -peers=$MASTER_PORT_9333_TCP_ADDR:$MASTER_PORT_9333_TCP_PORT"
+	fi
+  	exec /usr/bin/weed $@ $ARGS
+	;;
+
+  'volume')
+  	ARGS="-ip `hostname -i` -dir /data"
+	# Is this instance linked with a master? (Docker commandline "--link master1:master")
+  	if [ -n "$MASTER_PORT_9333_TCP_ADDR" ] ; then
+		ARGS="$ARGS -mserver=$MASTER_PORT_9333_TCP_ADDR:$MASTER_PORT_9333_TCP_PORT"
+	fi
+  	exec /usr/bin/weed $@ $ARGS
+	;;
+
+  'server')
+  	ARGS="-ip `hostname -i` -dir /data"
+  	if [ -n "$MASTER_PORT_9333_TCP_ADDR" ] ; then
+		ARGS="$ARGS -master.peers=$MASTER_PORT_9333_TCP_ADDR:$MASTER_PORT_9333_TCP_PORT"
+	fi
+  	exec /usr/bin/weed $@ $ARGS
+  	;;
+
+  'filer')
+  	ARGS="-ip `hostname -i` "
+  	if [ -n "$MASTER_PORT_9333_TCP_ADDR" ] ; then
+		ARGS="$ARGS -master=$MASTER_PORT_9333_TCP_ADDR:$MASTER_PORT_9333_TCP_PORT"
+	fi
+  	exec /usr/bin/weed $@ $ARGS
+	;;
+
+  's3')
+  	ARGS="-domainName=$S3_DOMAIN_NAME -key.file=$S3_KEY_FILE -cert.file=$S3_CERT_FILE"
+  	if [ -n "$FILER_PORT_8888_TCP_ADDR" ] ; then
+		ARGS="$ARGS -filer=$FILER_PORT_8888_TCP_ADDR:$FILER_PORT_8888_TCP_PORT"
+	fi
+  	exec /usr/bin/weed $@ $ARGS
+	;;
+
+  'cronjob')
+	MASTER=${WEED_MASTER-localhost:9333}
+	CRON_SCHEDULE=${CRON_SCHEDULE-*/5 * * * * *}
+	echo "$CRON_SCHEDULE" 'echo "volume.fix.replication" | weed shell -master='$MASTER > /crontab
+	echo "Running Crontab:"
+	cat /crontab
+	exec /usr/bin/supercronic /crontab
+	;;
+  *)
+  	exec /usr/bin/weed $@
+	;;
+esac
