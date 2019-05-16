c79d74d345fb87423f53b7cdd0c553f8d0280168
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2019-05-16 15:59:11 +0300

    Ещё подпиливание gitlab. Начата начальная интеграция с gitlab-ci.

--- src1/server/docker/sio/gitlab/docker-entrypoint.sh
+++ src1/server/docker/sio/gitlab/docker-entrypoint.sh
@@ -3,6 +3,11 @@
 ## Запускалка для redis на основе переменных окружения.
 ## - PG_HOST, PG_DATABASE, PG_PORT, PG_USERNAME, PG_PASSWORD
 ##   Реквизиты связи с СУБД PostgreSQL.
+##
+## Если ошибка 500 в project/settings/ci_cd:
+## su - gitlab -s /bin/sh -c '/usr/share/webapps/gitlab/bin/rails dbconsole'
+## UPDATE Projects SET runners_token_encrypted = NULL WHERE Name = 'sio2';
+## По мотивам https://forum.gitlab.com/t/project-ci-cd-settings-broken/25131/1
 
 set -e
 
@@ -29,9 +34,26 @@ if [ "x$PG_PASSWORD" = "x" ]; then
   PG_PASSWORD=$PG_USERNAME
 fi
 
-## Сгенерить конфиги на основе env.
-cat > /etc/webapps/gitlab/database.yml <<EOF
-production:
+## Организовать хостнейм для gitlab в конфиге:
+if [ "x$GITLAB_HOST" = "x" ]; then
+    GITLAB_HOST="source.suggest.io"
+fi
+## Пробросить trusted proxies:
+if [ "x$GITLAB_TRUSTED_PROXIES" = "x" ]; then
+  GITLAB_TRUSTED_PROXIES="10.0.0.0/8"
+fi
+## Если наружу торчит не-22-порт для ssh, то надо об этом сообщить.
+if [ "x$GITLAB_SSH_PORT" = "x" ]; then
+  ## Пока сразу выбрасываем наружу tcp:222, на будущее надо перейти на дефолтовый порт.
+  GITLAB_SSH_PORT=222
+fi
+
+_mkEnvConfigs() {
+  ## Сгенерить конфиги на основе env.
+  echo > /etc/webapps/gitlab/database.yml
+  for mode in production development test; do
+    cat >> /etc/webapps/gitlab/database.yml <<EOF
+$mode:
   adapter: postgresql
   encoding: unicode
   host: $PG_HOST
@@ -40,9 +62,11 @@ production:
   pool: 10
   username: $PG_USERNAME
   password: "$PG_PASSWORD"
-EOF
 
-cat >> /etc/webapps/gitlab-shell/config.yml << EOF
+EOF
+  done
+
+  cat >> /etc/webapps/gitlab-shell/config.yml << EOF
 # Redis settings used for pushing commit notices to gitlab 
 redis:
   bin: /usr/bin/redis-cli
@@ -54,33 +78,19 @@ redis:
   namespace: resque:gitlab
 EOF
 
+  HOST_RE="\s*host:\s*"
+  sed -r "0,/$HOST_RE.*/s/^($HOST_RE).*/\1${GITLAB_HOST}/" -i /etc/webapps/gitlab/gitlab.yml
+
+  TRUSTED_PROXIES_RE="trusted_proxies:"
+  sed -r "0,/^\s*$TRUSTED_PROXIES_RE/s/^(\s*)($TRUSTED_PROXIES_RE)/\1\2\n\1- $GITLAB_TRUSTED_PROXIES" -i /etc/webapps/gitlab/gitlab.yml
+
+  SSH_PORT_RE="ssh_port:"
+  sed -r "0,/$SSH_PORT_RE/s/(\s*)(#\s*)?($SSH_PORT_RE).*/\1\3 $GITLAB_SSH_PORT/" -i /etc/webapps/gitlab/gitlab.yml
+}
+
+
 ## TODO Проброс параметров следует позаимствовать из https://github.com/sameersbn/docker-gitlab assets/runtime/functions
 ## Там происходит подстановка множества переменных окружения в конфиги по шаблонам.
-
-
-## Организовать хостнейм для gitlab в конфиге:
-if [ "x$GITLAB_HOST" = "x" ]; then
-    GITLAB_HOST="source.suggest.io"
-fi
-HOST_RE="\s*host:\s*"
-sed -r "0,/$HOST_RE.*/s/^($HOST_RE).*/\1${GITLAB_HOST}/" -i /etc/webapps/gitlab/gitlab.yml
-
-## Пробросить trusted proxies:
-if [ "x$GITLAB_TRUSTED_PROXIES" = "x" ]; then
-  GITLAB_TRUSTED_PROXIES="10.0.0.0/8"
-fi
-TRUSTED_PROXIES_RE="trusted_proxies:"
-sed -r "0,/^\s*$TRUSTED_PROXIES_RE/s/^(\s*)($TRUSTED_PROXIES_RE)/\1\2\n\1- $GITLAB_TRUSTED_PROXIES" -i /etc/webapps/gitlab/gitlab.yml
-
-
-## Если наружу торчит не-22-порт для ssh, то надо об этом сообщить.
-if [ "x$GITLAB_SSH_PORT" = "x" ]; then
-  ## Пока сразу выбрасываем наружу tcp:222, на будущее надо перейти на дефолтовый порт.
-  GITLAB_SSH_PORT=222
-fi
-SSH_PORT_RE="ssh_port:"
-sed -r "0,/$SSH_PORT_RE/s/(\s*)(#\s*)?($SSH_PORT_RE).*/\1\3 $GITLAB_SSH_PORT/" -i /etc/webapps/gitlab/gitlab.yml
-
 
 ## Общий код вызова rake target.
 _rakeCmd() {
@@ -89,7 +99,7 @@ _rakeCmd() {
 }
 
 _initDb() {
-  _rakeCmd "gitlab:setup"
+  _rakeCmd "gitlab:setup DISABLE_DATABASE_ENVIRONMENT_CHECK=$1"
 }
 
 
@@ -104,6 +114,8 @@ _fatal() {
 _doInit() {
   echo "${gitlab_home}/ appears empty. First vol.mount. Re-initialize dir.content..."
   tar -C "${gitlab_home}" -xvpf "${gitlab_initial_backup_tar_gz}" || _fatal "Cannot unpack ${gitlab_initial_backup_tar_gz} into ${gitlab_home} ! Waiting for admin from ssh..."
+
+  _mkEnvConfigs
 
   ## Развернуть схему БД:
   echo "1. starting redis..."
@@ -137,19 +149,15 @@ _beforeStart() {
 
   ## Нужно запилить секретные seed'ы, которые должны выживать между обновлениями контейнера.
   for secretFile in "gitlab/secret" "gitlab-shell/secret"; do
-    volSecretFilePath="${gitlab_home}/_secrets/$secretFile"
     tgSecretFilePath="/etc/webapps/$secretFile"
-    if [ ! -s volSecretFilePath ]; then
+    if [ ! -s "$tgSecretFilePath" ]; then
       ## Ещё нет файла - первый запуск.
-      mkdir -p `dirname "$volSecretFilePath"`
-      hexdump -v -n 64 -e '1/1 "%02x"' /dev/urandom > "$volSecretFilePath"
-      chmod 600 "$volSecretFilePath"
-      chown root:root "$volSecretFilePath"
+      mkdir -p `dirname "$tgSecretFilePath"`
+      ## Если обычный (не-yaml) файл, то создать его, заполнив рандомной строкой данных:
+      hexdump -v -n 64 -e '1/1 "%02x"' /dev/urandom > "$tgSecretFilePath"
+      chmod 640 "$tgSecretFilePath"
+      chown root:${gitlab_group} "$tgSecretFilePath"
     fi
-    ## Уже есть этот файл с прошлых запусков. Скопипастить в нужное место, выставив права.
-    cp "$volSecretFilePath" "$tgSecretFilePath"
-    chown root:${gitlab_group} "$tgSecretFilePath"
-    chmod 640 "$tgSecretFilePath"
   done
 }
 
@@ -162,6 +170,7 @@ if [ "x$1" = "x" ]; then
   else
     ## Стандартный запуск:
     _beforeStart
+    _mkEnvConfigs
     ## redis запускаем с опережением, иначе gitlab не может запуститься из-за race condition.
     systemctl start redis.service
     exec systemctl --init default
@@ -169,14 +178,17 @@ if [ "x$1" = "x" ]; then
 
 elif [ "x$1" = "xmigrate" ]; then
   echo "Upgrading db..."
+  _mkEnvConfigs
   _rakeCmd "db:migrate"
 
 elif [ "x$1" = "xcheck" ]; then
   echo "Check..."
+  _mkEnvConfigs
   _rakeCmd "gitlab:check"
 
 elif [ "x$1" = "xinfo" ]; then
   echo "Info..."
+  _mkEnvConfigs
   _rakeCmd "gitlab:env:info"
   return $?
 
@@ -186,7 +198,8 @@ elif [ "x$1" = "xinit" ]; then
   
 elif [ "x$1" = "xinitdb" ]; then
   echo "Init DB..."
-  _initDb
+  _mkEnvConfigs
+  _initDb $2
 
 else
   ## Исполнение произвольных команд:
