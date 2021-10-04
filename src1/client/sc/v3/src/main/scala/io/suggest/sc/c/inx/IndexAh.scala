suggestio/suggestio

commit d2eaa35d94ec89627c973b4ffc1ddc9301339823

Add current location button to grid, so start to search nodes.

src1/client/sc/v3/src/main/scala/io/suggest/sc/c/inx/IndexAh.scala
@@ -7,7 +7,7 @@ import io.suggest.common.empty.OptionUtil
 import io.suggest.geo.{MGeoLoc, MGeoPoint, MLocEnv}
 import io.suggest.maps.nodes.MGeoNodesResp
 import io.suggest.msg.ErrorMsgs
-import io.suggest.spa.DiodeUtil.Implicits.ActionHandlerExt
+import io.suggest.spa.DiodeUtil.Implicits.{ActionHandlerExt, EffectsOps, PotOpsExt}
 import io.suggest.sc.ScConstants
 import io.suggest.sc.c.{IRespWithActionHandler, MRhCtx}
 import io.suggest.sc.index.{MSc3IndexResp, MScIndexArgs, MWelcomeInfo}
@@ -20,7 +20,6 @@ import io.suggest.sc.u.api.IScUniApi
 import io.suggest.sjs.common.async.AsyncUtil.defaultExecCtx
 import io.suggest.log.Log
 import io.suggest.maps.m.MMapS
-import io.suggest.spa.DiodeUtil.Implicits.EffectsOps
 import io.suggest.sc.ads.{MAdsSearchReq, MIndexAdOpenQs, MScFocusArgs, MScGridArgs, MScNodesArgs}
 import io.suggest.sc.c.search.{GeoTabAh, SearchAh}
 import io.suggest.sc.m.dia.err.MScErrorDia
@@ -108,7 +107,12 @@ object IndexAh {
     val indexViewAction = m.switchCtxOpt.flatMap(_.viewsAction)
 
     var i1 = i0.copy(
-      resp = i0.resp.ready(inx),
+      resp = i0.resp.ready(
+        MIndexRespData(
+          resp = inx,
+          scQs = m.qs,
+        )
+      ),
       state = i0.state.copy(
         switch = MInxSwitch.empty,
         // Обновить стопку индексов в связи с новым индексом:
@@ -220,7 +224,7 @@ object IndexAh {
 
     // Инициализация приветствия. Подготовить состояние welcome.
     val mWcSFutOpt = for {
-      resp <- i1.resp.toOption
+      respData <- i1.resp.toOption
       if {
         // Всякие FailingStale содержат старый ответ и новую ошибку, их надо отсеять:
         !i1.resp.isFailed &&
@@ -228,9 +232,9 @@ object IndexAh {
         m.switchCtxOpt.showWelcome
       }
 
-      wcInfo2 <- resp.welcome
+      wcInfo2 <- respData.resp.welcome
       // Не надо отображать текущее приветствие повторно:
-      if !i0.resp.exists(_.welcome contains[MWelcomeInfo] wcInfo2)
+      if !(respData.resp.welcome contains[MWelcomeInfo] wcInfo2)
     } yield {
       val tstamp = System.currentTimeMillis()
 
@@ -253,7 +257,7 @@ object IndexAh {
       .set( mWcSFutOpt.map(_._2) )(i1)
 
     // Нужно отребилдить ScCss, но только если что-то реально изменилось.
-    val scCssArgs2 = MScCssArgs.from( i1.resp, mroot.dev.screen.info )
+    val scCssArgs2 = MScCssArgs.from( i1.respOpt, mroot.dev.screen.info )
     if (scCssArgs2 !=* i1.scCss.args) {
       // Изменились аргументы. Пора отребилдить ScCss.
       i1 = MScIndex.scCss
@@ -342,9 +346,6 @@ class IndexRah
       .get( ctx )
     var nodesFoundMods = List.empty[MNodesFoundS => MNodesFoundS]
 
-    if (nodesFound0.reqSearchArgs.nonEmpty)
-      nodesFoundMods ::= MNodesFoundS.reqSearchArgs set None
-
     // Если на панели поиска pending, то его тоже сбросить. Такое бывает, когда происходило перемещение карты.
     if (nodesFound0.req.isPending)
       nodesFoundMods ::= MNodesFoundS.req.modify( _.fail(ex) )
@@ -423,8 +424,8 @@ class IndexRah
       !ctx.m.reason.isInstanceOf[ReGetIndex] &&
       resp.nodes.exists { node =>
         i0.resp.exists { rn =>
-          (rn isSamePlace node.props) &&
-          (rn isLooksFullySame node.props)
+          (rn.resp isSamePlace node.props) &&
+          (rn.resp isLooksFullySame node.props)
         }
       }
     ) {
@@ -483,8 +484,10 @@ class IndexRah
       )
 
       val v2 = MScRoot.index
-        .composeLens( IndexAh._inx_state_switch_ask_LENS )
-        .set( Some(switchAskState) )(v0)
+        .modify(
+          IndexAh._inx_state_switch_ask_LENS set Some(switchAskState) andThen
+          MScIndex.resp.modify( _.unPending )
+        )(v0)
 
       ActionResult.ModelUpdate(v2)
 
@@ -598,7 +601,7 @@ class IndexAh[M](
       // Forced index reload? Never deduplicate.
       !reason.isInstanceOf[ReGetIndex] &&
       // or if index-request args are really changed:
-      (v0.search.geo.found.reqSearchArgs contains[MScQs] args)
+      (v0.resp.exists(_.scQs ==* args))
     ) {
       // Дубликат запроса. Бывает при запуске, когда jsRouter и wzFirst сыплят одинаковые RouteTo().
       noChange
@@ -623,15 +626,14 @@ class IndexAh[M](
 
       var valueModF = MScIndex.resp.modify(_.pending(ts))
 
-      var nodesFoundModF = MNodesFoundS.reqSearchArgs set Some(args)
       // Выставить в состояние, что запущен поиск узлов, чтобы не было дублирующихся запросов от контроллера панели.
-      if (isSearchNodes)
-        nodesFoundModF = nodesFoundModF andThen MNodesFoundS.req.modify( _.pending(ts) )
-
-      valueModF = valueModF andThen MScIndex.search
-        .composeLens( MScSearch.geo )
-        .composeLens( MGeoTabS.found )
-        .modify( nodesFoundModF )
+      if (isSearchNodes) {
+        valueModF = valueModF andThen MScIndex.search
+          .composeLens( MScSearch.geo )
+          .composeLens( MGeoTabS.found )
+          .composeLens( MNodesFoundS.req )
+          .modify( _.pending(ts) )
+      }
 
       val v2 = valueModF(v0)
       ah.updateMaybeSilentFx(silentUpdate)(v2, fxAcc.mergeEffects.get)
@@ -960,12 +962,11 @@ class IndexAh[M](
     // Отладка: обнуление текущего индекса.
     case UnIndex =>
       val v0 = value
-      val resp2 = Pot.empty[MSc3IndexResp]
       val v2 = (
-        (MScIndex.resp set resp2) andThen
+        (MScIndex.resp set Pot.empty) andThen
         (MScIndex.scCss
           .composeLens(ScCss.args)
-          .set( MScCssArgs.from(resp2, rootRO.value.dev.screen.info) )
+          .set( MScCssArgs.from( None, rootRO.value.dev.screen.info ) )
         )
       )(v0)
       updated(v2)

src1/client/lk/common/src/main/scala/io/suggest/lk/r/plat/PlatformComponents.scala
@@ -1,6 +1,6 @@
 package io.suggest.lk.r.plat
 
-import com.materialui.{Mui, MuiDialogActionsClasses, MuiDialogActionsProps, MuiDialogTitle, MuiDialogTitleClasses, MuiDialogTitleProps, MuiSwitch, MuiTypoGraphy, MuiTypoGraphyProps, MuiTypoGraphyVariants}
+import com.materialui.{Mui, MuiDialogActionsClasses, MuiDialogActionsProps, MuiDialogTitle, MuiDialogTitleClasses, MuiDialogTitleProps, MuiSvgIcon, MuiSwitch, MuiTypoGraphy, MuiTypoGraphyProps, MuiTypoGraphyVariants}
 import com.mui.treasury.styles.switch
 import io.suggest.css.Css
 import japgolly.scalajs.react._
@@ -27,6 +27,16 @@ class PlatformComponents(
       }
     } else {
       MuiSwitch.component
+    }
+  }
+
+
+  /** On iOS, use location icon from apple docs. */
+  def muiLocationIconComp: MuiSvgIcon = {
+    if (getPlatformCss().isRenderIos) {
+      Mui.SvgIcons.NearMe
+    } else {
+      Mui.SvgIcons.MyLocation
     }
   }
 

src1/client/sc/v3/src/main/scala/io/suggest/sc/Sc3Circuit.scala
@@ -33,7 +33,7 @@ import io.suggest.sc.c.inx.{ConfUpdateRah, IndexAh, IndexRah, ScConfAh, WelcomeA
 import io.suggest.sc.c.jsrr.JsRouterInitAh
 import io.suggest.sc.c.menu.DlAppAh
 import io.suggest.sc.c.search._
-import io.suggest.sc.index.{MSc3IndexResp, MScIndexes}
+import io.suggest.sc.index.MScIndexes
 import io.suggest.sc.m._
 import io.suggest.sc.m.boot.MScBoot.MScBootFastEq
 import io.suggest.sc.m.boot.{Boot, IBootAction, MBootServiceIds, MSpaRouterState}
@@ -53,7 +53,7 @@ import io.suggest.sc.v.search.SearchCss
 import io.suggest.log.CircuitLog
 import io.suggest.sjs.common.async.AsyncUtil.defaultExecCtx
 import io.suggest.sjs.dom2._
-import io.suggest.spa.{CircuitUtil, DAction, DoNothing, DoNothingActionProcessor, FastEqUtil, IHwBtnAction, OptFastEq}
+import io.suggest.spa.{DAction, DoNothing, DoNothingActionProcessor, FastEqUtil, IHwBtnAction, OptFastEq}
 import io.suggest.spa.DiodeUtil.Implicits._
 import io.suggest.spa.CircuitUtil._
 import org.scalajs.dom
@@ -148,8 +148,6 @@ class Sc3Circuit(
       //unsafeOffsets = HwScreenUtil.etScreenUnsafeAreas( mscreen ),
     )
 
-    val scIndexResp = Pot.empty[MSc3IndexResp]
-
     // random seed изначально отсутствует в конфиге.
     val (conf2, gen2) = scInit.conf.gen.fold {
       val generation2 = System.currentTimeMillis()
@@ -182,7 +180,7 @@ class Sc3Circuit(
         ),
       ),
       index = MScIndex(
-        resp = scIndexResp,
+        resp = Pot.empty,
         search = MScSearch(
           geo = MGeoTabS(
             mapInit = MMapInitState(
@@ -192,7 +190,7 @@ class Sc3Circuit(
           ),
         ),
         scCss = ScCss(
-          MScCssArgs.from(scIndexResp, screenInfo)
+          MScCssArgs.from( None, screenInfo )
         ),
         state = MScIndexState(
           generation = gen2,
@@ -264,7 +262,7 @@ class Sc3Circuit(
     // Заголовок узла.
     for {
       inxResp <- mroot.index.resp
-      nodeName <- inxResp.name
+      nodeName <- inxResp.resp.name
     } {
       acc ::= nodeName
     }

src1/client/sc/v3/src/main/scala/io/suggest/sc/Sc3Module.scala
@@ -177,6 +177,7 @@ class Sc3Module extends Log { outer =>
 
   // grid
   lazy val gridR   = wire[GridR]
+  lazy val locationButtonR = wire[LocationButtonR]
 
 
   // search

src1/client/sc/v3/src/main/scala/io/suggest/sc/c/TailAh.scala
@@ -23,7 +23,7 @@ import io.suggest.sc.c.dia.WzFirstDiaAh
 import io.suggest.sc.index.{MIndexesRecentJs, MSc3IndexResp, MScIndexArgs, MScIndexInfo, MScIndexes}
 import io.suggest.sc.m.boot.{Boot, BootAfter, MBootServiceIds}
 import io.suggest.sc.m.dia.first.{InitFirstRunWz, MWzPhases, WzPhasePermRes}
-import io.suggest.sc.m.in.{MInternalInfo, MJsRouterS, MScInternals}
+import io.suggest.sc.m.in.{MGeoLocTimerData, MInternalInfo, MJsRouterS, MScInternals}
 import io.suggest.sc.m.inx.save.MIndexesRecentOuter
 import io.suggest.sc.m.menu.DlAppOpen
 import io.suggest.sc.u.api.IScStuffApi
@@ -104,12 +104,12 @@ object TailAh {
 
 
   private def _removeTimer(v0: MScRoot) = {
-    for (gltId <- v0.internals.info.geoLockTimer) yield {
+    for (glt <- v0.internals.info.geoLockTimer) yield {
       val alterF = MScRoot.internals
         .composeLens( MScInternals.info )
         .composeLens( MInternalInfo.geoLockTimer )
         .set( Pot.empty.unavailable() )
-      val fx = _clearTimerFx( gltId )
+      val fx = _clearTimerFx( glt.timerId )
       (alterF, fx)
     }
   }
@@ -126,15 +126,23 @@ object TailAh {
     * @return Обновлённое состояние + эффект ожидания срабатывания таймера.
     *         Но таймер - уже запущен к этому моменту.
     */
-  def mkGeoLocTimer(switchCtx: MScSwitchCtx, currTimerIdOpt: Pot[Int]): (MScInternals => MScInternals, Effect) = {
-    val tp = DomQuick.timeoutPromiseT( ScConstants.ScGeo.INIT_GEO_LOC_TIMEOUT_MS )( GeoLocTimeOut(switchCtx) )
+  def mkGeoLocTimer(glTimerStart: GeoLocTimerStart, currTimerPot: Pot[MGeoLocTimerData]): (MScInternals => MScInternals, Effect) = {
+    val tp = DomQuick.timeoutPromiseT( ScConstants.ScGeo.INIT_GEO_LOC_TIMEOUT_MS )( GeoLocTimeOut )
     val modifier = MScInternals.info
       .composeLens( MInternalInfo.geoLockTimer )
-      .set( Ready( tp.timerId ).pending() )
+      .set(
+        Ready(
+          MGeoLocTimerData(
+            timerId = tp.timerId,
+            reason = glTimerStart,
+          )
+        )
+          .pending()
+      )
 
     var timeoutFx: Effect = Effect( tp.fut )
-    for (currTimerId <- currTimerIdOpt) {
-      timeoutFx += _clearTimerFx(currTimerId)
+    for (currTimer <- currTimerPot) {
+      timeoutFx += _clearTimerFx( currTimer.timerId )
     }
 
     (modifier, timeoutFx)
@@ -372,7 +380,7 @@ class TailAh(
         // Эффект, если есть роута и что-то изменилось.
         val fxOpt = for {
           currRoute       <- v0.internals.info.currRoute
-          currIndexResp   <- v0.index.respOpt
+          currIndexRespData <- v0.index.respOpt
         } yield {
           Effect.action {
             // Получить на руки текущее сохранённое состояние:
@@ -391,13 +399,13 @@ class TailAh(
             val inxInfo2 = MScIndexInfo(
               state         = currRoute,
               indexResp     = {
-                if (currIndexResp.geoPoint.nonEmpty) {
-                  currIndexResp
+                if (currIndexRespData.geoPoint.nonEmpty) {
+                  currIndexRespData
                 } else {
                   // Если нет гео-точки узла, то передрать его с гео-карты.
                   (
                     MSc3IndexResp.geoPoint set Some(v0.index.search.geo.mapInit.state.center)
-                  )(currIndexResp)
+                  )(currIndexRespData)
                 }
               },
             )
@@ -739,7 +747,7 @@ class TailAh(
               retUserLoc  = false,
             )
           )
-          val (viMod, fx) = TailAh.mkGeoLocTimer( switchCtx, v0.internals.info.geoLockTimer )
+          val (viMod, fx) = TailAh.mkGeoLocTimer( GeoLocTimerStart(switchCtx), v0.internals.info.geoLockTimer )
           modsAcc ::= MScRoot.internals.modify(viMod)
           fxsAcc ::= fx
         }
@@ -839,11 +847,10 @@ class TailAh(
 
       for {
         // TODO Вероятно, надо как-то реагировать на первый GlPubSignal, даже если запущенного таймера (уже) нет.
-        geoLockTimerId <- v0.internals.info.geoLockTimer
+        geoLockTimer <- v0.internals.info.geoLockTimer
         glSignal <- m.origOpt
         if glSignal.glType.isHighAccuracy
-      } {
-        val indexMapReset2 = !mapNeedReset
+      } {val indexMapReset2 = !mapNeedReset
         val switchCtx = m.scSwitch.fold {
           MScSwitchCtx(
             indexQsArgs = MScIndexArgs(
@@ -857,7 +864,7 @@ class TailAh(
 
         fxAcc ::= TailAh.getIndexFx( switchCtx )
         fxAcc ::= Effect.action {
-          DomQuick.clearTimeout(geoLockTimerId)
+          DomQuick.clearTimeout( geoLockTimer.timerId )
           DoNothing
         }
 
@@ -932,23 +939,20 @@ class TailAh(
 
 
     // Наступил таймаут ожидания геолокации. Нужно активировать инициализацию в имеющемся состоянии
-    case m: GeoLocTimeOut =>
+    case GeoLocTimeOut =>
       val v0 = value
-      v0.internals.info.geoLockTimer.fold(noChange) { _ =>
+      v0.internals.info.geoLockTimer.fold(noChange) { glTimerData =>
         // Удалить из состояния таймер геолокации, запустить выдачу.
-        var fxsAcc = List.empty[Effect]
-        val v2 = TailAh._removeTimer(v0)
-          .fold(v0) { case (alterF, ctFx) =>
-            fxsAcc ::= ctFx
+        var fx: Effect = TailAh.getIndexFx( glTimerData.reason.switchCtx )
+
+        val v2 = TailAh
+          ._removeTimer(v0)
+          .fold(v0) { case (alterF, cancelTimerFx) =>
+            fx += cancelTimerFx
             alterF( v0 )
           }
 
-        // Если demandLocTest, то и остановиться на этой ошибке:
-        //if (!m.switchCtx.demandLocTest) {
-        fxsAcc ::= TailAh.getIndexFx( m.switchCtx )
-        //}
-
-        ah.updatedSilentMaybeEffect(v2, fxsAcc.mergeEffects)
+        updated( v2, fx )
       }
 
 
@@ -963,9 +967,9 @@ class TailAh(
         effectOnly( fx )
 
       } else {
-        val (viMod, fx) = TailAh.mkGeoLocTimer( m.switchCtx, v0.internals.info.geoLockTimer )
+        val (viMod, fx) = TailAh.mkGeoLocTimer( m, v0.internals.info.geoLockTimer )
         val v2 = MScRoot.internals.modify(viMod)(v0)
-        updatedSilent( v2, fx )
+        updated( v2, fx )
       }
 
 

src1/client/sc/v3/src/main/scala/io/suggest/sc/c/dia/WzFirstDiaAh.scala
@@ -8,9 +8,8 @@ import io.suggest.radio.beacon.{BtOnOff, IBeaconsListenerApi, MBeaconerOpts}
 import io.suggest.common.empty.OptionUtil
 import io.suggest.conf.ConfConst
 import io.suggest.cordova.CordovaConstants
-import io.suggest.dev.MPlatformS
+import io.suggest.dev.{MOsFamilies, MPlatformS}
 import io.suggest.geo.GeoLocUtilJs
-import io.suggest.i18n.MsgCodes
 import io.suggest.msg.ErrorMsgs
 import io.suggest.os.notify.NotificationPermAsk
 import io.suggest.os.notify.api.cnl.CordovaNotificationlLocalUtil
@@ -609,51 +608,7 @@ class WzFirstDiaAh[M <: AnyRef](
     // Список спецификаций фаз с инструкциями, которые необходимо пройти для инициализации.
     lazy val h5PermApiAvail = Html5PermissionApi.isApiAvail()
 
-    // Геолокация
-    new IPermissionSpec {
-      override def phase = MWzPhases.GeoLocPerm
-      override def isSupported(): Boolean = {
-        // Для cordova нет смысла проверять наличие плагина, который всегда должен быть.
-        CordovaConstants.isCordovaPlatform() || GeoLocUtilJs.envHasGeoLoc()
-      }
-      override def readPermissionState(): Future[IPermissionState] = {
-        def htmlAskPermF = {
-          IPermissionState.maybeKnownF(h5PermApiAvail)(
-            Html5PermissionApi.getPermissionState( PermissionName.geolocation )
-          )
-        }
-        if (platform.isCordova) {
-          CordovaDiagonsticPermissionUtil
-            .getGeoLocPerm()
-            .recoverWith { case ex: Throwable =>
-              // diag-плагин на разных платформах работает по-разному. Отрабатываем ситуацию, когда он может не работать:
-              logger.info( ErrorMsgs.DIAGNOSTICS_RETRIEVE_FAIL, ex, (Try(Cordova), Try(Cordova.plugins), Try(Cordova.plugins.diagnostic)) )
-              htmlAskPermF
-            }
-        } else {
-          htmlAskPermF
-        }
-      }
-      override def requestPermissionFx = Effect.action {
-        GeoLocOnOff( enabled = true, isHard = true )
-      }
-      override def listenChangesOfPot = Some {
-        sc3Circuit.scGeoLocRW.zoom[Pot[_]] { scGeoLoc =>
-          (for {
-            glWatcher <- scGeoLoc.watchers.valuesIterator
-            // glWatcher.watchId - not ok here, because it can be Ready() even with missing permissions.
-            pot = glWatcher.lastPos
-            // TODO Commented code, because watchers map have lenght 0 or 1. For many items, need to uncomment.
-            //if pot !=* Pot.empty
-          } yield {
-            pot
-          })
-            .nextOption()
-            .getOrElse( Pot.empty )
-        }
-      }
-      override def settingKeys = ConfConst.ScSettings.LOCATION_ENABLED :: Nil
-    } #:: new IPermissionSpec {
+    val specs0 = new IPermissionSpec {
       // Bluetooth
       override def phase = MWzPhases.BlueToothPerm
       override def isSupported() = hasBleRO()
@@ -706,6 +661,57 @@ class WzFirstDiaAh[M <: AnyRef](
     } #::
     // End of permissions specs.
     LazyList.empty[IPermissionSpec]
+
+    if (platform.osFamily contains MOsFamilies.Apple_iOS) {
+      specs0
+
+    } else {
+      // Запрос доступа к геолокации.
+      new IPermissionSpec {
+        override def phase = MWzPhases.GeoLocPerm
+        override def isSupported(): Boolean = {
+          // Для cordova нет смысла проверять наличие плагина, который всегда должен быть.
+          CordovaConstants.isCordovaPlatform() || GeoLocUtilJs.envHasGeoLoc()
+        }
+        override def readPermissionState(): Future[IPermissionState] = {
+          def htmlAskPermF = {
+            IPermissionState.maybeKnownF(h5PermApiAvail)(
+              Html5PermissionApi.getPermissionState( PermissionName.geolocation )
+            )
+          }
+          if (platform.isCordova) {
+            CordovaDiagonsticPermissionUtil
+              .getGeoLocPerm()
+              .recoverWith { case ex: Throwable =>
+                // diag-плагин на разных платформах работает по-разному. Отрабатываем ситуацию, когда он может не работать:
+                logger.info( ErrorMsgs.DIAGNOSTICS_RETRIEVE_FAIL, ex, (Try(Cordova), Try(Cordova.plugins), Try(Cordova.plugins.diagnostic)) )
+                htmlAskPermF
+              }
+          } else {
+            htmlAskPermF
+          }
+        }
+        override def requestPermissionFx = Effect.action {
+          GeoLocOnOff( enabled = true, isHard = true )
+        }
+        override def listenChangesOfPot = Some {
+          sc3Circuit.scGeoLocRW.zoom[Pot[_]] { scGeoLoc =>
+            (for {
+              glWatcher <- scGeoLoc.watchers.valuesIterator
+              // glWatcher.watchId - not ok here, because it can be Ready() even with missing permissions.
+              pot = glWatcher.lastPos
+              // TODO Commented code, because watchers map have lenght 0 or 1. For many items, need to uncomment.
+              //if pot !=* Pot.empty
+            } yield {
+              pot
+            })
+              .nextOption()
+              .getOrElse( Pot.empty )
+          }
+        }
+        override def settingKeys = ConfConst.ScSettings.LOCATION_ENABLED :: Nil
+      } #:: specs0
+    }
   }
 
 }

src1/client/sc/v3/src/main/scala/io/suggest/sc/m/MScRoot.scala
@@ -36,9 +36,8 @@ object MScRoot {
   @inline implicit def univEq: UnivEq[MScRoot] = UnivEq.derive
 
 
-  def scCssArgsFrom(scRoot: MScRoot): MScCssArgs = {
-    MScCssArgs.from(scRoot.index.resp, scRoot.dev.screen.info)
-  }
+  def scCssArgsFrom(scRoot: MScRoot): MScCssArgs =
+    MScCssArgs.from( scRoot.index.respOpt, scRoot.dev.screen.info )
 
   /** "Линзы" для упрощённого доступа к полям нижнего уровня. */
   val dev       = GenLens[MScRoot](_.dev)

src1/client/sc/v3/src/main/scala/io/suggest/sc/m/ScRootActions.scala
@@ -166,11 +166,17 @@ case class IndexRecentNodeClick( inxRecent: MSc3IndexResp ) extends IScTailActio
 /** Запуск таймера ожидания получения гео-координат.
   * @param switchCtx Index switch context.
   * @param allowImmediate Allow immediate timerless reindex, if accurate geo.data already presents.
+  * @param animateLocBtn Animate location button on the grid?
   */
-case class GeoLocTimerStart( switchCtx: MScSwitchCtx, allowImmediate: Boolean = true ) extends IScTailAction
+case class GeoLocTimerStart(
+                             switchCtx        : MScSwitchCtx,
+                             allowImmediate   : Boolean = true,
+                             animateLocBtn    : Boolean = false,
+                           )
+  extends IScTailAction
 
 /** Наступление таймаута получения гео-координат. */
-case class GeoLocTimeOut( switchCtx: MScSwitchCtx ) extends IScTailAction
+case object GeoLocTimeOut extends IScTailAction
 
 
 /** Экшен для запуска обработки унифицированного ответа выдачи, который бывает сложным и много-гранным.

commit 95845308bdcf68ce3987a5a6f092fa501e633e6b

Fixed welcome screen on nodes appearance.
Due to regression since d2eaa35d94ec89627c973b4ffc1ddc9301339823,
welcome pictures was on appeared at all.

src1/client/sc/v3/src/main/scala/io/suggest/sc/c/inx/IndexAh.scala
@@ -243,7 +243,7 @@ object IndexAh {
 
       wcInfo2 <- respData.resp.welcome
       // Не надо отображать текущее приветствие повторно:
-      if !(respData.resp.welcome contains[MWelcomeInfo] wcInfo2)
+      if !(i0.resp.exists(_.resp.welcome contains[MWelcomeInfo] wcInfo2))
     } yield {
       val tstamp = System.currentTimeMillis()
 

src1/client/util/beaconer/src/main/scala/io/suggest/radio/beacon/MBeaconerS.scala
@@ -20,7 +20,7 @@ import monocle.macros.GenLens
   */
 object MBeaconerS {
 
-  def empty = MBeaconerS()
+  def empty = apply()
 
   @inline implicit def univEq: UnivEq[MBeaconerS] = UnivEq.force
 